c5337536b35dc2b92b34b6567600d96d897d90b0
valmack <valmack@protonmail.com>
2019-09-25 18:36:28 -0400

    Add black color

--- config/themes/colors.js
+++ config/themes/colors.js
@@ -1,4 +1,5 @@
 const colors = {
+  BLACK: (opacity = 1) => `rgba(36, 31, 58, ${opacity})`,
   BLUE: (opacity = 1) => `rgba(78, 83, 255, ${opacity})`,
   PURPLE: (opacity = 1) => `rgba(36, 31, 58, ${opacity})`,
   GREY: (opacity = 1) => `rgba(193, 193, 207, ${opacity})`,
