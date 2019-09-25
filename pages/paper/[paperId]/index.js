4554a10514f742a6a972b5116d5d2075a3c478f0
valmack <valmack@protonmail.com>
2019-09-25 15:53:21 -0400

    Move redirect to utils

--- pages/paper/[paperId]/index.js
+++ pages/paper/[paperId]/index.js
@@ -1,5 +1,4 @@
-import { Router, useRouter } from "next/router";
-import { endsWithSlash } from "~/config/utils";
+import { redirect } from "~/config/utils";
 
 function Paper() {}
 
@@ -9,24 +8,5 @@ Paper.getInitialProps = async (ctx) => {
   redirect(ctx, "paperId", redirectPath);
   return {};
 };
-
-function redirect(ctx, baseKey, path) {
-  path = buildRedirectPath(ctx, baseKey, path);
-  const { res } = ctx;
-  if (res) {
-    res.writeHead(302, { Location: path });
-    res.end();
-  } else {
-    Router.push(path);
-  }
-}
-
-function buildRedirectPath({ asPath, query }, baseKey, path) {
-  if (!endsWithSlash(asPath)) {
-    const base = query[baseKey];
-    path = base + "/" + path;
-  }
-  return path;
-}
 
 export default Paper;
