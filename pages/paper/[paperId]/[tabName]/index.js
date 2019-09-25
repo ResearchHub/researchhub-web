bd8ea53b8a2ad59bc5abdfb469e9ebe35613906a
valmack <valmack@protonmail.com>
2019-09-25 15:21:41 -0400

    Use alias to import paper tabs component

--- pages/paper/[paperId]/[tabName]/index.js
+++ pages/paper/[paperId]/[tabName]/index.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import PaperTabs from "../../../../components/paper-tabs";
+import PaperTabs from "~/components/paper-tabs";
 
 const Paper = () => {
   const router = useRouter();
