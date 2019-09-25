23f9a2ea9747f543771f4fb694eee59ba45611a9
valmack <valmack@protonmail.com>
2019-09-25 18:38:08 -0400

    Set app body element margin to 0

--- pages/_document.js
+++ pages/_document.js
@@ -40,7 +40,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body style={{ margin: 0 }}>
           <Main />
           <NextScript />
         </body>
