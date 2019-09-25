1a0f38ae3e21ba5efa62b50f80c684985ba0d885
joshslee <joshslee93@gmail.com>
2019-09-25 10:18:23 -0700

    add modal reducer and actions

--- redux/index.js
+++ redux/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import paperReducer from "./paper";
+import modals from "./modals";
 
 export default combineReducers({
+  modals,
   paper: paperReducer,
 });
