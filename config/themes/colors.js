6a021c6a8a257a76bbecbe39f0c939cac7517f6b
joshslee <joshslee93@gmail.com>
2019-09-25 10:12:38 -0700

    update values of lighter colors

--- config/themes/colors.js
+++ config/themes/colors.js
@@ -1,11 +1,13 @@
 const colors = {
-  BLUE: opacity => `rgba(78, 83, 255, ${opacity})`,
-  PURPLE: opacity => `rgba(36, 31, 58, ${opacity})`,
-  GREY: opacity => `rgba(193, 193, 207, ${opacity})`,
-  LIGHT_GREY: opacity => `rgba(242, 242, 246, ${opacity})`,
-  GREEN: opacity => `rgba(30, 207, 49, ${opacity})`,
-  LIGHT_GREEN: opacity => this.GREEN(0.1),
-  LIGHT_BLUE: opacity => this.BLUE(0.1),
-  ORANGE: opacity => `rgba(255, 109, 0, ${opacity})`,
-  RED: opacity => `rgba(255, 45, 45, ${opacity})`
+  BLUE: (opacity = 1) => `rgba(78, 83, 255, ${opacity})`,
+  PURPLE: (opacity = 1) => `rgba(36, 31, 58, ${opacity})`,
+  GREY: (opacity = 1) => `rgba(193, 193, 207, ${opacity})`,
+  LIGHT_GREY: (opacity = 1) => `rgba(242, 242, 246, ${opacity})`,
+  GREEN: (opacity = 1) => `rgba(30, 207, 49, ${opacity})`,
+  LIGHT_GREEN: (opacity = 1) => `rgba(236, 249, 235, ${opacity})`,
+  LIGHT_BLUE: (opacity = 1) => `rgba(237, 238, 254, ${opacity})`,
+  ORANGE: (opacity = 1) => `rgba(255, 109, 0, ${opacity})`,
+  RED: (opacity = 1) => `rgba(255, 45, 45, ${opacity})`,
 };
+
+export default colors;
