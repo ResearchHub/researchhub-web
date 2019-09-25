d1e1b2039c71c6dc40d2b7f2d6bd227ba6ae7363
valmack <valmack@protonmail.com>
2019-09-25 11:56:53 -0400

    Create links to route paper tabs

---
+++ components/paper-tabs.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { StyleSheet, css } from "aphrodite";
+
+const PaperTabs = (props) => {
+  const selectedTab = props.selectedTab;
+  return (
+    <nav>
+      <ul>{tabs.map((link) => renderTabs(link, selectedTab))}</ul>
+      <style jsx>{``}</style>
+    </nav>
+  );
+};
+
+const tabs = [
+  { href: "summary", label: "summary" },
+  { href: "discussion", label: "discussion" },
+  { href: "full", label: "full paper" },
+  { href: "citations", label: "citations" },
+].map(formatTabs);
+
+function formatTabs(tab) {
+  tab.key = `nav-link-${tab.href}-${tab.label}`;
+  return tab;
+}
+
+function renderTabs({ key, href, label }, selected) {
+  let classNames = [];
+
+  if (href === selected) {
+    classNames.push(styles.selected);
+  }
+
+  return (
+    <li key={key} className={css(classNames)}>
+      <Link href={href}>
+        <a>{label}</a>
+      </Link>
+    </li>
+  );
+}
+
+const styles = StyleSheet.create({
+  selected: {
+    backgroundColor: "yellow",
+  },
+});
+
+export default PaperTabs;
--- pages/paper/[paperId]/[tabName]/index.js
+++ pages/paper/[paperId]/[tabName]/index.js
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
+import PaperTabs from "../../../../components/paper-tabs";
 
 const Paper = () => {
   const router = useRouter();
   const { paperId, tabName } = router.query;
 
+  // TODO: Display different tab content based on tabName
   return (
     <div>
       <div>Paper: {paperId}</div>
+      <PaperTabs baseUrl={paperId} selectedTab={tabName} />
       <div>Tab: {tabName}</div>
     </div>
   );
