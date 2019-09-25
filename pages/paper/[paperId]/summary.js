77eea004f1a72ac274b08a85c69e56b7b83c3533
Craig Lu <craig@quantfive.org>
2019-09-25 09:59:13 -0700

    added fontawesome library

--- pages/paper/[paperId]/summary.js
+++ pages/paper/[paperId]/summary.js
@@ -2,9 +2,10 @@ import Link from "next/link";
 import Router, { withRouter } from "next/router";
 import { StyleSheet, css } from "aphrodite";
 import { EditorState, convertFromRaw } from "draft-js";
+import dynamic from "next/dynamic";
 
 // Components
-import dynamic from "next/dynamic";
+import SummaryTab from "../../../components/Paper/Tabs/SummaryTab";
 
 // Config
 import API from "../../../config/api";
@@ -16,66 +17,11 @@ const DraftEditor = dynamic(
 );
 
 class Summary extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      readOnly: true,
-      editorState: EditorState.createEmpty()
-    };
-  }
-
-  onEditorStateChange = editorState => {
-    this.setState({ editorState });
-  };
-
-  save = ({ raw }) => {
-    let param = { summary: raw, paper: this.props.paperId };
-    fetch(API.SUMMARY(), API.POST_CONFIG(param))
-      .then(Helpers.checkStatus)
-      .then(Helpers.parseJSON)
-      .then(resp => {});
-  };
-
-  getSummary = () => {
-    let { query } = this.props.router;
-    fetch(API.SUMMARY({ summaryId: query.paperId }), API.GET_CONFIG())
-      .then(Helpers.checkStatus)
-      .then(Helpers.parseJSON)
-      .then(resp => {
-        let contentState = convertFromRaw(resp.summary);
-        let editorState = EditorState.createWithContent(contentState);
-        this.setState({
-          editorState
-        });
-      });
-  };
-
-  edit = () => {
-    this.setState({
-      readOnly: false
-    });
-  };
-
-  componentDidMount = () => {
-    this.getSummary();
-  };
   render() {
     let { query } = this.props.router;
     return (
       <div className={css(styles.container)}>
-        <div className={css(styles.summaryActions)}>
-          <div className={css(styles.button)} onClick={() => this.edit()}>
-            edit
-          </div>
-        </div>
-        <DraftEditor
-          paperId={query.paperId}
-          readOnly={this.state.readOnly}
-          editorState={this.state.editorState}
-          onEditorStateChange={this.onEditorStateChange}
-          save={this.save}
-        />
+        <SummaryTab />
       </div>
     );
   }
