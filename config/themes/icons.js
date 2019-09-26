6e926a85df32aef2179dfd67cc63db2d0b43f716
valmack <valmack@protonmail.com>
2019-09-26 15:43:14 -0400

    Add chat and share icons

--- config/themes/icons.js
+++ config/themes/icons.js
@@ -1,4 +1,11 @@
+const icons = {
+  chat: <i className="fas fa-comment"></i>,
+  share: <i className="fas fa-share"></i>,
+};
+
 export const voteWidgetIcons = {
   upvote: <i className="fas fa-caret-up"></i>,
   downvote: <i className="fas fa-caret-down"></i>,
 };
+
+export default icons;
