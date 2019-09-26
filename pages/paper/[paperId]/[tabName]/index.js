3a5e4bddca0bf8ff810241676bac27fd2a0ad85b
valmack <valmack@protonmail.com>
2019-09-26 11:12:15 -0400

    Style tab content container

--- pages/paper/[paperId]/[tabName]/index.js
+++ pages/paper/[paperId]/[tabName]/index.js
@@ -3,11 +3,13 @@ import { useRouter } from "next/router";
 import PaperTabs from "~/components/paper-tabs";
 import SummaryTab from "~/components/Paper/Tabs/SummaryTab";
 import PaperTab from "~/components/Paper/Tabs/PaperTab";
+import { css, StyleSheet } from "aphrodite";
+
 const Paper = () => {
   const router = useRouter();
   const { paperId, tabName } = router.query;
 
-  function renderTab() {
+  function renderTabContent() {
     switch (tabName) {
       case "summary":
         return <SummaryTab paperId={paperId} />;
@@ -20,14 +22,23 @@ const Paper = () => {
     }
   }
 
-  // TODO: Display different tab content based on tabName
   return (
     <div>
       <div>Paper: {paperId}</div>
       <PaperTabs baseUrl={paperId} selectedTab={tabName} />
-      {renderTab()}
+      <div className={css(styles.contentContainer)}>{renderTabContent()}</div>
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    width: "70%",
+    padding: "30px 0px",
+    margin: "auto",
+    display: "flex",
+    justifyContent: "center",
+  },
+});
 
 export default Paper;
