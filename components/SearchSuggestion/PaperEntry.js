378afd1c9f83b43b7260848773ba55f1e8a26b76
joshslee <joshslee93@gmail.com>
2019-09-26 09:29:31 -0700

    paper entry suggestion component

---
+++ components/SearchSuggestion/PaperEntry.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
+const PaperEntry = ({ title, date }) => {
+  return (
+    <div className={css(styles.entry)}>
+      <div className={css(styles.title, styles.text)}>{title && title}</div>
+      <div className={css(styles.date, styles.text)}>{date && date}</div>
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  entry: {
+    width: "calc(100% - 32px)",
+    height: 58,
+    padding: 16,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    backgroundColor: "#F7F7FB",
+    textAlign: "left",
+    cursor: "pointer",
+    marginBottom: 10,
+    border: "solid 1px #F7F7FB",
+    ":hover": {
+      border: "solid 1px #D2D2E6",
+    },
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 18,
+    color: "#241F3A",
+  },
+  date: {
+    fontWeight: 400,
+    fontSize: 14,
+    color: "#8c8b9a",
+  },
+  text: {
+    fontFamily: "Roboto",
+  },
+});
+
+export default PaperEntry;
