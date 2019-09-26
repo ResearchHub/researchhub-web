011ef836d66e983ed3db5cdb1c0e5d3d63b14abc
valmack <valmack@protonmail.com>
2019-09-26 13:25:09 -0400

    Style discussion thread card

--- components/DiscussionThreadCard.js
+++ components/DiscussionThreadCard.js
@@ -1,16 +1,30 @@
+import { css, StyleSheet } from "aphrodite";
 import VoteWidget from "./VoteWidget";
 
 const DiscussionThreadCard = () => {
   return (
-    <div>
+    <div className={css(styles.container)}>
       <VoteWidget score={5} fontSize={16} width={"44px"} />
-      <div>User component</div>
-      <div>Thread title</div>
-      <div>Number of comments</div>
-      <div>share button</div>
+      <div className={css(styles.infoContainer)}>
+        <div>User component</div>
+        <div>Thread title</div>
+        <div>Number of comments</div>
+        <div>share button</div>
+      </div>
       <div>read button</div>
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  infoContainer: {
+    width: "100%",
+  },
+});
 
 export default DiscussionThreadCard;
