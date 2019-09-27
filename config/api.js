1e5e3886b7c3fd0eee09b573e11ced907592de97
Craig Lu <craig@quantfive.org>
2019-09-26 18:13:44 -0700

    grab paper information

--- config/api.js
+++ config/api.js
@@ -3,11 +3,11 @@ import { API } from "@quantfive/js-web-config";
 const apiRoot = {
   production: "localhost:8000",
   staging: "localhost:8000",
-  dev: "localhost:8000"
+  dev: "localhost:8000",
   //dev: 'https://staging.solestage.com/api/',
 };
 
-const routes = BASE_URL => {
+const routes = (BASE_URL) => {
   return {
     SUMMARY: ({ summaryId }) => {
       let url = BASE_URL + `summary/`;
@@ -19,12 +19,24 @@ const routes = BASE_URL => {
       }
 
       return url;
-    }
+    },
+
+    PAPER: ({ paperId }) => {
+      let url = BASE_URL + `paper/`;
+
+      if (paperId) {
+        url += `${paperId}/?`;
+      } else {
+        url += "?";
+      }
+
+      return url;
+    },
   };
 };
 
 export default API({
   authTokenName: "researchhub_token",
   apiRoot,
-  routes
+  routes,
 });
