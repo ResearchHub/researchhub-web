d33293738f7fad2fdfbbca93067e7e1e17f165ab
valmack <valmack@protonmail.com>
2019-09-27 11:53:51 -0400

    Start discussion thread page

---
+++ pages/paper/[paperId]/[tabName]/[discussionThreadId].js
@@ -0,0 +1,52 @@
+import { css, StyleSheet } from "aphrodite";
+
+import VoteWidget from "~/components/VoteWidget";
+
+const DiscussionThreadPage = () => {
+  return (
+    <div>
+      <Thread
+        title={"This is the thread title"}
+        body={"This is the body of the thread"}
+      />
+      <Comments />
+    </div>
+  );
+};
+
+const Thread = (props) => {
+  const { title, body } = props;
+  return (
+    <div>
+      <div className={css(styles.threadTopContainer)}>
+        <VoteWidget score={123} fontSize={"16px"} width={"58px"} />
+        <div>{title}</div>
+        <ShareButton />
+      </div>
+      <div className={css(styles.threadBodyContainer)}>
+        <div>{body}</div>
+      </div>
+    </div>
+  );
+};
+
+const Comments = () => {
+  return <div>Comments</div>;
+};
+
+const ShareButton = () => {
+  return <div>share</div>;
+};
+
+const styles = StyleSheet.create({
+  threadTopContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  threadBodyContainer: {
+    marginLeft: "58px",
+  },
+});
+
+export default DiscussionThreadPage;
