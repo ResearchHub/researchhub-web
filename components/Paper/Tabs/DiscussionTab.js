aab81d8cea903015da945079c0ff618246dd47fe
valmack <valmack@protonmail.com>
2019-09-27 12:24:11 -0400

    Format thread discussion paths with trailing slash

--- components/Paper/Tabs/DiscussionTab.js
+++ components/Paper/Tabs/DiscussionTab.js
@@ -1,15 +1,23 @@
 import { useRouter } from "next/router";
 
 import DiscussionThreadCard from "~/components/DiscussionThreadCard";
+import { endsWithSlash } from "~/config/utils/routing";
 
 const DiscussionTab = () => {
   const router = useRouter();
-  const basePath = router.asPath;
+  const basePath = formatBasePath(router.asPath);
 
-  const threads = [{ key: "key", data: "data", path: basePath + "/" + "1" }];
+  const threads = [{ key: "key", data: "data", path: basePath + "1" }];
 
   return <div>{renderThreads(threads)}</div>;
 };
+
+function formatBasePath(path) {
+  if (endsWithSlash(path)) {
+    return path;
+  }
+  return path + "/";
+}
 
 function renderThreads(threads) {
   return threads.map((t, i) => {
