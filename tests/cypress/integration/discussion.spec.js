1db4ec41ec0526c64424821aad1d1ea62ffcb018
valmack <valmack@protonmail.com>
2019-09-30 13:48:09 -0400

    Add test for ensure discussion thread card links to discussion page

---
+++ tests/cypress/integration/discussion.spec.js
@@ -0,0 +1,15 @@
+describe("Discussion Thread Card", function() {
+  it("navigates to the discussion page", function() {
+    let path = "/paper/1/discussion";
+    cy.visit(path);
+
+    cy.get("a").contains("Read").as("button");
+
+    cy.get("@button")
+      .should("have.attr", "href")
+      .then((href) => {
+        cy.get("@button").click();
+        cy.url().should("include", href);
+      });
+  });
+});
