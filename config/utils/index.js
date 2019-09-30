a8fcd6b81172266dac306a0032d47d4def2065d0
valmack <valmack@protonmail.com>
2019-09-30 17:08:20 -0400

    Add function to get nested value from object

--- config/utils/index.js
+++ config/utils/index.js
@@ -1,6 +1,24 @@
 export * from "./dates";
 export * from "./routing";
 
+export function getNestedValue(root, nodes, defaultValue = null) {
+  const initialValue = root;
+
+  if (doesNotExist(initialValue)) {
+    return defaultValue;
+  }
+
+  const value = nodes.slice(0).reduce((acc, curr, i, arr) => {
+    if (doesNotExist(acc[curr])) {
+      arr.splice(1);
+      return defaultValue;
+    }
+    return acc[curr];
+  }, initialValue);
+
+  return value;
+}
+
 export function doesNotExist(value) {
   if (value === undefined || value === null) {
     return true;
