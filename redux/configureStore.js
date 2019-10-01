e8ded9bd38caedfda0aca6e14d0dc226b5fd5c41
valmack <valmack@protonmail.com>
2019-10-01 14:25:43 -0400

    Add redux logger to log in development

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,13 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import reducer from "./index";
 
 export function configureStore(initialState = {}) {
+  const middleware = [thunkMiddleware];
+
+  if (process.env.NODE_ENV === "development") {
+    middleware.push(logger); // Logger must be the last item in middleware
+  }
+
   let store = createStore(
     reducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
+    composeWithDevTools(applyMiddleware(...middleware))
   );
   return store;
 }
