539a97c215fd554406bdd4bb4ed668b64ce57b25
valmack <valmack@protonmail.com>
2019-10-01 15:12:23 -0400

    Use redux to get data on discussion thread page

--- redux/discussion/actions.js
+++ redux/discussion/actions.js
@@ -1,15 +1,39 @@
 import * as types from "./types";
 
+export function setThreadPending() {
+  return {
+    type: types.FETCH_THREAD_PENDING,
+    payload: { doneFetching: false },
+  };
+}
 export function setThread(res) {
-  return { type: types.FETCH_THREAD_SUCCESS, payload: res };
+  return {
+    type: types.FETCH_THREAD_SUCCESS,
+    payload: { doneFetching: true, ...res },
+  };
 }
 export function setThreadFailure() {
-  return { type: types.FETCH_THREAD_FAILURE };
+  return {
+    type: types.FETCH_THREAD_FAILURE,
+    payload: { doneFetching: true },
+  };
 }
 
+export function setCommentsPending() {
+  return {
+    type: types.FETCH_COMMENTS_PENDING,
+    payload: { doneFetching: false },
+  };
+}
 export function setComments(res) {
-  return { type: types.FETCH_COMMENTS_SUCCESS, payload: res };
+  return {
+    type: types.FETCH_COMMENTS_SUCCESS,
+    payload: { doneFetching: true, ...res },
+  };
 }
 export function setCommentsFailure() {
-  return { type: types.FETCH_COMMENTS_FAILURE };
+  return {
+    type: types.FETCH_COMMENTS_FAILURE,
+    payload: { doneFetching: true },
+  };
 }
--- redux/discussion/reducer.js
+++ redux/discussion/reducer.js
@@ -9,8 +9,16 @@ export const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case types.FETCH_THREAD_PENDING:
+      return {
+        ...state,
+        ...action.payload,
+      };
     case types.FETCH_THREAD_FAILURE:
-      return state;
+      return {
+        ...state,
+        ...action.payload,
+      };
     case types.FETCH_THREAD_SUCCESS:
       return {
         ...action.payload,
