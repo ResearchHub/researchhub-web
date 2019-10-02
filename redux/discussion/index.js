5e2a666381b03c39ca1c7a25b2df837475035e75
valmack <valmack@protonmail.com>
2019-10-02 12:14:02 -0400

    Refactor discussion action wrappers

--- redux/discussion/index.js
+++ redux/discussion/index.js
@@ -3,22 +3,26 @@ import * as shims from "./shims";
 import API from "~/config/api";
 import { logFetchError } from "~/config/utils";
 
+const FETCH_ERROR_MESSAGE = "Fetch error caught in promise";
+
 export function fetchThread(paperId, threadId) {
   return async (dispatch) => {
     const response = await fetch(
       API.THREAD(paperId, threadId),
       API.GET_CONFIG()
-    ).catch((err) => console.log("Fetch error caught in promise", err));
+    ).catch(handleCatch);
 
-    if (!response.ok) {
-      logFetchError(response);
-      return dispatch(actions.setThreadFailure());
-    }
+    const successDispatch = async () => {
+      const body = await response.json();
+      const thread = shims.thread(body);
+      return dispatch(actions.setThread(thread));
+    };
 
-    const body = await response.json();
-    const thread = shims.thread(body);
-
-    return dispatch(actions.setThread(thread));
+    return dispatchResult(
+      response,
+      dispatch(actions.setThreadFailure()),
+      successDispatch()
+    );
   };
 }
 
@@ -27,19 +31,39 @@ export function fetchComments(paperId, threadId, page) {
     const response = await fetch(
       API.THREAD_COMMENT(paperId, threadId, page),
       API.GET_CONFIG()
-    ).catch((err) => console.log("Fetch error caught in promise", err));
+    ).catch(handleCatch);
 
-    if (!response.ok) {
-      logFetchError(response);
-      return dispatch(actions.setCommentsFailure());
-    }
+    const successDispatch = async () => {
+      const body = await response.json();
+      const comments = shims.comments(body);
+      comments.page = page;
 
-    const body = await response.json();
-    const comments = shims.comments(body);
-    comments.page = page;
+      return dispatch(actions.setComments(comments));
+    };
 
-    return dispatch(actions.setComments(comments));
+    return dispatchResult(
+      response,
+      dispatch(actions.setCommentsFailure()),
+      successDispatch()
+    );
   };
+}
+
+
+  };
+}
+
+function handleCatch(err) {
+  console.log(FETCH_ERROR_MESSAGE, err);
+  return err;
+}
+
+function dispatchResult(response, failure, success) {
+  if (!response.ok) {
+    logFetchError(response);
+    return failure;
+  }
+  return success;
 }
 
 const DiscussionActions = {
