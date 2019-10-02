7a93c8430d4dabfa44a9e70e92ca467185ec3684
valmack <valmack@protonmail.com>
2019-10-02 12:04:53 -0400

    Add success state to redux discussion actions

--- redux/discussion/actions.js
+++ redux/discussion/actions.js
@@ -9,13 +9,13 @@ export function setThreadPending() {
 export function setThread(res) {
   return {
     type: types.FETCH_THREAD_SUCCESS,
-    payload: { doneFetching: true, ...res },
+    payload: { ...res, doneFetching: true, success: true },
   };
 }
 export function setThreadFailure() {
   return {
     type: types.FETCH_THREAD_FAILURE,
-    payload: { doneFetching: true },
+    payload: { doneFetching: true, success: false },
   };
 }
 
@@ -28,12 +28,12 @@ export function setCommentsPending() {
 export function setComments(res) {
   return {
     type: types.FETCH_COMMENTS_SUCCESS,
-    payload: { doneFetching: true, ...res },
+    payload: { ...res, doneFetching: true, success: true },
   };
 }
 export function setCommentsFailure() {
   return {
     type: types.FETCH_COMMENTS_FAILURE,
-    payload: { doneFetching: true },
+    payload: { doneFetching: true, success: false },
   };
 }
