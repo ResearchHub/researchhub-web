3ef447e2c542e045f67e8ea5784f2f88d1b4cd2f
joshslee <joshslee93@gmail.com>
2019-10-03 11:36:39 -0700

    add formTextArea componenent

---
+++ components/Form/FormTextArea.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import TextareaAutosize from "react-autosize-textarea";
+
+// Config
+import colors from "../../config/themes/colors";
+
+const FormTextArea = (props) => {
+  let {
+    id,
+    label,
+    placeholder,
+    required,
+    containerStyle,
+    labelStyle,
+    inputStyle,
+  } = props;
+
+  return (
+    <div
+      id={id}
+      className={css(styles.inputContainer, containerStyle && containerStyle)}
+    >
+      <div
+        className={css(
+          styles.inputLabel,
+          labelStyle && labelStyle,
+          styles.text,
+          !label && styles.hide
+        )}
+      >
+        {label && label}
+        {required && <div className={css(styles.asterick)}>*</div>}
+      </div>
+      <TextareaAutosize
+        required={required && required}
+        placeholder={placeholder && placeholder}
+        style={inputStyle && inputStyle}
+      />
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    height: 75,
+    width: 525,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "space-between",
+    marginTop: 20,
+    marginBottom: 20,
+    position: "relative",
+  },
+  inputLabel: {
+    height: 19,
+    fontWeight: "500",
+    marginBottom: 10,
+    color: "#232038",
+    display: "flex",
+    justifyContent: "flex-start",
+  },
+  input: {
+    height: 50,
+    width: 495,
+    display: "flex",
+    alignItems: "center",
+    border: "1px solid #E8E8F2",
+    backgroundColor: "#FBFBFD",
+    padding: 15,
+    fontWeight: "400",
+    borderRadius: 2,
+    color: "#232038",
+    highlight: "none",
+    outline: "none",
+    ":focus": {
+      borderColor: "#D2D2E6",
+    },
+  },
+  asterick: {
+    color: colors.BLUE(1),
+  },
+  placeholder: {
+    color: "#8e8d9a",
+    fontWeight: 400,
+  },
+  text: {
+    fontFamily: "Roboto",
+    fontSize: 16,
+  },
+});
+
+export default FormTextArea;
