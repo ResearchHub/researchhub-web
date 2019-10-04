bd729a17d91114d0bc2d6d32fc333f8e9a956e46
valmack <valmack@protonmail.com>
2019-10-04 10:05:01 -0400

    Update discussion test to only run if there are threads

--- tests/cypress/integration/discussion.spec.js
+++ tests/cypress/integration/discussion.spec.js
@@ -1,16 +1,20 @@
 describe("Discussion Thread Card", function() {
-  // TODO: Only test this is we know we have threads
-  it.skip("navigates to the discussion page", function() {
+  it("navigates to the discussion page", function() {
     let path = "/paper/1/discussion";
     cy.visit(path);
 
-    cy.get("a").contains("Read").as("button");
+    cy.get("#discussions_ui").as("count").invoke("text").then((count) => {
+      if (count) {
+        cy.get("a").contains("Read").as("button");
 
-    cy.get("@button")
-      .should("have.attr", "href")
-      .then((href) => {
-        cy.get("@button").click();
-        cy.url().should("include", href);
-      });
+        cy.get("@button")
+          .should("have.attr", "href")
+          .then((href) => {
+            cy.get("@button").click();
+            cy.url().should("include", href);
+          });
+      }
+    });
+
   });
 });
