5583506c827adb2a528e6cca65d8c4e0ff8f16ef
Craig Lu <craig@quantfive.org>
2019-10-03 19:49:23 -0700

    stylized header

--- pages/paper/[paperId]/[tabName]/index.js
+++ pages/paper/[paperId]/[tabName]/index.js
@@ -10,10 +10,15 @@ import DiscussionTab from "~/components/Paper/Tabs/DiscussionTab";
 import SummaryTab from "~/components/Paper/Tabs/SummaryTab";
 import PaperTab from "~/components/Paper/Tabs/PaperTab";
 import ComponentWrapper from "~/components/ComponentWrapper";
+import VoteWidget from "~/components/VoteWidget";
+import ActionButton from "~/components/ActionButton";
 
 import { PaperActions } from "~/redux/paper";
 
 import { getNestedValue } from "~/config/utils";
+
+// Config
+import colors from "~/config/themes/colors";
 
 const Paper = (props) => {
   const router = useRouter();
@@ -54,12 +59,37 @@ const Paper = (props) => {
     return authors;
   }
 
+  function renderHubs() {
+    let hubs =
+      paper &&
+      paper.hubs.map((hub, index) => {
+        return (
+          <div className={css(styles.hubTag)}>{hub.name.toUpperCase()}</div>
+        );
+      });
+    return hubs;
+  }
+
   return (
     <div className={css(styles.container)}>
       <ComponentWrapper>
         <div className={css(styles.header)}>
-          <div className={css(styles.title)}>{paper && paper.title}</div>
-          <div className={css(styles.authors)}>{renderAuthors()}</div>
+          <div className={css(styles.voting)}>
+            <VoteWidget />
+          </div>
+          <div className={css(styles.topHeader)}>
+            <div className={css(styles.title)}>{paper && paper.title}</div>
+            <div className={css(styles.actionButtons)}>
+              <ActionButton icon={"fas fa-pencil"} action={null} />
+              <ActionButton icon={"fas fa-share-alt"} action={null} />
+              <ActionButton icon={"fas fa-bookmark"} action={null} />
+            </div>
+          </div>
+          <div className={css(styles.tags)}>
+            <div className={css(styles.authors)}>{renderAuthors()}</div>
+            <div className={css(styles.hubs)}>{renderHubs()}</div>
+          </div>
+          <div className={css(styles.tagline)}>{paper && paper.tagline}</div>
           <div className={css(styles.infoSection)}>
             {paper.paper_publish_date && (
               <div className={css(styles.info)}>
@@ -104,10 +134,12 @@ const styles = StyleSheet.create({
     padding: "30px 0px",
     width: "100%",
     boxSizing: "border-box",
+    position: "relative",
   },
   title: {
     fontSize: 33,
     marginBottom: 10,
+    position: "relative",
   },
   infoSection: {
     display: "flex",
@@ -118,11 +150,47 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   authors: {
-    marginBottom: 10,
     display: "flex",
+    marginRight: 15,
   },
   authorContainer: {
     marginRight: 5,
+  },
+  tags: {
+    display: "flex",
+    marginBottom: 10,
+  },
+  hubTag: {
+    fontSize: 10,
+    fontWeight: "bold",
+    color: colors.BLUE(1),
+    background: colors.BLUE(0.1),
+    padding: 5,
+    margin: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 13,
+    borderRadius: 4,
+  },
+  hubs: {
+    display: "flex",
+  },
+  tagline: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  voting: {
+    position: "absolute",
+    width: 70,
+    left: -80,
+    top: 15,
+  },
+  actionButtons: {
+    display: "flex",
+  },
+  topHeader: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 });
 
