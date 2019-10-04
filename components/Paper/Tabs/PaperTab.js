1407f98a98d264276b8d3eebe789958cb6295ff3
Craig Lu <craig@quantfive.org>
2019-10-04 15:08:47 -0700

    adding navigation to pdg

--- components/Paper/Tabs/PaperTab.js
+++ components/Paper/Tabs/PaperTab.js
@@ -14,19 +14,45 @@ function PaperTab(props) {
   const { paperUrl } = props;
   const [pageNumber, setPageNumber] = useState(0);
   const [numPages, setNumPages] = useState(0);
-
   function onLoadSuccess({ numPages }) {
     setNumPages(numPages);
     setPageNumber(1);
   }
 
+  function nextPage() {
+    if (pageNumber >= numPages) {
+      return;
+    }
+    setPageNumber(pageNumber + 1);
+  }
+
+  function prevPage() {
+    if (pageNumber <= 1) {
+      return;
+    }
+    setPageNumber(pageNumber - 1);
+  }
   return (
     <div className={css(styles.container)}>
       <Document file={paperUrl} onLoadSuccess={onLoadSuccess}>
         <Page pageNumber={pageNumber} width={800} />
       </Document>
-      <div>
+      <div className={css(styles.pageNavigator)}>
+        <i
+          className={
+            css(styles.icon, pageNumber <= 1 && styles.disable) +
+            " fas fa-caret-left"
+          }
+          onClick={prevPage}
+        ></i>
         Page {pageNumber} of {numPages}
+        <i
+          className={
+            css(styles.icon, pageNumber >= numPages && styles.disable) +
+            " fas fa-caret-right"
+          }
+          onClick={nextPage}
+        ></i>
       </div>
     </div>
   );
@@ -40,6 +66,19 @@ var styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     boxSizing: "border-box",
+  },
+  pageNavigator: {
+    display: "flex",
+    alignItems: "center",
+  },
+  icon: {
+    marginLeft: 10,
+    marginRight: 10,
+    cursor: "pointer",
+    fontSize: 20,
+  },
+  disable: {
+    opacity: 0.3,
   },
 });
 
