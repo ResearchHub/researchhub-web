41161504c5ca22afa4735ceb2795b809806c9952
valmack <valmack@protonmail.com>
2019-10-08 14:29:14 -0400

    Refactor discusson comment and reply box

---
+++ components/DiscussionCommentBox.js
@@ -0,0 +1,65 @@
+import { useRouter } from "next/router";
+import { useDispatch, useStore } from "react-redux";
+
+import TextEditor from "~/components/TextEditor";
+
+import DiscussionActions from "~/redux/discussion";
+
+const DiscussionComment = (props) => {
+  const { postMethod, onSubmit } = props;
+
+  const dispatch = useDispatch();
+  const store = useStore();
+  const router = useRouter();
+  const { paperId, discussionThreadId } = router.query;
+
+  const post = async (text) => {
+    await postMethod(
+      { dispatch, store, paperId, discussionThreadId, onSubmit },
+      text
+    );
+  };
+
+  return (
+    <TextEditor
+      canEdit={true}
+      canSubmit={true}
+      onSubmit={post}
+      commentEditor={true}
+    />
+  );
+};
+
+async function postComment(props, text) {
+  const { dispatch, store, paperId, discussionThreadId, onSubmit } = props;
+
+  dispatch(DiscussionActions.postCommentPending());
+  await dispatch(
+    DiscussionActions.postComment(paperId, discussionThreadId, text)
+  );
+
+  const comment = store.getState().discussion.postedComment;
+  onSubmit(comment);
+}
+
+async function postReply(props, text) {
+  const { dispatch, store, paperId, discussionThreadId, onSubmit } = props;
+
+  dispatch(DiscussionActions.postReplyPending());
+  await dispatch(
+    DiscussionActions.postReply(paperId, discussionThreadId, text)
+  );
+
+  const reply = store.getState().discussion.postedReply;
+  onSubmit(reply);
+}
+
+export const CommentBox = (props) => {
+  const { onSubmit } = props;
+  return <DiscussionComment onSubmit={onSubmit} postMethod={postComment} />;
+};
+export const ReplyBox = (props) => {
+  const { onSubmit } = props;
+
+  return <DiscussionComment onSubmit={onSubmit} postMethod={postReply} />;
+};
