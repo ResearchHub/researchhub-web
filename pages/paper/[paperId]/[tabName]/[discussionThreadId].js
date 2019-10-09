a84b0ef84b14ec73099aa5d3a41cff76c87d326e
valmack <valmack@protonmail.com>
2019-10-09 15:59:53 -0400

    Refactor discussion thread page components

--- pages/paper/[paperId]/[tabName]/[discussionThreadId].js
+++ pages/paper/[paperId]/[tabName]/[discussionThreadId].js
@@ -3,14 +3,16 @@ import { Fragment, useEffect, useState } from "react";
 // NPM Modules
 import { css, StyleSheet } from "aphrodite";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useDispatch, useStore } from "react-redux";
+import Router, { useRouter } from "next/router";
+import { connect, useDispatch, useStore } from "react-redux";
 import { Value } from "slate";
 import Plain from "slate-plain-serializer";
 
 // components
 import DiscussionCard from "~/components/DiscussionCard";
+import { Comment, Reply } from "~/components/DiscussionComment";
 import { CommentBox, ReplyBox } from "~/components/DiscussionCommentBox";
+import Thread from "~/components/DiscussionPageThread";
 import DiscussionPostMetadata from "~/components/DiscussionPostMetadata";
 import TextEditor from "~/components/TextEditor";
 import VoteWidget from "~/components/VoteWidget";
@@ -21,7 +23,7 @@ import DiscussionActions from "~/redux/discussion";
 // Utils
 import colors, { discussionPageColors } from "~/config/themes/colors";
 import icons from "~/config/themes/icons";
-import { doesNotExist, isEmpty } from "~/config/utils";
+import { createUsername, doesNotExist, isEmpty } from "~/config/utils";
 
 const DiscussionThreadPage = (props) => {
   const { discussion } = props;
@@ -113,183 +115,6 @@ DiscussionThreadPage.getInitialProps = async ({ isServer, store, query }) => {
 
   return { discussion };
 };
-
-const BackButton = () => {
-  const message = "Go back to all discussions";
-  const router = useRouter();
-  const url = getBackUrl(router.asPath);
-
-  function getBackUrl(url) {
-    let parts = url.split("/");
-    parts.pop();
-    parts = parts.join("/");
-    return parts;
-  }
-
-  return (
-    <div className={css(styles.backButtonContainer)}>
-      <Link href={"/paper/[paperId]/[tabName]"} as={url}>
-        <a className={css(styles.backButton)}>
-          {icons.longArrowLeft} {message}
-        </a>
-      </Link>
-    </div>
-  );
-};
-
-const Thread = (props) => {
-  const { title, body, username, date } = props;
-
-  return (
-    <div>
-      <BackButton />
-      <DiscussionCard
-        top={
-          <Fragment>
-            <VoteWidget
-              styles={styles.voteWidget}
-              score={123}
-              fontSize={"16px"}
-              width={"58px"}
-            />
-            <div className={css(styles.threadTitle)}>{title}</div>
-            <ShareButton />
-          </Fragment>
-        }
-        info={<div className={css(styles.body)}>{body}</div>}
-        infoStyle={styles.threadInfo}
-        action={<DiscussionPostMetadata username={username} date={date} />}
-      />
-    </div>
-  );
-};
-
-const ShareButton = () => {
-  return <div className={css(styles.shareContainer)}>{icons.share}</div>;
-};
-
-class DiscussionComment extends React.Component {
-  state = {
-    id: this.props.data.id,
-    date: this.props.data.createdDate,
-    text: this.props.data.text,
-    username: createUsername(this.props.data),
-  };
-
-  deserializeComment = (text) => {
-    try {
-      text = Value.fromJSON(JSON.parse(text));
-    } catch (SyntaxError) {
-      text = Plain.deserialize(text);
-    }
-    return text;
-  };
-
-  renderTop = () => {
-    return (
-      <Fragment>
-        <VoteWidget score={0} />
-        <DiscussionPostMetadata
-          username={this.state.username}
-          date={this.state.date}
-        />
-      </Fragment>
-    );
-  };
-
-  renderInfo = () => {
-    const text = this.deserializeComment(this.state.text);
-    return <TextEditor readOnly={true} canEdit={false} initialValue={text} />;
-  };
-
-  render() {
-    const action = this.renderAction ? this.renderAction() : null;
-    const replies = this.renderReplies ? this.renderReplies() : null;
-
-    return (
-      <Fragment>
-        <DiscussionCard
-          top={this.renderTop()}
-          info={this.renderInfo()}
-          infoStyle={this.props.infoStyle}
-          action={action}
-        />
-        {replies}
-      </Fragment>
-    );
-  }
-}
-
-class Comment extends DiscussionComment {
-  constructor(props) {
-    super(props);
-    this.state.showReplyBox = false;
-    this.state.replies = [];
-  }
-
-  renderAction = () => {
-    return (
-      <div className={css(styles.actionBar)}>
-        {!this.state.showReplyBox
-          ? this.renderReplyButton()
-          : this.renderReplyBox()}
-      </div>
-    );
-  };
-
-  renderReplyButton = () => {
-    return (
-      <div className={css(styles.reply)} onClick={this.showReplyBox}>
-        Reply
-      </div>
-    );
-  };
-
-  showReplyBox = () => {
-    this.setState({ showReplyBox: true });
-  };
-
-  renderReplyBox = () => {
-    return (
-      <ReplyBox onSubmit={this.addSubmittedReply} commentId={this.state.id} />
-    );
-  };
-
-  addSubmittedReply = (reply) => {
-    if (!doesNotExist(reply)) {
-      let newReplies = [reply];
-      newReplies = newReplies.concat(this.state.replies);
-      console.log(newReplies);
-      this.setState({ replies: newReplies });
-    }
-  };
-
-  renderReplies = () => {
-    return this.state.replies.map((r, i) => {
-      let divider = <div className={css(styles.divider)} />;
-      if (i === 0) {
-        divider = null;
-      }
-      return (
-        <Fragment key={r.id}>
-          {divider}
-          <Reply key={r.id} data={r} />
-        </Fragment>
-      );
-    });
-  };
-}
-
-class Reply extends DiscussionComment {
-  constructor(props) {
-    super(props);
-  }
-}
-
-function createUsername({ createdBy }) {
-  const { firstName, lastName } = createdBy;
-  return `${firstName} ${lastName}`;
-}
 
 const styles = StyleSheet.create({
   backButtonContainer: {
