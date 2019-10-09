fa900c228edd9a1fe4d6e5f64ff799ae31c8756e
valmack <valmack@protonmail.com>
2019-10-09 15:59:53 -0400

    Refactor discussion thread page components

---
+++ components/DiscussionComment.js
@@ -0,0 +1,236 @@
+import { Fragment } from "react";
+
+import { css, StyleSheet } from "aphrodite";
+import Router from "next/router";
+import { connect } from "react-redux";
+import { Value } from "slate";
+import Plain from "slate-plain-serializer";
+
+import DiscussionCard from "~/components/DiscussionCard";
+import { ReplyBox } from "~/components/DiscussionCommentBox";
+import DiscussionPostMetadata from "~/components/DiscussionPostMetadata";
+import TextEditor from "~/components/TextEditor";
+import VoteWidget from "~/components/VoteWidget";
+
+import DiscussionActions from "~/redux/discussion";
+
+import colors, { discussionPageColors } from "~/config/themes/colors";
+import { createUsername, getNestedValue, doesNotExist } from "~/config/utils";
+import { UPVOTE, DOWNVOTE } from "../config/constants";
+
+class DiscussionComment extends React.Component {
+  state = {
+    id: this.props.data.id,
+    date: this.props.data.createdDate,
+    text: this.props.data.text,
+    selectedVoteType: this.props.data.userVote.voteType,
+    score: this.props.data.score,
+    username: createUsername(this.props.data),
+  };
+
+  deserializeComment = (text) => {
+    try {
+      text = Value.fromJSON(JSON.parse(text));
+    } catch (SyntaxError) {
+      text = Plain.deserialize(text);
+    }
+    return text;
+  };
+
+  upvote = async () => {
+    const { paperId, discussionThreadId } = Router.query;
+
+    this.props.dispatch(DiscussionActions.postUpvotePending());
+    await this.props.dispatch(
+      DiscussionActions.postUpvote(paperId, discussionThreadId, this.state.id)
+    );
+
+    this.updateWidgetUI(this.props.voteResult);
+  };
+
+  downvote = async () => {
+    const { paperId, discussionThreadId } = Router.query;
+
+    this.props.dispatch(DiscussionActions.postDownvotePending());
+    await this.props.dispatch(
+      DiscussionActions.postDownvote(paperId, discussionThreadId, this.state.id)
+    );
+
+    this.updateWidgetUI(this.props.voteResult);
+  };
+
+  updateWidgetUI = (voteResult) => {
+    const vote = getNestedValue(voteResult, ["vote"], false);
+    if (vote) {
+      console.log(vote);
+      const voteType = vote.voteType;
+      console.log(voteType);
+      if (voteType === UPVOTE) {
+        console.log("upvote");
+        this.setState({ selectedVoteType: UPVOTE });
+      } else if (voteType === DOWNVOTE) {
+        console.log("downvote");
+        this.setState({ selectedVoteType: DOWNVOTE });
+      }
+    }
+  };
+
+  renderTop = () => {
+    return (
+      <Fragment>
+        <VoteWidget
+          score={this.state.score}
+          onUpvote={this.upvote}
+          onDownvote={this.downvote}
+          selected={this.state.selectedVoteType}
+        />
+        <DiscussionPostMetadata
+          username={this.state.username}
+          date={this.state.date}
+        />
+      </Fragment>
+    );
+  };
+
+  renderInfo = () => {
+    const text = this.deserializeComment(this.state.text);
+    return <TextEditor readOnly={true} canEdit={false} initialValue={text} />;
+  };
+
+  render() {
+    const action = this.renderAction ? this.renderAction() : null;
+    const replies = this.renderReplies ? this.renderReplies() : null;
+
+    return (
+      <Fragment>
+        <DiscussionCard
+          top={this.renderTop()}
+          info={this.renderInfo()}
+          infoStyle={this.props.infoStyle}
+          action={action}
+        />
+        {replies}
+      </Fragment>
+    );
+  }
+}
+
+class CommentClass extends DiscussionComment {
+  constructor(props) {
+    super(props);
+    this.state.showReplyBox = false;
+    this.state.replies = [];
+  }
+
+  renderAction = () => {
+    return (
+      <div className={css(styles.actionBar)}>
+        {!this.state.showReplyBox
+          ? this.renderReplyButton()
+          : this.renderReplyBox()}
+      </div>
+    );
+  };
+
+  renderReplyButton = () => {
+    return (
+      <div className={css(styles.reply)} onClick={this.showReplyBox}>
+        Reply
+      </div>
+    );
+  };
+
+  showReplyBox = () => {
+    this.setState({ showReplyBox: true });
+  };
+
+  renderReplyBox = () => {
+    return (
+      <ReplyBox onSubmit={this.addSubmittedReply} commentId={this.state.id} />
+    );
+  };
+
+  addSubmittedReply = (reply) => {
+    if (!doesNotExist(reply)) {
+      let newReplies = [reply];
+      newReplies = newReplies.concat(this.state.replies);
+      this.setState({ replies: newReplies });
+    }
+  };
+
+  renderReplies = () => {
+    return this.state.replies.map((r, i) => {
+      let divider = <div className={css(styles.divider)} />;
+      if (i === 0) {
+        divider = null;
+      }
+      return (
+        <Fragment key={r.id}>
+          {divider}
+          <Reply key={r.id} data={r} />
+        </Fragment>
+      );
+    });
+  };
+}
+
+const mapStateToProps = (state) => {
+  return {
+    voteResult: state.discussion.voteResult,
+  };
+};
+
+export const Comment = connect(
+  mapStateToProps,
+  null
+)(CommentClass);
+
+class ReplyClass extends DiscussionComment {
+  constructor(props) {
+    super(props);
+  }
+}
+
+export const Reply = connect(
+  mapStateToProps,
+  null
+)(ReplyClass);
+
+const styles = StyleSheet.create({
+  voteWidget: {
+    marginRight: 18,
+  },
+  actionBar: {
+    marginTop: 8,
+    width: "100%",
+  },
+  reply: {
+    cursor: "pointer",
+  },
+  contentContainer: {
+    width: "70%",
+    padding: "30px 0px",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  allCommentsContainer: {
+    width: "100%",
+  },
+  commentContainer: {
+    padding: "30px 30px 36px 30px",
+  },
+  commentInfo: {
+    color: colors.BLACK(0.8),
+  },
+  commentBoxContainer: {
+    width: "100%",
+  },
+  divider: {
+    borderBottom: "1px solid",
+    display: "block",
+    borderColor: discussionPageColors.DIVIDER,
+  },
+});
---
+++ components/DiscussionPageThread.js
@@ -0,0 +1,177 @@
+import { Fragment, useEffect, useState } from "react";
+
+// NPM Modules
+import { css, StyleSheet } from "aphrodite";
+import Link from "next/link";
+import Router, { useRouter } from "next/router";
+import { connect, useDispatch, useStore } from "react-redux";
+
+// components
+import DiscussionCard from "~/components/DiscussionCard";
+import DiscussionPostMetadata from "~/components/DiscussionPostMetadata";
+import VoteWidget from "~/components/VoteWidget";
+
+// Redux
+import DiscussionActions from "~/redux/discussion";
+
+// Utils
+import colors, { discussionPageColors } from "~/config/themes/colors";
+import icons from "~/config/themes/icons";
+
+const Thread = (props) => {
+  const { title, body, username, date, score, vote } = props;
+
+  const dispatch = useDispatch();
+  const store = useStore();
+  const router = useRouter();
+  const { paperId, discussionThreadId } = router.query;
+  const selectedVoteType = vote && vote.voteType;
+
+  async function upvote() {
+    dispatch(DiscussionActions.postUpvotePending());
+    await dispatch(DiscussionActions.postUpvote(paperId, discussionThreadId));
+
+    const latestVotes = store.getState().discussion.latestVotes;
+  }
+
+  async function downvote() {
+    dispatch(DiscussionActions.postDownvotePending());
+    await dispatch(DiscussionActions.postDownvote(paperId, discussionThreadId));
+
+    const latestVotes = store.getState().discussion.latestVotes;
+  }
+
+  return (
+    <div>
+      <BackButton />
+      <DiscussionCard
+        top={
+          <Fragment>
+            <VoteWidget
+              selected={selectedVoteType}
+              styles={styles.voteWidget}
+              score={score}
+              fontSize={"16px"}
+              width={"58px"}
+              onUpvote={upvote}
+              onDownvote={downvote}
+            />
+            <div className={css(styles.threadTitle)}>{title}</div>
+            <ShareButton />
+          </Fragment>
+        }
+        info={<div className={css(styles.body)}>{body}</div>}
+        infoStyle={styles.threadInfo}
+        action={<DiscussionPostMetadata username={username} date={date} />}
+      />
+    </div>
+  );
+};
+
+const BackButton = () => {
+  const message = "Go back to all discussions";
+  const router = useRouter();
+  const url = getBackUrl(router.asPath);
+
+  function getBackUrl(url) {
+    let parts = url.split("/");
+    parts.pop();
+    parts = parts.join("/");
+    return parts;
+  }
+
+  return (
+    <div className={css(styles.backButtonContainer)}>
+      <Link href={"/paper/[paperId]/[tabName]"} as={url}>
+        <a className={css(styles.backButton)}>
+          {icons.longArrowLeft} {message}
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+const ShareButton = () => {
+  return <div className={css(styles.shareContainer)}>{icons.share}</div>;
+};
+
+const styles = StyleSheet.create({
+  backButtonContainer: {
+    paddingLeft: 68,
+  },
+  backButton: {
+    color: colors.BLACK(0.5),
+    textDecoration: "none",
+  },
+  threadContainer: {
+    width: "80%",
+    padding: "30px 0px",
+    margin: "auto",
+  },
+  voteWidget: {
+    marginRight: 18,
+  },
+  threadInfo: {
+    paddingLeft: 68,
+    color: colors.BLACK(0.8),
+    "@media only screen and (min-width: 1024px)": {
+      width: "calc(100% - 68px - 170px)",
+    },
+  },
+  actionBar: {
+    marginTop: 8,
+    width: "100%",
+  },
+  threadTitle: {
+    width: "100%",
+    fontSize: 33,
+  },
+  body: {
+    marginBottom: 28,
+    marginTop: 14,
+    fontSize: 16,
+    lineHeight: "24px",
+  },
+  reply: {
+    cursor: "pointer",
+  },
+  contentContainer: {
+    width: "70%",
+    padding: "30px 0px",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  shareContainer: {
+    background: colors.LIGHT_GREY(),
+    color: colors.GREY(),
+    height: "46px",
+    width: "46px",
+    borderRadius: "50%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexShrink: 0,
+  },
+  allCommentsContainer: {
+    width: "100%",
+  },
+  commentContainer: {
+    padding: "30px 30px 36px 30px",
+  },
+  commentInfo: {
+    color: colors.BLACK(0.8),
+  },
+  commentBoxContainer: {
+    width: "100%",
+  },
+  divider: {
+    borderBottom: "1px solid",
+    display: "block",
+    borderColor: discussionPageColors.DIVIDER,
+  },
+});
+
+export default Thread;
