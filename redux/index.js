7c187cc8a5d6159a8c629230b0f5f9e30eab6d15
valmack <valmack@protonmail.com>
2019-10-09 18:25:23 -0400

    Add vote functionality to paper

--- redux/index.js
+++ redux/index.js
@@ -4,10 +4,12 @@ import discussionReducer from "./discussion/reducer";
 import paperReducer from "./paper/reducer";
 import modalReducer from "./modals";
 import authReducer from "./auth";
+import voteReducer from "./vote/reducer";
 
 export default combineReducers({
   modals: modalReducer,
   discussion: discussionReducer,
   paper: paperReducer,
   auth: authReducer,
+  vote: voteReducer,
 });
