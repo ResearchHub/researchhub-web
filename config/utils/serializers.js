d9ab97112bde1d5fe46087a030db11846b8f9d3e
valmack <valmack@protonmail.com>
2019-10-14 11:12:05 -0400

    Refactor deserializer

--- config/utils/serializers.js
+++ config/utils/serializers.js
@@ -1,3 +1,6 @@
+import { Value } from "slate";
+import Plain from "slate-plain-serializer";
+
 export function createUsername({ createdBy }) {
   const { firstName, lastName } = createdBy;
   return `${firstName} ${lastName}`;
@@ -5,4 +8,13 @@ export function createUsername({ createdBy }) {
 
 export function getVoteType(vote) {
   return vote && vote.voteType;
+}
+
+export function deserializeEditor(text) {
+  try {
+    text = Value.fromJSON(JSON.parse(text));
+  } catch (SyntaxError) {
+    text = Plain.deserialize(text);
+  }
+  return text;
 }
