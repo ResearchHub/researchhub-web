66f3a6612146032bf63a569647adbe4ce2c9db6e
valmack <valmack@protonmail.com>
2019-10-14 12:16:59 -0400

    Setup redux actions to edit threads

--- redux/discussion/types.js
+++ redux/discussion/types.js
@@ -1,6 +1,9 @@
 export const FETCH_THREAD_PENDING = "@@discussion/FETCH_THREAD_PENDING";
 export const FETCH_THREAD_FAILURE = "@@discussion/FETCH_THREAD_FAILURE";
 export const FETCH_THREAD_SUCCESS = "@@discussion/FETCH_THREAD_SUCCESS";
+export const UPDATE_THREAD_PENDING = "@@discussion/UPDATE_THREAD_PENDING";
+export const UPDATE_THREAD_FAILURE = "@@discussion/UPDATE_THREAD_FAILURE";
+export const UPDATE_THREAD_SUCCESS = "@@discussion/UPDATE_THREAD_SUCCESS";
 export const FETCH_COMMENTS_PENDING = "@@discussion/FETCH_COMMENTS_PENDING";
 export const FETCH_COMMENTS_FAILURE = "@@discussion/FETCH_COMMENTS_FAILURE";
 export const FETCH_COMMENTS_SUCCESS = "@@discussion/FETCH_COMMENTS_SUCCESS";
