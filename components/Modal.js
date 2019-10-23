6dec07944dceab3f3227b6818159923a86f89e48
valmack <valmack@protonmail.com>
2019-10-23 15:22:57 -0400

    Create modal component

---
+++ components/Modal.js
@@ -0,0 +1,36 @@
+import { css } from "aphrodite";
+import Modal from "react-modal";
+
+import { RHLogo } from "~/config/themes/icons";
+import { modalStyles as styles } from "~/config/themes/styles";
+
+export default (props) => {
+  const { isOpen, close, shouldCloseOnOverlayClick, title } = props;
+
+  return (
+    <Modal
+      className={css(styles.modal)}
+      isOpen={isOpen}
+      closeModal={close}
+      shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
+      onRequestClose={close}
+      style={styles.overlay}
+      ariaHideApp={false} // TODO: Set as true and add appElement
+    >
+      <div className={css(styles.modalContent)}>
+        <img
+          src={"/static/icons/close.png"}
+          className={css(styles.closeButton)}
+          onClick={close}
+        />
+        <div className={css(styles.titleContainer)}>
+          <div className={css(styles.title, styles.text)}>{title}</div>
+        </div>
+        {props.children}
+        <div className={css(styles.logoContainer)}>
+          <RHLogo iconStyle={styles.logo} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
