cac266f6797902d09286fcedbffb5598eff8e78d
Patrick Lu <lightning.lu7@gmail.com>
2019-10-26 03:05:49 -0700

    brought back favicons

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,26 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <style
             data-aphrodite
             dangerouslySetInnerHTML={{ __html: this.props.css.content }}
