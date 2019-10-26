fd4a52c4abae491e9be18f724c439ac26da921b5
joshslee <joshslee93@gmail.com>
2019-10-25 18:58:28 -0700

    refactor hub index.js

--- pages/hub/[hubname]/index.js
+++ pages/hub/[hubname]/index.js
@@ -9,66 +9,109 @@ import LockedHubPage from "~/components/Hubs/LockedHubPage";
 // Redux
 import { HubActions } from "~/redux/hub";
 
-const Index = (props) => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const store = useStore();
-  let { hubname } = router.query;
-  let hubName = hubname;
-  var { currentHub, hubs } = store.getState().hubs;
+// Config
+import API from "~/config/api";
+import { Helpers } from "@quantfive/js-web-config";
 
-  const getHubs = async () => {
-    await dispatch(HubActions.getHubs());
-    hubs = store.getState().hubs.hubs;
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hub: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState(
+      {
+        hub: Router.router.query.hubname,
+      },
+      () => {
+        this.fetchHubInfo(this.state.hub);
+      }
+    );
+  }
+
+  fetchHubInfo = (name) => {
+    return fetch(API.HUB({ name }), API.GET_CONFIG())
+      .then(Helpers.checkStatus)
+      .then(Helpers.parseJSON)
+      .then((res) => {
+        console.log("res", res);
+        // this.setState({ currentHub: res.result })
+      });
   };
 
-  const convertUrlToName = (hubName) => {
-    let nameArr = hubName.split("-");
-    if (nameArr.length > 1) {
-      return nameArr
-        .map((el) => {
-          let name = el[0].toUpperCase() + el.slice(1);
-          return name;
-        })
-        .join(" ");
-    } else {
-      let name = hubName[0].toUpperCase() + hubName.slice(1);
-      return name;
-    }
-  };
+  render() {
+    let currentHub = {
+      name: "blank",
+      id: 1,
+    };
 
-  if (hubs.length < 1) {
-    getHubs();
+    return <HubPage hub={currentHub} hubName={this.state.hubname} />;
   }
+}
 
-  hubName = convertUrlToName(hubName);
+// const Index = (props) => {
+// const router = useRouter();
+// const dispatch = useDispatch();
+// const store = useStore();
+// let { hubname } = router.query;
+// let hubName = hubname;
+// var { currentHub, hubs } = store.getState().hubs;
 
-  /**
-   * If current hub doesn't exist then use the hubName passed down
-   * to set current hub and then check to see if currentHub.isLocked is true / false
-   */
+// const getHubs = async () => {
+//   await dispatch(HubActions.getHubs());
+//   hubs = store.getState().hubs.hubs;
+// };
 
-  if (
-    currentHub === null ||
-    JSON.stringify(currentHub) === JSON.stringify({})
-  ) {
-    // if currentHub is empty
-    let current = hubs.filter((hub) => hub.name === hubName).pop();
-    if (current !== undefined) {
-      dispatch(HubActions.updateCurrentHubPage(current));
-      currentHub = store.getState().hubs.currentHub;
-    } else {
-      currentHub = {
-        is_locked: true,
-      };
-    }
-  }
+// const convertUrlToName = (hubName) => {
+//   let nameArr = hubName.split("-");
+//   if (nameArr.length > 1) {
+//     return nameArr
+//       .map((el) => {
+//         let name = el[0].toUpperCase() + el.slice(1);
+//         return name;
+//       })
+//       .join(" ");
+//   } else {
+//     let name = hubName[0].toUpperCase() + hubName.slice(1);
+//     return name;
+//   }
+// };
 
-  if (currentHub.is_locked) {
-    return <LockedHubPage hub={currentHub} hubName={hubName} />;
-  } else {
-    return <HubPage hub={currentHub} hubName={hubName} />;
-  }
-};
+// if (hubs.length < 1) {
+//   getHubs();
+// }
+
+// hubName = convertUrlToName(hubName);
+
+// /**
+//  * If current hub doesn't exist then use the hubName passed down
+//  * to set current hub and then check to see if currentHub.isLocked is true / false
+//  */
+
+// if (
+//   currentHub === null ||
+//   JSON.stringify(currentHub) === JSON.stringify({})
+// ) {
+//   // if currentHub is empty
+//   let current = hubs.filter((hub) => hub.name === hubName).pop();
+//   if (current !== undefined) {
+//     dispatch(HubActions.updateCurrentHubPage(current));
+//     currentHub = store.getState().hubs.currentHub;
+//   } else {
+//     currentHub = {
+//       is_locked: true,
+//     };
+//   }
+// }
+
+// if (currentHub.is_locked) {
+//   return <LockedHubPage hub={currentHub} hubName={hubName} />;
+// } else {
+//   return <HubPage hub={currentHub} hubName={hubName} />;
+// }
+// };
 
 export default Index;
