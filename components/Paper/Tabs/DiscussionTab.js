20039f12f8b15a5c4d906cfbaba0d963499f283a
valmack <valmack@protonmail.com>
2019-10-28 16:45:41 -0400

    Require login for user actions Show login modal when the user is not logged in and they try to post a thread, comment, reply, upload a paper, edit a paper, or propose a summary

--- components/Paper/Tabs/DiscussionTab.js
+++ components/Paper/Tabs/DiscussionTab.js
@@ -183,6 +183,7 @@ const DiscussionTab = (props) => {
             onClick={addDiscussion}
             modalMessage="create a discussion thread"
             permissionKey="CreateDiscussionThread"
+            loginRequired={true}
           >
             <button
               className={css(
