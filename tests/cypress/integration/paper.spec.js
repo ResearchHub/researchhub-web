19b0f058da94b09bdb1adcf54413602c8695af59
valmack <valmack@protonmail.com>
2019-10-29 16:15:32 -0400

    Test title and description on paper summary tab

--- tests/cypress/integration/paper.spec.js
+++ tests/cypress/integration/paper.spec.js
@@ -1,11 +1,29 @@
 describe("Paper Page", function() {
-  it("redirects to the summary tab", function() {
-    let path = "/paper/1";
+  const paperTitle = "";
+  const paperTagline = "";
 
+  before(function() {
+    let path = "/paper/1";
     cy.visit(path);
+  });
+
+  it("redirects to the summary tab", function() {
     cy.url().should("include", path + "/summary");
 
     cy.visit(path + "/");
     cy.url().should("include", path + "/summary");
   });
+
+  it.skip("has the paper title in the head title element", function() {
+    cy.get("title").contains(paperTitle);
+  });
+
+  it.skip("has the paper tagline in the head meta description element", function() {
+    cy.get("meta[name='description']").should(
+      "have.attr",
+      "content",
+      paperTagline
+    );
+  });
+
 });
