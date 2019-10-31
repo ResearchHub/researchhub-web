8f8f9cd72af1e9d1ea1099644ee22de21c55c593
joshslee <joshslee93@gmail.com>
2019-10-31 11:22:42 -0700

    responsive ui for addAuthorModal

--- components/Navbar.js
+++ components/Navbar.js
@@ -85,7 +85,7 @@ const Navbar = (props) => {
 
   const menuTabs = [
     { label: "Add Paper", onClick: addPaperModal, icon: "addPaper" },
-    { label: "Profile", route: `user/${user.id}/contributions`, icon: "user" },
+    { label: "Profile", route: `/user/${user.id}/contributions`, icon: "user" },
     { label: "Logout", onClick: signout, icon: "signOut", color: "ORANGE" },
   ];
 
@@ -460,7 +460,7 @@ const styles = StyleSheet.create({
     highlight: "none",
     outline: "none",
     ":hover": {
-      color: colors.BLUE(1),
+      color: colors.GREEN(1),
     },
   },
   icon: {
