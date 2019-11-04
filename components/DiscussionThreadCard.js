27a2c179e681911d2a561bddbed5cea0d52b36c7
joshslee <joshslee93@gmail.com>
2019-11-04 09:17:37 -0800

    discussion card responsive ui at 321-width

--- components/DiscussionThreadCard.js
+++ components/DiscussionThreadCard.js
@@ -186,7 +186,6 @@ const DiscussionThreadCard = (props) => {
 };
 
 function createUsername({ createdBy }) {
-  console.log("createdBy", createdBy);
   if (createdBy) {
     const { firstName, lastName } = createdBy;
     return `${firstName} ${lastName}`;
@@ -274,6 +273,10 @@ const styles = StyleSheet.create({
       backgroundColor: colors.BLUE(1),
       color: "#FFF",
     },
+    "@media only screen and (max-width: 321px)": {
+      minWidth: "unset",
+      width: 70,
+    },
   },
   readLabel: {
     fontSize: 14,
@@ -294,11 +297,12 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 22,
-    paddingBottom: 20,
-    color: colors.BLACK(1),
+    paddingBottom: 18,
+    // color: colors.BLACK(1),
+    color: "#232038",
     textOverflow: "eclipsis",
-    "@media only screen and (max-width: 321px)": {
-      fontSize: 21,
+    "@media only screen and (max-width: 436px)": {
+      fontSize: 18,
     },
   },
   link: {
@@ -321,8 +325,8 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   mobileVoteWidget: {
-    paddingLeft: 18,
-    marginBottom: 19,
+    paddingLeft: 20,
+    marginBottom: 15,
   },
   mobileReadButton: {
     marginRight: 20,
