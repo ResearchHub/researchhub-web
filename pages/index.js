1f1715d098e8a64164b7e9e7f40674800799bdc0
Patrick Lu <lightning.lu7@gmail.com>
2019-11-08 10:08:55 -0800

    summary loading pproperly for userrs

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
-import Link from "next/link";
-import Router from "next/router";
 import { connect } from "react-redux";
-import { StyleSheet, css } from "aphrodite";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { StyleSheet } from "aphrodite";
 
 // Component
-import Button from "../components/Form/Button";
-import HubsList from "~/components/Hubs/HubsList";
-import FormSelect from "~/components/Form/FormSelect";
-import InfiniteScroll from "react-infinite-scroller";
-import PaperEntryCard from "../components/Hubs/PaperEntryCard";
-import Loader from "~/components/Loader/Loader";
-import GoogleLoginButton from "~/components/GoogleLoginButton";
-
-// Redux
-import { ModalActions } from "~/redux/modals";
-import { AuthActions } from "~/redux/auth";
-import { PaperActions } from "~/redux/paper";
 
 // Config
-import API from "~/config/api";
-import { Helpers } from "@quantfive/js-web-config";
-import colors from "~/config/themes/colors";
-import { UPVOTE_ENUM, DOWNVOTE_ENUM } from "../config/constants";
 import HubPage from "../components/Hubs/HubPage";
 
 const filterOptions = [
