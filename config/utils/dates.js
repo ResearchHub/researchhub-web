634c4997c862a593825bf9d5119ec0ba12edf218
valmack <valmack@protonmail.com>
2019-11-12 17:59:45 -0500

    Add date format utils

--- config/utils/dates.js
+++ config/utils/dates.js
@@ -5,3 +5,12 @@ import en from "javascript-time-ago/locale/en";
 TimeAgo.addLocale(en);
 
 export const timeAgo = new TimeAgo("en-US");
+
+export function formatDateStandard(momentDate) {
+  console.log("the moment", momentDate);
+  return momentDate.format("MMM Do, YYYY");
+}
+
+export function formatPublishedDate(momentDate) {
+  return `Published: ${formatDateStandard(momentDate)}`;
+}
