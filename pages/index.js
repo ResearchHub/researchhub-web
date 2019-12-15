e97d353cbd9feb00a2f65a4b9d58dfd3d1e3cc55
Patrick Lu <lightning.lu7@gmail.com>
2019-12-14 22:50:54 -0800

    try without nginx configs

--- pages/index.js
+++ pages/index.js
@@ -6,41 +6,7 @@ import { StyleSheet } from "aphrodite";
 // Config
 import HubPage from "../components/Hubs/HubPage";
 
-const filterOptions = [
-  {
-    value: "top_rated",
-    label: "Top Rated",
-  },
-  {
-    value: "newest",
-    label: "Newest",
-  },
-  {
-    value: "most_discussed",
-    label: "Most Discussed",
-  },
-];
-
-const filterScope = [
-  {
-    value: "year",
-    label: "This Year",
-  },
-  {
-    value: "month",
-    label: "This Month",
-  },
-  {
-    value: "day",
-    label: "Today",
-  },
-];
-
 class Index extends React.Component {
-  getInitialProps = () => {
-    // TODO: GET THE DATA HERE
-  };
-
   render() {
     return <HubPage home={true} />;
   }
