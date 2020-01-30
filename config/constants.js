2d86e40308dc8d23b7eabd2aaeb96b91297b0fc4
valmack <valmack@protonmail.com>
2020-01-29 20:39:05 -0500

    WIP - Add orcid login button to navbar

--- config/constants.js
+++ config/constants.js
@@ -10,6 +10,12 @@ export const GOOGLE_CLIENT_ID =
     ? "192509748493-3enrmve4vlikpff88lujns7b4d72hgbg.apps.googleusercontent.com"
     : "192509748493-amjlt30mbpo9lq5gppn7bfd5c52i0ioe.apps.googleusercontent.com";
 
+export const ORCID_CLIENT_ID = "APP-JRYHYFMMJTSPBLCP";
+export const ORCID_REDIRECT_URI =
+  process.env.NODE_ENV === "production"
+    ? "https://researchhub.com/orcid/"
+    : "http://localhost:8000/orcid/login/";
+
 export function nameToUrl(name) {
   let arr = name.split(" ");
   return arr.length > 1 ? arr.join("-").toLowerCase() : name.toLowerCase();
