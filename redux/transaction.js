94c071f8e4dd2d8ba769090f2a772560bccc3f31
joshslee <joshslee93@gmail.com>
2020-01-31 17:30:49 -0800

    hot fixes for login ui

--- redux/transaction.js
+++ redux/transaction.js
@@ -52,7 +52,7 @@ const TransactionReducer = (state = defaultTransactionState, action) => {
       return {
         ...state,
         ...action.payload,
-        withdrawals: [...action.payload.withdrawals],
+        withdrawals: [...state.withdrawals, ...action.payload.withdrawals],
       };
     default:
       return state;
