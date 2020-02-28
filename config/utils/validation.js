9948926f56b833456ce73d7142be9b85e6389fb6
valmack <valmack@protonmail.com>
2020-02-27 19:29:05 -0500

    Show user votes for reddit style discussion - Closes #378

--- config/utils/validation.js
+++ config/utils/validation.js
@@ -1,4 +1,5 @@
 import { getCurrentUserReputation } from "./serializers";
+import { getNestedValue } from "./index";
 
 export function isValidEmail(email) {
   const re = /\S+@\S+\.edu/;
@@ -9,3 +10,11 @@ export function currentUserHasMinimumReputation(stateObject, minimum) {
   let reputation = getCurrentUserReputation(stateObject);
   return reputation >= minimum;
 }
+
+export const checkVoteTypeChanged = (prev, next) => {
+  const prevVoteType = getNestedValue(prev, ["user_vote", "vote_type"]);
+  const nextVoteType = getNestedValue(next, ["user_vote", "vote_type"]);
+  if (prevVoteType !== nextVoteType) {
+    return nextVoteType;
+  }
+};
