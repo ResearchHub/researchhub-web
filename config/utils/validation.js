74d0e93c20ae97875f3aefe34a4d21f915090e49
valmack <valmack@protonmail.com>
2020-02-27 20:00:33 -0500

    Test - Log votes

--- config/utils/validation.js
+++ config/utils/validation.js
@@ -12,8 +12,12 @@ export function currentUserHasMinimumReputation(stateObject, minimum) {
 }
 
 export const checkVoteTypeChanged = (prev, next) => {
+  console.log("prev", prev);
+  console.log("next", next);
   const prevVoteType = getNestedValue(prev, ["user_vote", "vote_type"]);
   const nextVoteType = getNestedValue(next, ["user_vote", "vote_type"]);
+  console.log("prev vote", prevVoteType);
+  console.log("next vote", nextVoteType);
   if (prevVoteType !== nextVoteType) {
     return nextVoteType;
   }
