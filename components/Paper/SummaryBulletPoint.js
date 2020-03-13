ee2b1fa884ce32549b6fb7aba6761a6b605447ff
joshslee <joshslee93@gmail.com>
2020-03-13 11:52:18 -0700

    show profile avatar of bullet point contributors

--- components/Paper/SummaryBulletPoint.js
+++ components/Paper/SummaryBulletPoint.js
@@ -2,6 +2,7 @@ import { StyleSheet, css } from "aphrodite";
 import Ripples from "react-ripples";
 
 import FormTextArea from "../Form/FormTextArea";
+import AuthorAvatar from "../AuthorAvatar";
 
 // Config
 import API from "~/config/api";
@@ -9,14 +10,22 @@ import { Helpers } from "@quantfive/js-web-config";
 import colors from "~/config/themes/colors";
 
 const SummaryBulletPoint = ({ data, editable, manage }) => {
-  let { text, plain_text } = data;
+  let { text, plain_text, created_by } = data;
+  let authorProfile = created_by.author_profile;
+
   return (
     <Ripples className={css(styles.bulletpoint, manage && styles.cursorMove)}>
-      <div className={css(styles.bulletpointIcon)}>
-        <i class="far fa-chevron-down" />
+      <div className={css(styles.topRow)}>
+        <div className={css(styles.bulletpointIcon)}>
+          <i class="far fa-chevron-down" />
+        </div>
+        <div className={css(styles.bulletpointText)}>
+          {plain_text && plain_text}
+        </div>
       </div>
-      <div className={css(styles.bulletpointText)}>
-        {plain_text && plain_text}
+      <div className={css(styles.bottomRow)}>
+        <span className={css(styles.contributorText)}>Contributors</span>
+        <AuthorAvatar author={authorProfile} size={25} />
       </div>
     </Ripples>
   );
@@ -25,8 +34,9 @@ const SummaryBulletPoint = ({ data, editable, manage }) => {
 const styles = StyleSheet.create({
   bulletpoint: {
     display: "flex",
+    flexDirection: "column",
     backgroundColor: "#FBFBFD",
-    alignItems: "center",
+    alignItems: "flex-start",
     boxSizing: "border-box",
     borderRadius: 3,
     padding: 16,
@@ -36,6 +46,11 @@ const styles = StyleSheet.create({
     ":hover": {
       // borderColor: "#E0E0E0",
     },
+  },
+  topRow: {
+    width: "100%",
+    display: "flex",
+    alignItems: "flex-start",
   },
   bulletpointIcon: {
     color: "#3971FF",
@@ -59,6 +74,20 @@ const styles = StyleSheet.create({
     fontWeight: 400,
     fontSize: 15,
     width: "calc(100% - 90px)",
+    paddingTop: 4,
+  },
+  bottomRow: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: 50,
+    marginTop: 5,
+  },
+  contributorText: {
+    color: "rgba(36, 31, 58, 0.4)",
+    fontStyle: "italic",
+    fontSize: 14,
+    marginRight: 8,
   },
   cursorMove: {
     cursor: "move",
