6c97cd8f7b37da7f758d03f05afcd9bb23e5633d
valmack <valmack@protonmail.com>
2020-03-27 16:56:20 -0400

    WIP

--- components/Head.js
+++ components/Head.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 
 export default (props) => {
@@ -5,12 +6,39 @@ export default (props) => {
   const description =
     props.description ||
     "We're a community seeking to improve prioritization, collaboration, reproducibility, and funding of scientific research. Discuss and discover academic research on Research Hub";
+  const url = props.url || ""; // (window && window.location);
+  const socialImage = props.socialImage || "url of image from s3";
+
   return (
     <Head>
       <title>{title}</title>
       <meta key="description" name="description" content={description} />
+      {/* Social meta tags */}
       <meta property="og:description" content={description} />
+      {/* Image dimensions: minimum 200x200; recommended 1500x1500 */}
+      {/* <meta property="og:image" content={socialImage} /> */}
+      <meta property="og:image" content="https://ogi.sh?title=Hello%20World" />
+      <meta property="og:image:alt" content={title} />
+      <meta property="og:image:secure_url" content={socialImage} />
+      <meta property="og:image:type" content={"image/png"} />
+      <meta property="og:image:width" content={"1500"} />
+      <meta property="og:image:height" content={"1500"} />
       <meta property="og:title" content={title} />
+      <meta property="og:type" content={"article"} />
+      <meta property="og:site_name" content={"ResearchHub"} />
+      <meta property="og:url" content={url} />
+      {/* Twitter meta tags */}
+      <meta property="twitter:card" content={"summary_large_image"} />
+      <meta property="twitter:description" value={description} />
+      <meta property="twitter:image" content={socialImage} />
+      {/* Left */}
+      <meta property="twitter:label1" value={title} />
+      <meta property="twitter:data1" value={title} />
+      {/* Right */}
+      <meta property="twitter:label2" value={title} />
+      <meta property="twitter:data2" value={title} />
+      <meta property="twitter:title" value={title} />
+      <meta property="twitter:url" value={url} />
       {props.children}
     </Head>
   );
