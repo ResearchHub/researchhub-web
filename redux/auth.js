d3406922b9860e405643aea88310872fc9bb86aa
lightninglu10 <lightning.lu7@gmail.com>
2020-05-08 13:07:13 -0700

    subscribed set

--- redux/auth.js
+++ redux/auth.js
@@ -62,7 +62,9 @@ let getUserHelper = (dispatch, dispatchFetching) => {
     .then(Helpers.checkStatus)
     .then(Helpers.parseJSON)
     .then((json) => {
-      dispatch(HubActions.updateSubscribedHubs(json.results[0].subscribed)); // updates the subscribedHubs on Hub Redux State
+      if (json.results[0]) {
+        dispatch(HubActions.updateSubscribedHubs(json.results[0].subscribed)); // updates the subscribedHubs on Hub Redux State
+      }
 
       if (json.results.length > 0) {
         return dispatch({
