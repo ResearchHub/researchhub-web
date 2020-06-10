0866ed6706533d8d17137d4c14aad3c9c674965f
joshslee <joshslee93@gmail.com>
2020-06-10 16:46:56 -0700

    allow user to toggle twitter comments

--- config/api.js
+++ config/api.js
@@ -210,7 +210,7 @@ const routes = (BASE_URL) => {
       return BASE_URL + "permissions/";
     },
 
-    DISCUSSION: (paperId, filter, page, progress) => {
+    DISCUSSION: (paperId, filter, page, progress, twitter) => {
       let url = `${BASE_URL}paper/${paperId}/discussion/`;
 
       if (progress) {
@@ -226,6 +226,8 @@ const routes = (BASE_URL) => {
           url += `&ordering=${filter}`;
         }
       }
+
+      url += `&source=${twitter ? "twitter" : "researchhub"}`;
 
       return url;
     },
