e6cbd03aa06905c195db56bf159128179aaf82a6
valmack <valmack@protonmail.com>
2020-07-22 16:08:26 -0400

    Add awscliv2 deploy scripts

---
+++ scripts/deploy/production-awscliv2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if git diff-index --quiet HEAD --; then
+    set -o errexit; # Exit on error
+echo Step 1/3: Logging into ECR;
+    # $(aws ecr get-login --no-include-email --region us-west-2 --profile researchhub);
+echo Step 2/3: Creating new production image;
+    yarn run build:prod;
+    docker tag researchhub-web:latest 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-web:latest
+    docker push 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-web:latest
+echo Step 3/3: Creating elastic beanstalk environment;
+    mv Dockerfile Dockerfile.prod.off;
+    mv Dockerrun.aws.json.production Dockerrun.aws.json;
+    git add Dockerfile.prod.off Dockerfile;
+    git add Dockerrun.aws.json.production Dockerrun.aws.json;
+    eb deploy production2 --profile researchhub --staged;
+    git reset;
+    mv Dockerfile.prod.off Dockerfile;
+    mv Dockerrun.aws.json Dockerrun.aws.json.production;
+    SENTRY_RELEASE=production-$(git rev-parse --short HEAD) yarn run sentry;
+else
+    echo Please commit your changes first.;
+fi
