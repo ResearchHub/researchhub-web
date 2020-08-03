e30989faf438571399c3b88d2d1ba7206bd27461
joshslee <joshslee93@gmail.com>
2020-08-03 13:11:32 -0700

    fix sentry errors

--- components/Banner/PaperPromotionBanner.js
+++ components/Banner/PaperPromotionBanner.js
@@ -7,6 +7,8 @@ import { MessageActions } from "~/redux/message";
 import { ModalActions } from "~/redux/modals";
 import { BannerActions } from "~/redux/banner";
 
+import API from "~/config/api";
+import { Helpers } from "@quantfive/js-web-config";
 import colors from "~/config/themes/colors";
 
 class PaperPromotionBanner extends React.Component {
