161b4368ee8a75871d307395911fe1ef6a028931
joshslee <joshslee93@gmail.com>
2020-08-04 16:18:17 -0700

    recaptcha init

--- components/modal/ReCaptchaPrompt.js
+++ components/modal/ReCaptchaPrompt.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { StyleSheet, css } from "aphrodite";
 
 // Config
 import API from "~/config/api";
 import { Helpers } from "@quantfive/js-web-config";
 import colors from "~/config/themes/colors";
+import { RECAPTCHA_CLIENT_KEY } from "~/config/constants";
 
 class ReCaptchaPrompt extends React.Component {
   constructor(props) {
@@ -14,7 +16,35 @@ class ReCaptchaPrompt extends React.Component {
     this.recaptchaRef = React.createRef();
   }
 
+  componentDidMount() {
+    console.log("this.recaptchaRef.current", this.recaptchaRef.current);
+    setTimeout(() => {
+      this.recaptchaRef.current.execute();
+    }, 8000);
+  }
+
+  onSuccess = (e) => {
+    console.log("success", e);
+  };
+
+  onError = (e) => {
+    console.log("error", e);
+  };
+
   render() {
-    return <form onSubmit={this.submit}></form>;
+    return (
+      <form onSubmit={this.submit}>
+        <ReCAPTCHA
+          ref={this.recaptchaRef}
+          sitekey={RECAPTCHA_CLIENT_KEY}
+          onChange={this.onSuccess}
+          onExpired={this.onError}
+          onErrored={this.onError}
+          size={"normal"}
+        />
+      </form>
+    );
   }
 }
+
+export default ReCaptchaPrompt;
