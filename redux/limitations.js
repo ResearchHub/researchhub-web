6fd823ca80f8b3150ff16fe3fcb181975251d3ba
joshslee <joshslee93@gmail.com>
2020-08-06 16:14:33 -0700

    remove redundant conditional

--- redux/limitations.js
+++ redux/limitations.js
@@ -86,17 +86,7 @@ export const LimitationsActions = {
           });
         })
         .catch((err) => {
-          if (err.response.status === 429) {
-            handleCatch(err, dispatch);
-            return dispatch({
-              type: LimitationsConstants.POST_FAILURE,
-              payload: {
-                pending: false,
-                success: false,
-                status: 429,
-              },
-            });
-          }
+          handleCatch(err, dispatch);
           return dispatch({
             type: LimitationsConstants.POST_FAILURE,
             payload: {
@@ -138,9 +128,7 @@ export const LimitationsActions = {
           });
         })
         .catch((err) => {
-          if (err.response.status === 429) {
-            handleCatch(err, dispatch);
-          }
+          handleCatch(err, dispatch);
           return dispatch({
             type: LimitationsConstants.REORDER_FAILURE,
             payload: {
