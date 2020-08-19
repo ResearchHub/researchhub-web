39de6ccc1f1ec2ab06aa4ab8529355bf99608616
joshslee <joshslee93@gmail.com>
2020-08-19 10:37:15 -0700

    change key for raw_authors

--- redux/paper/shims.js
+++ redux/paper/shims.js
@@ -53,9 +53,8 @@ export const paperPost = ({
   if (raw_authors) {
     if (raw_authors.length) {
       raw_authors.forEach((raw_author, i) => {
-        return formData.append(`raw_authors[]`, JSON.stringify(raw_author));
+        return formData.append(`raw_authors`, JSON.stringify(raw_author));
       });
-      // formData.append("raw_authors_length", raw_authors.length);
     }
   }
 
