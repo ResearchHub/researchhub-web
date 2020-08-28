9fd2ec56ed38b3358f7af01e61958dedd3995980
joshslee <joshslee93@gmail.com>
2020-08-28 12:02:40 -0700

    move call for main feed to initialProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
 import { connect } from "react-redux";
 import { StyleSheet } from "aphrodite";
+import API from "~/config/api";
+import moment from "moment";
 
 import HubPage from "../components/Hubs/HubPage";
 
 class Index extends React.Component {
+  static async getInitialProps(ctx) {
+    let scope = {
+      start: moment()
+        .startOf("day")
+        .unix(),
+      end: moment().unix(),
+    };
+
+    let res = await fetch(
+      API.GET_HUB_PAPERS({
+        hubId: 0,
+        ordering: "hot",
+        timePeriod: scope,
+      }),
+      API.GET_CONFIG()
+    );
+
+    let json = await res.json();
+
+    return { papers: { ...json } };
+  }
+
   render() {
-    return <HubPage home={true} />;
+    return <HubPage home={true} initialFeed={this.props.papers} />;
   }
 }
 
