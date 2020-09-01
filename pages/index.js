a61f5c37df4f5821abf4fd3dc025bb24ff2dc174
joshslee <joshslee93@gmail.com>
2020-09-01 11:03:27 -0700

    inital props on ?server=true

--- pages/index.js
+++ pages/index.js
@@ -8,37 +8,36 @@ import { getInitialScope } from "~/config/utils/dates";
 
 class Index extends React.Component {
   static async getInitialProps(ctx) {
-    try {
-      // Grab initial home feed as props
-      const [initialFeed, leaderboardFeed] = await Promise.all([
-        fetch(
-          API.GET_HUB_PAPERS({
-            hubId: 0,
-            ordering: "hot",
-            timePeriod: getInitialScope(),
-          }),
-          API.GET_CONFIG()
-        ).then((res) => res.json()),
-        fetch(
-          API.LEADERBOARD({ limit: 10, page: 1, hubId: 0 }),
-          API.GET_CONFIG()
-        ).then((res) => res.json()),
-      ]);
+    let { query } = ctx;
+    if (query.server) {
+      try {
+        // Grab initial home feed as props
+        const [initialFeed, leaderboardFeed] = await Promise.all([
+          fetch(
+            API.GET_HUB_PAPERS({
+              hubId: 0,
+              ordering: "hot",
+              timePeriod: getInitialScope(),
+            }),
+            API.GET_CONFIG()
+          ).then((res) => res.json()),
+          fetch(
+            API.LEADERBOARD({ limit: 10, page: 1, hubId: 0 }),
+            API.GET_CONFIG()
+          ).then((res) => res.json()),
+        ]);
 
-      return { initialFeed, leaderboardFeed };
-    } catch {
+        return { initialFeed, leaderboardFeed, query };
+      } catch {
+        return { initialFeed: null, leaderboardFeed: null };
+      }
+    } else {
       return { initialFeed: null, leaderboardFeed: null };
     }
   }
 
   render() {
-    return (
-      <HubPage
-        home={true}
-        initialFeed={this.props.initialFeed}
-        leaderboardFeed={this.props.leaderboardFeed}
-      />
-    );
+    return <HubPage home={true} {...this.props} />;
   }
 }
 
