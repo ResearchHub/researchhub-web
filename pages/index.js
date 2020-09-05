6ef539d22c06b00047b277c867acb198ab0046f9
lightninglu10 <lightning.lu7@gmail.com>
2020-09-05 15:12:27 -0700

    loading hubs later

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ class Index extends React.Component {
     };
 
     try {
-      const [initialFeed, leaderboardFeed, initialHubList] = await Promise.all([
+      const [initialFeed, leaderboardFeed] = await Promise.all([
         fetch(
           API.GET_HUB_PAPERS({
             // Initial Feed
@@ -30,10 +30,10 @@ class Index extends React.Component {
           API.LEADERBOARD({ limit: 10, page: 1, hubId: 0 }), // Leaderboard
           API.GET_CONFIG()
         ).then((res) => res.json()),
-        fetch(API.SORTED_HUB({}), API.GET_CONFIG()).then((res) => res.json()),
+        // fetch(API.SORTED_HUB({}), API.GET_CONFIG()).then((res) => res.json()),
       ]);
 
-      return { initialFeed, leaderboardFeed, initialHubList, query };
+      return { initialFeed, leaderboardFeed, query };
     } catch {
       return defaultProps;
     }
