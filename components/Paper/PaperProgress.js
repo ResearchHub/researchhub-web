a55c6e2434e5c296a0ae89492afd9c0b5206ec1b
joshslee <joshslee93@gmail.com>
2020-09-11 14:47:49 -0700

    revert legacy; update post for check_user_vote to get_config_with_body

--- components/Paper/PaperProgress.js
+++ components/Paper/PaperProgress.js
@@ -183,7 +183,7 @@ class PaperProgress extends React.Component {
       .then(Helpers.parseJSON)
       .then((res) => {});
 
-    fetch(API.AMP_ANALYTICS, API.GET_CONFIG_WITH_BODY(ampPayload))
+    fetch(API.AMP_ANALYTICS, API.POST_CONFIG(ampPayload))
       .then(Helpers.checkStatus)
       .then(Helpers.parseJSON)
       .then((res) => {});
