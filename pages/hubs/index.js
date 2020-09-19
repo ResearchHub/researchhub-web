08ec903f8be6f3f6c383d751d61b7fa1b2e74ecf
Thomas Vu <shuhari@macbook.local>
2020-09-18 23:21:34 -0600

    fixed anchor offset and encoding bug, made hub images non-draggable

--- pages/hubs/index.js
+++ pages/hubs/index.js
@@ -101,8 +101,8 @@ class Index extends React.Component {
       return (
         <>
           <div
-            name={categoryName}
-            className={css(styles.label)}
+            name={`${encodeURIComponent(categoryName)}`}
+            className={css(styles.categoryLabel)}
           >{`${categoryName}`}</div>
           <div key={`${categoryName}_${i}`} className={css(styles.grid)}>
             {this.renderHubs(categoryID)}
@@ -154,7 +154,6 @@ class Index extends React.Component {
                 <Button
                   isWhite={true}
                   label={"Suggest a Hub"}
-                  buttonStyle={styles.button}
                   hideRipples={true}
                 />
               </PermissionNotificationWrapper>
@@ -223,7 +222,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-  label: {
+  categoryLabel: {
     borderBottom: "1px solid #ededed",
     fontSize: 22,
     fontWeight: 500,
@@ -232,6 +231,8 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     cursor: "default",
     userSelect: "none",
+    paddingTop: 90,
+    marginTop: -90,
   },
   grid: {
     display: "flex",
