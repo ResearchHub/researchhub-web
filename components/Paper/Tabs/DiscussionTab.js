66dec104f4879b02d3f5067264cdd7ca5f47a864
joshslee <joshslee93@gmail.com>
2020-10-01 09:45:57 -0700

    use discussion_count in place of threadCount

--- components/Paper/Tabs/DiscussionTab.js
+++ components/Paper/Tabs/DiscussionTab.js
@@ -138,7 +138,7 @@ const DiscussionTab = (props) => {
               path={t.path}
               newCard={transition && i === 0} //conditions when a new card is made
               mobileView={mobileView}
-              discussionCount={props.paper.threadCount}
+              discussionCount={props.paper.discussion_count}
               setCount={setCount}
               paper={props.paperState}
             />
