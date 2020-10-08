2802831d8b853eecaf330906ea6f17bbe851302a
lightninglu10 <lightning.lu7@gmail.com>
2020-10-08 14:28:54 -0700

    fix sidebar issues

--- pages/hubs/index.js
+++ pages/hubs/index.js
@@ -232,15 +232,20 @@ const styles = StyleSheet.create({
     userSelect: "none",
   },
   sidebar: {
-    minWidth: 225,
-    "@media only screen and (max-width: 769px)": {
+    minWidth: 220,
+    width: "18%",
+    position: "sticky",
+    top: 79,
+    background: "#fff",
+    minHeight: "100vh",
+    "@media only screen and (max-width: 767px)": {
       display: "none",
     },
   },
   content: {
     borderLeft: "1px solid #ededed",
     "@media only screen and (min-width: 900px)": {
-      width: "90%",
+      width: "82%",
     },
   },
   row: {
