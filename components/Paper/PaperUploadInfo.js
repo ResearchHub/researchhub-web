a09dedaa0ff74a8cff35fbe2cef5e50f2fe40587
lightninglu10 <lightning.lu7@gmail.com>
2020-11-17 20:34:56 -0800

    changing to paper submission guideline

--- components/Paper/PaperUploadInfo.js
+++ components/Paper/PaperUploadInfo.js
@@ -558,7 +558,7 @@ class PaperUploadInfo extends React.Component {
         {label}{" "}
         <a
           className={css(styles.authorGuidelines)}
-          href="https://www.notion.so/ResearchHub-Summary-Guidelines-7ebde718a6754bc894a2aa0c61721ae2"
+          href="https://www.notion.so/researchhub/Paper-Submission-Guidelines-a2cfa1d9b53c431a91c9816e17f212e1"
           target="_blank"
         >
           Submission Guidelines
