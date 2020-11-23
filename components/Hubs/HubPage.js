017ed988da68a8182166c7a36fc414f35fde49ac
joshslee <joshslee93@gmail.com>
2020-11-23 13:34:11 -0800

    fix pagination for 'newest' filter on home page

--- components/Hubs/HubPage.js
+++ components/Hubs/HubPage.js
@@ -56,7 +56,7 @@ class HubPage extends React.Component {
         this.props.initialFeed && this.props.initialFeed.next
           ? this.props.initialFeed.next
           : null,
-      page: this.props.page || 1,
+      page: this.props.page || 2,
       doneFetching: this.props.initialFeed ? true : false,
       filterBy: this.props.filter ? this.props.filter : defaultFilter,
       scope: this.props.scope ? this.props.scope : defaultScope,
@@ -362,8 +362,10 @@ class HubPage extends React.Component {
               this.state.next ? this.state.next : this.state.prev
             ); // grab page from backend response
             let offset = this.state.next ? -1 : 1;
+            // if (this.state.filterBy.value === 'newest') {
+            //   offset = 0;
+            // }
             page = Number(page) + offset;
-
             this.updateSlugs(page);
           }
         );
