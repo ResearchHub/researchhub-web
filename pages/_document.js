8eaeb29b0788c2582d5171ea5902c3c769f5015c
joshslee <joshslee93@gmail.com>
2021-01-06 17:49:28 -0800

    remove preload attribute for stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -101,7 +101,6 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
             rel="stylesheet"
-            preload
           />
           <CustomHead />
         </Head>
