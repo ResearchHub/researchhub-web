9f79860afb344e3243233c4b99e08bdeb2d9509c
joshslee <joshslee93@gmail.com>
2021-01-08 14:01:27 -0800

    faster google font api

--- pages/_document.js
+++ pages/_document.js
@@ -67,6 +67,7 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
             async
           ></script>
+          <link rel="preconnect" href="https://fonts.gstatic.com/"></link>
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
             rel="stylesheet"
