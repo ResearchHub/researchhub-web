7be448905f65ca177c5c3bfa19a322a06ad0775e
joshslee <joshslee93@gmail.com>
2021-01-20 17:22:21 -0800

    author page placeholder

--- components/Placeholders/TabBarPlaceholder.js
+++ components/Placeholders/TabBarPlaceholder.js
@@ -1,15 +1,24 @@
 import React, { Fragment } from "react";
 import { StyleSheet, css } from "aphrodite";
 
-import { RectShape } from "react-placeholder/lib/placeholders";
+import { TextRow, RectShape } from "react-placeholder/lib/placeholders";
 
 const TabBarPlaceholder = ({ color }) => (
   <div className={css(styles.placeholderContainer) + " show-loading-animation"}>
-    <RectShape className={css(styles.textRow)} rows={1} color={color} />
-    <RectShape className={css(styles.textRow)} rows={1} color={color} />
-    <RectShape className={css(styles.textRow)} rows={1} color={color} />
-    <RectShape className={css(styles.textRow)} rows={1} color={color} />
-    <RectShape className={css(styles.textRow)} rows={1} color={color} />
+    <TextRow className={css(styles.textRow)} color={color} />
+    <RectShape className={css(styles.count)} rows={1} color={color} />
+    <TextRow className={css(styles.textRow)} color={color} />
+    <RectShape className={css(styles.count)} rows={1} color={color} />
+    <TextRow className={css(styles.textRow)} color={color} />
+    <RectShape className={css(styles.count)} rows={1} color={color} />
+    <TextRow className={css(styles.textRow)} color={color} />
+    <RectShape className={css(styles.count)} rows={1} color={color} />
+    <TextRow className={css(styles.textRow)} color={color} />
+    <RectShape
+      className={css(styles.count, styles.last)}
+      rows={1}
+      color={color}
+    />
   </div>
 );
 
@@ -22,13 +31,20 @@ const styles = StyleSheet.create({
     boxSizing: "border-box",
   },
   textRow: {
-    width: "25%",
+    width: "15%",
     height: 25,
-    marginRight: 15,
+    marginBottom: 16,
+  },
+  last: {
+    marginRight: 0,
+  },
+  count: {
+    height: 25,
+    width: 40,
+    marginLeft: 5,
+    marginRight: 20,
     marginBottom: 5,
   },
-  space: {},
-  label: {},
 });
 
 export default TabBarPlaceholder;
