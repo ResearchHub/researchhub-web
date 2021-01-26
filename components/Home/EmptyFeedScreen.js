781362344f60fd67fdab5b3ce2e479c6be127de6
joshslee <joshslee93@gmail.com>
2021-01-25 19:27:04 -0800

    hub page fetch refactor

--- components/Home/EmptyFeedScreen.js
+++ components/Home/EmptyFeedScreen.js
@@ -1,32 +1,30 @@
-import { useState } from "react";
-import { connect } from "react-redux";
 import { StyleSheet, css } from "aphrodite";
-import Ripples from "react-ripples";
+import Router from "next/router";
 
-import Loader from "~/components/Loader/Loader";
-
-import { ModalActions } from "~/redux/modals";
-
-import icons from "~/config/themes/icons";
-import colors from "~/config/themes/colors";
+import PermissionNotificationWrapper from "~/components/PermissionNotificationWrapper";
+import Button from "../Form/Button";
 
 const EmpytFeedScreen = () => {
+  const navigateToPaperUploadPage = () => {
+    Router.push(`/paper/upload/info`, `/paper/upload/info`);
+  };
+
   return (
-    <div className={css(styles.column)} style={{ height: this.state.height }}>
+    <div className={css(styles.column)}>
       <img
         className={css(styles.emptyPlaceholderImage)}
         src={"/static/background/homepage-empty-state.png"}
         loading="lazy"
         alt="Empty State Icon"
       />
-      <div className={css(styles.text, styles.emptyPlaceholderText)}>
+      <div className={css(styles.emptyPlaceholderText)}>
         There are no academic papers uploaded for this hub.
       </div>
-      <div className={css(styles.text, styles.emptyPlaceholderSubtitle)}>
+      <div className={css(styles.emptyPlaceholderSubtitle)}>
         Click ‘Upload paper’ button to upload a PDF
       </div>
       <PermissionNotificationWrapper
-        onClick={this.navigateToPaperUploadPage}
+        onClick={navigateToPaperUploadPage}
         modalMessage="upload a paper"
         loginRequired={true}
         permissionKey="CreatePaper"
@@ -36,3 +34,43 @@ const EmpytFeedScreen = () => {
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  column: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  emptyPlaceholderImage: {
+    width: 400,
+    objectFit: "contain",
+    marginTop: 40,
+    "@media only screen and (max-width: 415px)": {
+      width: "70%",
+    },
+  },
+  emptyPlaceholderText: {
+    width: 500,
+    textAlign: "center",
+    fontSize: 18,
+    color: "#241F3A",
+    marginTop: 20,
+    "@media only screen and (max-width: 415px)": {
+      width: "85%",
+    },
+  },
+  emptyPlaceholderSubtitle: {
+    width: 500,
+    textAlign: "center",
+    fontSize: 14,
+    color: "#4e4c5f",
+    marginTop: 10,
+    marginBottom: 15,
+    "@media only screen and (max-width: 415px)": {
+      width: "85%",
+    },
+  },
+});
+
+export default EmpytFeedScreen;
---
+++ components/Home/MainFeed.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { connect } from "react-redux";
+import { StyleSheet, css } from "aphrodite";
+import Ripples from "react-ripples";
+import ReactPlaceholder from "react-placeholder/lib";
+
+import PaperEntryCard from "~/components/Hubs/PaperEntryCard";
+import Loader from "~/components/Loader/Loader";
+import PaperPlaceholder from "../Placeholders/PaperPlaceholder";
+
+import { ModalActions } from "~/redux/modals";
+
+import icons from "~/config/themes/icons";
+import colors from "~/config/themes/colors";
+
+const MainFeed = () => {};
