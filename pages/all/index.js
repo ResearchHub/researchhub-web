d802465fc7103437b0ed08ed7c98a0e23ecbba67
lightninglu10 <lightning.lu7@gmail.com>
2021-01-29 13:29:07 -0800

    sample feed if feed_type is not subscribed

--- pages/all/index.js
+++ pages/all/index.js
@@ -3,6 +3,8 @@ import HubPage from "~/components/Hubs/HubPage";
 import { getInitialScope } from "~/config/utils/dates";
 import { fetchPaperFeed } from "~/config/fetch";
 import { filterOptions } from "~/config/utils/options";
+import nookies from "nookies";
+import { AUTH_TOKEN } from "~/config/constants";
 
 const Index = (props) => {
   return <HubPage home={true} {...props} />;
@@ -14,17 +16,26 @@ Index.getInitialProps = async (ctx) => {
   const { query } = ctx;
   const { filter, page } = query;
   const filterObj = filterOptions.filter((el) => el.value === filter)[0];
-  if (!isServer()) {
-    return { home: true, page: 1, feed: 1, filter: filterObj, query };
-  }
-
+  const cookies = nookies.get(ctx);
+  const authToken = cookies[AUTH_TOKEN];
   const defaultProps = {
     initialFeed: null,
     leaderboardFeed: null,
     initialHubList: null,
     feed: 1,
+    loggedIn: authToken !== undefined,
   };
 
+  if (!isServer()) {
+    return {
+      ...defaultProps,
+      home: true,
+      page: 1,
+      feed: 1,
+      filter: filterObj,
+      query,
+    };
+  }
   const PARAMS = {
     ordering: "hot",
     timePeriod: getInitialScope(),
@@ -36,6 +47,7 @@ Index.getInitialProps = async (ctx) => {
     const initialFeed = await fetchPaperFeed(PARAMS);
 
     return {
+      ...defaultProps,
       initialFeed,
       feed: 1,
     };
