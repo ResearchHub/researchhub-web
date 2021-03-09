3faca8924df67fcc47cb3930f20ca9b0909329e1
calvinhlee23@gmail.com <calvinhlee23@gmail.com>
2021-03-09 15:43:20 -0800

    updating state name

--- components/Paper/Tabs/summary_tab_v2/SummaryTabV2Abstract.react.ts
+++ components/Paper/Tabs/summary_tab_v2/SummaryTabV2Abstract.react.ts
@@ -1,15 +1,15 @@
 import React, { useState } from "React";
 
-type AbstractState = {
-  abstract?: string;
+type ComponentState = {
+  abstractBody?: string;
   shouldEditAbstract: boolean;
   shouldShowAbstract: boolean;
 };
 type Props = {};
 
 function SummaryTabV2Abstract(props: Props) {
-  const [abstractState, setAbstractState] = useState<AbstractState>({
-    abstract: "",
+  const [ComponentState, setComponentState] = useState<ComponentState>({
+    abstractBody: "",
     shouldEditAbstract: false,
     shouldShowAbstract: true,
   });
