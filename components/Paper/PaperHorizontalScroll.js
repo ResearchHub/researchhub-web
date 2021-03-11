c395043c123d49d5493f47a79cc17dbfabda3194
joshslee <joshslee93@gmail.com>
2021-03-11 15:26:06 -0800

    remove empty file

--- components/Paper/PaperHorizontalScroll.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, css } from "aphrodite";
-
-// Component
-import ColumnContainer from "./ColumnContainer";
-
-// Config
-import icons from "~/config/themes/icons";
-import colors from "~/config/themes/colors";
