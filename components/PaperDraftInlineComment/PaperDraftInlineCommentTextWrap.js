7cc2019b860f8127add71b39ca50a9bd428ae0f9
Calvin Lee <calvinhlee23@gmail.com>
2021-03-22 14:25:31 -0700

    styling

--- components/PaperDraftInlineComment/PaperDraftInlineCommentTextWrap.js
+++ components/PaperDraftInlineComment/PaperDraftInlineCommentTextWrap.js
@@ -3,6 +3,7 @@ import React from "react";
 function PaperDraftInlineCommentTextWrap(props) {
   const { block = {} } = props ?? {};
   const { text: blockText } = block;
+  console.warn("YOYOYO: ", block.getType());
   return <React.Fragment>{blockText}</React.Fragment>;
 }
 
