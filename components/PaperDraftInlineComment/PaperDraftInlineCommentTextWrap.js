c9fb6aaed64719349278e7cc7aed833cdfba82ec
Calvin Lee <calvinhlee23@gmail.com>
2021-03-22 14:27:43 -0700

    removing comment

--- components/PaperDraftInlineComment/PaperDraftInlineCommentTextWrap.js
+++ components/PaperDraftInlineComment/PaperDraftInlineCommentTextWrap.js
@@ -3,7 +3,6 @@ import React from "react";
 function PaperDraftInlineCommentTextWrap(props) {
   const { block = {} } = props ?? {};
   const { text: blockText } = block;
-  console.warn("YOYOYO: ", block.getType());
   return <React.Fragment>{blockText}</React.Fragment>;
 }
 
