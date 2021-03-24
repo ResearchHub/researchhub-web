750a3c884b385277f60433efad243f7d5f01a416
Calvin Lee <calvinhlee23@gmail.com>
2021-03-23 17:11:25 -0700

    adding ts config

---
+++ components/PaperDraftInlineComment/PaperDraftInlineCommentDisplayContainer.tsx
@@ -0,0 +1,9 @@
+import InlineCommentUnduxStore from "./undux/InlineCommentUnduxStore";
+import React, { useEffect } from "react";
+
+function PaperDraftInlineCommentDisplayContainer(_props) {
+  const inlineCommentStore = InlineCommentUnduxStore.useStore();
+  return <div>"YOYOYOYO"</div>;
+}
+
+export default PaperDraftInlineCommentDisplayContainer;
