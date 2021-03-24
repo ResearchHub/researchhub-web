d5dcda9671670473d46dc09c1d2697242e0701ab
Calvin Lee <calvinhlee23@gmail.com>
2021-03-23 23:53:52 -0700

    ...

--- components/PaperDraftInlineComment/PaperDraftInlineCommentDisplayContainer.tsx
+++ components/PaperDraftInlineComment/PaperDraftInlineCommentDisplayContainer.tsx
@@ -1,12 +1,36 @@
-import InlineCommentUnduxStore from "./undux/InlineCommentUnduxStore";
+import InlineCommentUnduxStore, {
+  InlineComment,
+  InlineCommentStore,
+} from "./undux/InlineCommentUnduxStore";
 import React, { useEffect } from "react";
 
-type Props = {};
+type ID = number | string | null;
+type fetchInlineCommentThreadsArgs = {
+  paperID: ID;
+  inlineCommentStore: InlineCommentStore;
+};
+
+function fetchInlineCommentThreads({
+  paperID,
+  inlineCommentStore,
+}: fetchInlineCommentThreadsArgs): void {
+  // TODO: calvinhlee - make api call here.
+  // inlineCommentStore.set('inlineComments')({});
+}
 
 function PaperDraftInlineCommentDisplayContainer(
-  props: Props
+  _props: any
 ): React.ReactElement<"div"> {
   const inlineCommentStore = InlineCommentUnduxStore.useStore();
+  const paperID = inlineCommentStore.get("paperID");
+  useEffect((): void => {
+    console.warn("HI", paperID);
+    fetchInlineCommentThreads({ paperID, inlineCommentStore });
+  }, [paperID, inlineCommentStore]);
+  // const inlineCommentData = inlineCommentStore.get("inlineComments");
+  // const inlineComments = Object.keys(inlineCommentStore.get("inlineComments")).map((inlineCommentKey: string): typeof => {
+  //  return <></>
+  // });
   return <div>"YOYOYOYO"</div>;
 }
 
