2b31156c39d590cd8f40088c8235399f4a2ccdcd
Calvin Lee <calvinhlee23@gmail.com>
2021-03-23 17:14:29 -0700

    nice

--- components/PaperDraftInlineComment/PaperDraftInlineCommentDisplayContainer.tsx
+++ components/PaperDraftInlineComment/PaperDraftInlineCommentDisplayContainer.tsx
@@ -1,7 +1,11 @@
 import InlineCommentUnduxStore from "./undux/InlineCommentUnduxStore";
 import React, { useEffect } from "react";
 
-function PaperDraftInlineCommentDisplayContainer(_props) {
+type Props = {};
+
+function PaperDraftInlineCommentDisplayContainer(
+  props: Props
+): React.ReactElement<"div"> {
   const inlineCommentStore = InlineCommentUnduxStore.useStore();
   return <div>"YOYOYOYO"</div>;
 }
