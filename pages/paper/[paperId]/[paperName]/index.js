ac9b9857e74f9d092c654a87b46b0e6e7435d86c
Calvin Lee <calvinhlee23@gmail.com>
2021-03-25 12:23:07 -0700

    in progress

--- pages/paper/[paperId]/[paperName]/index.js
+++ pages/paper/[paperId]/[paperName]/index.js
@@ -605,7 +605,6 @@ const Paper = (props) => {
             ) : (
               <React.Fragment>
                 <PaperPreview paper={paper} paperId={paperId} />
-
                 <PaperSideColumn
                   authors={getAllAuthors()}
                   paper={paper}
