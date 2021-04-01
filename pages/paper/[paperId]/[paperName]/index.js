fc015094d7dfc397a675e759d6cfe95af039d223
Calvin Lee <calvinhlee23@gmail.com>
2021-04-01 08:30:03 -0700

    ui interaction done

--- pages/paper/[paperId]/[paperName]/index.js
+++ pages/paper/[paperId]/[paperName]/index.js
@@ -429,7 +429,6 @@ const Paper = (props) => {
   const inlineCommentUnduxStore = InlineCommentUnduxStore.useStore();
   const shouldShowInlineComment =
     inlineCommentUnduxStore.get("displayableInlineComments").length > 0;
-
   return (
     <div>
       <PaperBanner paper={paper} loadingPaper={loadingPaper} />
