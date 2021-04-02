63cc4416de0096f35f2e310d36dcb259cf7b3dfc
lightninglu10 <lightning.lu7@gmail.com>
2021-04-01 22:37:44 -0700

    removed paper preview

--- pages/paper/[paperId]/[paperName]/index.js
+++ pages/paper/[paperId]/[paperName]/index.js
@@ -599,7 +599,6 @@ const Paper = (props) => {
               </div>
             ) : (
               <React.Fragment>
-                <PaperPreview paper={paper} paperId={paperId} />
                 <PaperSideColumn
                   authors={getAllAuthors()}
                   paper={paper}
