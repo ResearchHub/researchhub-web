bc78cc8d6917514feaa603aeb92f60b0daf5b508
Calvin Lee <calvinhlee23@gmail.com>
2021-04-08 14:28:24 -1000

    rea

--- components/InlineCommentDisplay/InlineCommentThreadCardResponseSection.tsx
+++ components/InlineCommentDisplay/InlineCommentThreadCardResponseSection.tsx
@@ -1,17 +1,31 @@
 import { css, StyleSheet } from "aphrodite";
 import { ID } from "../PaperDraft/undux/PaperDraftUnduxStore";
-import React, { ReactElement, ReactFragment } from "react";
+import React, { ReactElement, ReactFragment, useEffect, useState } from "react";
 import InlineCommentComposer from "./InlineCommentComposer";
+import icons from "../../config/themes/icons";
 
 type Props = {
   commentData: Array<any>;
   commentThreadID: ID;
+  isActive?: boolean;
 };
+
+function emptyFunction(): void {}
 
 export default function InlineCommentThreadCardResponseSection({
   commentData,
   commentThreadID,
-}: Props): ReactElement<ReactFragment> {
+  isActive = false,
+}: Props): ReactElement<"div"> {
+  const [composedResponse, setComposedResponse] = useState<any>(null);
+  const [shouldShowComposer, setShouldShowComposer] = useState<boolean>(
+    isActive
+  );
+
+  useEffect((): void => {
+    setShouldShowComposer(isActive);
+  }, [isActive]);
+
   const commentResponses =
     commentData.length > 0
       ? commentData.map((commentData, i: number) => {
@@ -19,8 +33,8 @@ export default function InlineCommentThreadCardResponseSection({
             <InlineCommentComposer
               isReadOnly={true}
               key={`thread-response-${commentData.id}-${i}`}
-              onCancel={() => {}}
-              onSubmit={() => {}}
+              onCancel={emptyFunction}
+              onSubmit={emptyFunction}
               textData={commentData ? commentData.text : null}
             />
           );
@@ -28,22 +42,40 @@ export default function InlineCommentThreadCardResponseSection({
       : null;
 
   return (
-    <React.Fragment>
-      <div className={css(styles.threadResponseComposerWrap)}>
-        <InlineCommentComposer
-          isReadOnly={false}
-          onCancel={() => {}}
-          onSubmit={() => {}}
-          textData={null} /* initial data should be empty */
-          placeholder={"Respond to comment above"}
-        />
-      </div>
+    <div className={css(styles.inlineCommentThreadCardResponseSection)}>
+      {shouldShowComposer && (
+        <div className={css(styles.threadResponseComposerWrap)}>
+          <InlineCommentComposer
+            isReadOnly={false}
+            onCancel={emptyFunction}
+            onSubmit={emptyFunction}
+            placeholder={"Respond to comment above"}
+            textData={composedResponse}
+          />
+        </div>
+      )}
       <div className={css(styles.threadResponsesWrap)}>{commentResponses}</div>
-    </React.Fragment>
+    </div>
   );
 }
 
 const styles = StyleSheet.create({
+  inlineCommentThreadCardResponseSection: {},
+  replyIcon: {
+    color: "#918f9b",
+    marginRight: 8,
+  },
+  responseText: {
+    fontFamily: "Roboto",
+    fontSize: 14,
+    color: "#AAAAAA",
+    "@media only screen and (max-width: 415px)": {
+      fontSize: 12,
+    },
+  },
+  textBump: {
+    marginBottom: 2,
+  },
   threadResponsesWrap: { display: "flex", flexDirection: "column" },
   threadResponseComposerWrap: { marginBottom: 8 },
 });
