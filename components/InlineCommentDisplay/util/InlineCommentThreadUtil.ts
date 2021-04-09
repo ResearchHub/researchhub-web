d9a65dd7f5f7ebf866c35e9c0e5f31de86c54778
Calvin Lee <calvinhlee23@gmail.com>
2021-04-09 08:33:38 -1000

    scroll to discussionEntry

---
+++ components/InlineCommentDisplay/util/InlineCommentThreadUtil.ts
@@ -0,0 +1,53 @@
+import { SyntheticEvent } from "react";
+import { ID } from "../../PaperDraftInlineComment/undux/InlineCommentUnduxStore";
+
+type GetTargetDraftEntityElArgs = {
+  commentThreadID: ID;
+  entityKey: ID;
+};
+type ScrollToTargetElFncArgs = {
+  onSuccess: () => void;
+  targetElement: HTMLElement | null;
+};
+type ScrollToTargetElFncReturn = (event: SyntheticEvent) => void;
+
+function isElemntWithinViewPort(element: HTMLElement): boolean {
+  var rect = element.getBoundingClientRect();
+  const result =
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= document.documentElement.clientHeight;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= document.documentElement.clientHeight
+  );
+}
+
+export function getTargetInlineDraftEntityEl({
+  commentThreadID,
+  entityKey,
+}): HTMLElement | null {
+  let entityEl = document.getElementById(`inline-comment-${commentThreadID}`);
+  if (entityEl == null) {
+    entityEl = document.getElementById(`inline-comment-${entityKey}`);
+  }
+  return entityEl;
+}
+
+export const getScrollToTargetElFnc = ({
+  onSuccess,
+  targetElement,
+}: ScrollToTargetElFncArgs): ScrollToTargetElFncReturn => (
+  event: SyntheticEvent
+): void => {
+  event.stopPropagation();
+  if (targetElement != null && !isElemntWithinViewPort(targetElement)) {
+    targetElement.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "center",
+    });
+    onSuccess();
+  }
+};
