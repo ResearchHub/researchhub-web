e49a82166f4846f81a10d175e1e102d536929261
Calvin Lee <calvinhlee23@gmail.com>
2021-04-19 21:49:56 -1000

    typing

--- config/utils/nullchecks.ts
+++ config/utils/nullchecks.ts
@@ -1,4 +1,6 @@
-export function emptyFncWithMsg(message) {
+export function emptyFncWithMsg(
+  message: Error | string | null | undefined
+): void {
   if (message == null) {
     console.warn("emptyFncWithMsg is used. this maybe a bug");
   } else {
@@ -6,15 +8,15 @@ export function emptyFncWithMsg(message) {
   }
 }
 
-export function isUndefined(given) {
+export function isUndefined(given: any): boolean {
   return given === "undefined" || typeof given === "undefined" || given == null;
 }
 
-export function nullToEmptyString(given) {
+export function nullToEmptyString(given: string | null | undefined): string {
   return given || "";
 }
 
-export function silentEmptyFnc() {}
+export function silentEmptyFnc(): void {}
 
 export function nullthrows<T>(
   given: T,
