551de5f7b04c7a951f9c6cda0a49a6a6f3f875c3
Calvin Lee <calvinhlee23@gmail.com>
2021-04-22 12:25:00 -1000

    cleanup

--- config/types/root_types.ts
+++ config/types/root_types.ts
@@ -1,3 +1,3 @@
 export type ID = string | number | null;
-export type KeyOf<Object> = keyof Object;
-export type ValueOf<T> = T[keyof T];
+export type KeyOf<ObjectType> = keyof ObjectType;
+export type ValueOf<ObjectType> = ObjectType[keyof ObjectType];
