299cdba64e7010d96efc8cec7353ad3bcef903a1
Calvin Lee <calvinhlee23@gmail.com>
2021-04-22 16:10:07 -1000

    ok

--- components/PaperDraft/util/parse_tools/engrafo.ts
+++ components/PaperDraft/util/parse_tools/engrafo.ts
@@ -1,10 +1,5 @@
 // TODO: calvinhlee improve this maybe
 export function htmlToBlockForEngrafo({ idsToRemove, node, nodeName }) {
-  // const result = {
-  //   type: node.className,
-  //   data: { ...node },
-  // };
-  // console.warn("hitting here: ", result);
   return {
     type: node.className,
     data: {},
