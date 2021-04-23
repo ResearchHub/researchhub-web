8bd93df9161ffff5d435fd6a6fb2d69eef91bd31
Calvin Lee <calvinhlee23@gmail.com>
2021-04-23 11:57:16 -1000

    allow KeyEvents to be passed down as long as the user is loggedin

--- components/PaperDraft/PaperDraft.js
+++ components/PaperDraft/PaperDraft.js
@@ -172,7 +172,7 @@ class PaperDraft extends React.Component {
             />
           </div>
           <PaperDraftEventCaptureWrap
-            shouldAllowKeyEvents={isLoggedIn && isInEditMode}
+            shouldAllowKeyEvents={isLoggedIn}
             shouldAllowMouseEvents={isLoggedIn}
           >
             <div
