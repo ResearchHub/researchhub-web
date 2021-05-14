b91b5cd550df889cf0e2275b559659254a38a7ff
Calvin Lee <calvinhlee23@gmail.com>
2021-05-14 04:53:08 -0700

    validation is working

--- pages/author-claim-validation/api/authorClaimValidateToken.js
+++ pages/author-claim-validation/api/authorClaimValidateToken.js
@@ -1,7 +1,21 @@
 import { emptyFncWithMsg } from "~/config/utils/nullchecks";
+import { Helpers } from "@quantfive/js-web-config";
+import API from "~/config/api";
 
 export function authenticateToken({
   onError = emptyFncWithMsg,
   onSuccess = emptyFncWithMsg,
   token,
-}) {}
+}) {
+  fetch(API.AUTHOR_CLAIM_TOKEN_VALIDATION(), API.POST_CONFIG({ token }))
+    .then(Helpers.checkStatus)
+    .then(Helpers.parseJSON)
+    .then((response) => {
+      console.warn("response: ", response);
+      onSuccess();
+    })
+    .catch((err) => {
+      console.warn("err: ", err);
+      onError(err);
+    });
+}
