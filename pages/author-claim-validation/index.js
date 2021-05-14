35989f4d12e59ce5c3b081d90d61d92cf88ed2f6
Calvin Lee <calvinhlee23@gmail.com>
2021-05-14 04:53:08 -0700

    validation is working

--- pages/author-claim-validation/index.js
+++ pages/author-claim-validation/index.js
@@ -7,9 +7,7 @@ import { VALIDATION_STATE } from "./constants";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 export default function AuthorClaimValidation() {
-  const [validationState, setValidationState] = useState(
-    VALIDATION_STATE.LOADING
-  );
+  const [validationState, setValidationState] = useState(null);
 
   const pageBody = useMemo(() => getPageBody(validationState), [
     getPageBody,
@@ -19,7 +17,7 @@ export default function AuthorClaimValidation() {
   const onValidationError = useCallback(
     ({ error, validationState }) => {
       emptyFncWithMsg(error);
-      setValidationState(validationState);
+      setValidationState(validationState || VALIDATION_STATE.LOADING);
     },
     [setValidationState]
   );
@@ -29,7 +27,9 @@ export default function AuthorClaimValidation() {
     const token = urlParams.get("token");
     if (isNullOrUndefined(token) || token.length < 1) {
       setValidationState(VALIDATION_STATE.REQUEST_NOT_FOUND);
-    } else if (validationState === VALIDATION_STATE.LOADING) {
+    } else if (isNullOrUndefined(validationState)) {
+      console.warn("calling");
+      setValidationState(VALIDATION_STATE.LOADING);
       authenticateToken({
         onError: onValidationError,
         onSuccess: () => setValidationState(VALIDATION_STATE.VALIDATED),
