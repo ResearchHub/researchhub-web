a86debd558ce60838ebc3be00f252b6772807f56
Brian Santoso <brian.santoso@berkeley.edu>
2021-05-18 13:25:47 -0700

    Created NewPostModal

--- pages/paper/upload/info/index.js
+++ pages/paper/upload/info/index.js
@@ -1,19 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useRouter } from "next/router";
+import { connect } from "react-redux";
 
 // Component
 import Head from "~/components/Head";
 import PaperUploadInfo from "~/components/Paper/PaperUploadInfo";
 
-const Index = () => {
+import NewPostModal from "~/components/Modals/NewPostModal";
+
+// Redux
+import { ModalActions } from "~/redux/modals";
+import ComponentWrapper from "../../../../components/ComponentWrapper";
+
+const Index = (props) => {
   const router = useRouter();
   const { uploadPaperTitle, type } = router.query;
+
+  useEffect(() => {
+    props.openNewPostModal(true);
+    console.log("showing new post modal", props);
+  }, []);
+
   return (
     <Fragment>
       <Head title={`Upload Paper`} description="Upload paper to ResearchHub" />
+      <NewPostModal />
       <PaperUploadInfo paperTitle={uploadPaperTitle} type={type} />
     </Fragment>
   );
 };
 
-export default Index;
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {
+  openNewPostModal: ModalActions.openNewPostModal,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Index);
+
+// export default Index;
