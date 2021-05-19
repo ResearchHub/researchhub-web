5f6a5054b1844ca5192a41ce915656cd0b6a3c00
Brian Santoso <brian.santoso@berkeley.edu>
2021-05-18 17:27:31 -0700

    Split LargeList & LargeListItem into 2 files. Renaming and optimizations.

--- components/Form/LargeList.js
+++ components/Form/LargeList.js
@@ -1,28 +1,34 @@
 import React, { useState } from "react";
 import { StyleSheet, css } from "aphrodite";
 
-import CheckBox from "~/components/Form/CheckBox";
+// Components
+import LargeListItem from "~/components/Form/LargeListItem";
 
-const LargeList = ({ selectMany, items = [], onChange }) => {
-  let [actives, setActives] = useState(items.map(() => false));
+export default function LargeList({
+  selectMany,
+  customListStyle,
+  onChange,
+  children,
+}) {
+  let [isActives, setIsActives] = useState(children.map(() => false));
 
   const renderListItem = (item, index) => {
     return (
       <LargeListItem
         checkboxSquare={selectMany}
-        active={actives[index]}
-        id={`item-${index}`}
+        active={isActives[index]}
+        id={`largeListItem-${index}`}
         onChange={(id, value) => {
           // id of LargeListItem/Checkbox, new value
           let changed;
           if (selectMany) {
-            changed = [...actives]; // Do not modify other boxes
+            changed = [...isActives]; // Do not modify other boxes
           } else {
-            changed = actives.map(() => false); // Set all other boxes to false
+            changed = isActives.map(() => false); // Set all other boxes to false
           }
           changed[index] = value;
-          setActives(changed);
-          onChange && onChange(actives, index, value); // Send information about the state change
+          setIsActives(changed);
+          onChange && onChange(isActives, index, value, id); // Send information about the state change
         }}
       >
         {item}
@@ -30,55 +36,18 @@ const LargeList = ({ selectMany, items = [], onChange }) => {
     );
   };
 
+  const wrappedChildren = children.map(renderListItem);
   return (
-    <div className={css(styles.largeList)}>{items.map(renderListItem)}</div>
-  );
-};
-
-const LargeListItem = ({ checkboxSquare, active, id, onChange, children }) => {
-  return (
-    <div className={css(styles.largeListItem)}>
-      <div className={css(styles.checkboxAligner)}>
-        <CheckBox
-          isSquare={checkboxSquare}
-          active={active}
-          id={id}
-          onChange={onChange}
-        />
-      </div>
-      {children}
+    <div className={css(styles.largeList, customListStyle)}>
+      {wrappedChildren}
     </div>
   );
-};
+}
 
 const styles = StyleSheet.create({
   largeList: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "flex-start",
-    margin: "31px",
-  },
-  largeListItem: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    width: "480px", // TODO: let programmer set height, width
-    borderRadius: "4px",
-    backgroundColor: "#FAFAFA",
-    border: "1.5px solid #F0F0F0",
-    margin: "5px 0px",
-    padding: "20px",
-  },
-  checkboxAligner: {
-    display: "flex",
-    alignSelf: "stretch",
-    paddingRight: "20px",
-  },
-  contentAligner: {
-    display: "flex",
-    alignSelf: "flex-grow",
   },
 });
-
-export default LargeList;
-export { LargeListItem };
