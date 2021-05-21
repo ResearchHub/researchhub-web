916339db47bae67c6c95e91c0578750a9ce3bb6b
Calvin Lee <calvinhlee23@gmail.com>
2021-05-20 20:30:06 -0700

    ..

--- components/AuthorClaimCaseDashboard/AuthorClaimCaseCard.tsx
+++ components/AuthorClaimCaseDashboard/AuthorClaimCaseCard.tsx
@@ -1,5 +1,27 @@
-import React, { useEffect, ReactElement } from "react";
+import { AUTHOR_CLAIM_STATUS } from "./constants/AuthorClaimStatus";
+import { ID, ValueOf } from "../../config/types/root_types";
+import React, { ReactElement, useState } from "react";
 
-export default function AuthorClaimCaseCard(): ReactElement<"div"> {
-  return <div>This is card</div>;
+export type AuthorClaimCase = {
+  caseStatus: ValueOf<typeof AUTHOR_CLAIM_STATUS> | string;
+  requestID: ID;
+  requestorEmail: string;
+  requestorName: string;
+  targetAuthorID: ID;
+  targetAuthorName: string;
+};
+
+type Props = {
+  allowedActions: Array<ValueOf<typeof AUTHOR_CLAIM_STATUS>>;
+  authorClaimCase: AuthorClaimCase;
+  cardWidth: number | string;
+};
+
+export default function AuthorClaimCaseCard({
+  allowedActions,
+  authorClaimCase,
+  cardWidth,
+}: Props): ReactElement<"div"> {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+  return <div style={{ width: cardWidth }}>This is card</div>;
 }
