4b0ee23e1fe896734b2a2a4f2a36618ddee0142e
Calvin Lee <calvinhlee23@gmail.com>
2021-05-21 13:59:43 -0700

    count done

---
+++ components/AuthorClaimCaseDashboard/api/AuthorClaimCaseGetCounts.ts
@@ -0,0 +1,29 @@
+import API from "../../../config/api";
+import { Helpers } from "@quantfive/js-web-config";
+import { emptyFncWithMsg } from "../../../config/utils/nullchecks";
+
+export type Counts = {
+  CLOSED: number;
+  OPEN: number;
+};
+
+type ApiArgs = {
+  onSuccess: (counts: Counts) => void;
+  onError?: Function;
+};
+
+export function getCaseCounts({
+  onSuccess,
+  onError = emptyFncWithMsg,
+}: ApiArgs): void {
+  fetch(API.MODERATORS_AUTHOR_CLAIM_CASE_COUNT(), API.GET_CONFIG())
+    .then(Helpers.checkStatus)
+    .then(Helpers.parseJSON)
+    .then((response: any) =>
+      onSuccess({
+        CLOSED: response.closed_count || 0,
+        OPEN: response.open_count || 0,
+      })
+    )
+    .catch((e) => onError(e));
+}
