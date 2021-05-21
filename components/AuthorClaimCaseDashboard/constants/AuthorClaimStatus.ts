f17e75a1ecdeb5f25ae54a52661db4ad912674b8
Calvin Lee <calvinhlee23@gmail.com>
2021-05-20 19:41:30 -0700

    path management

---
+++ components/AuthorClaimCaseDashboard/constants/AuthorClaimStatus.ts
@@ -0,0 +1,17 @@
+export const AUTHOR_CLAIM_STATUS = {
+  CLOSED: "CLOSED",
+  DENIED: "DENIED",
+  INITIATED: "INITIATED",
+  INVALIDATED: "INVALIDATED",
+  NULLIFIED: "NULLIFIED",
+  OPEN: "OPEN",
+};
+
+export const AUTHOR_CLAIM_STATUS_LABEL = {
+  CLOSED: "Closed",
+  DENIED: "Denied",
+  INITIATED: "Initiated",
+  INVALIDATED: "Invalidated",
+  NULLIFIED: "Nullified",
+  OPEN: "Open",
+};
