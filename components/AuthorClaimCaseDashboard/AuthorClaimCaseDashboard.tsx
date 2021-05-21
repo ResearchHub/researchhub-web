41672e05727f597ec1e9d15580017cfbd4f37855
Calvin Lee <calvinhlee23@gmail.com>
2021-05-20 20:45:02 -0700

    ..

--- components/AuthorClaimCaseDashboard/AuthorClaimCaseDashboard.tsx
+++ components/AuthorClaimCaseDashboard/AuthorClaimCaseDashboard.tsx
@@ -1,14 +1,14 @@
 import { css, StyleSheet } from "aphrodite";
 import React, { ReactElement } from "react";
 import AuthorClaimCaseContainer from "./AuthorClaimCaseContainer";
-import AuthorClaimDashbaordNavbar from "./AuthorClaimDashboadNavbar";
+import AuthorClaimDashboardNavbar from "./AuthorClaimDashboardNavbar";
 
 export const INNER_EL_WIDTH = 1276;
 
 export default function AuthorClaimCaseDashboard(): ReactElement<"div"> {
   return (
     <div className={css(styles.authorClaimCaseDashboard)}>
-      <AuthorClaimDashbaordNavbar innerElWidth={INNER_EL_WIDTH} />
+      <AuthorClaimDashboardNavbar innerElWidth={INNER_EL_WIDTH} />
       <div className={css(styles.caseContinaerWrap)}>
         <AuthorClaimCaseContainer />
       </div>
@@ -19,7 +19,7 @@ export default function AuthorClaimCaseDashboard(): ReactElement<"div"> {
 const styles = StyleSheet.create({
   authorClaimCaseDashboard: {
     alignItems: "center",
-    backgroundColor: "#F2F2F2",
+    backgroundColor: "#FBFBFD",
     display: "flex",
     flexDirection: "column",
     height: "100vh",
