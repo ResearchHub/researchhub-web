d6e11a31388a404ff89e71eaf163352b65acbcc9
Calvin Lee <calvinhlee23@gmail.com>
2021-05-24 14:21:42 -0700

    reload & refetch done

--- components/AuthorClaimCaseDashboard/AuthorClaimCaseContainer.tsx
+++ components/AuthorClaimCaseDashboard/AuthorClaimCaseContainer.tsx
@@ -1,12 +1,15 @@
-import { AUTHOR_CLAIM_STATUS } from "./constants/AuthorClaimStatus";
+import {
+  AUTHOR_CLAIM_STATUS,
+  AUTHOR_CLAIM_STATUS_LABEL,
+} from "./constants/AuthorClaimStatus";
 import { AuthorClaimCase } from "./api/AuthorClaimCaseGetCases";
 import { css, StyleSheet } from "aphrodite";
 import { getCases } from "./api/AuthorClaimCaseGetCases";
 import { INNER_EL_WIDTH } from "./AuthorClaimCaseDashboard";
-import { nullthrows } from "../../config/utils/nullchecks";
 import { useRouter, NextRouter } from "next/router";
 import AuthorClaimCaseCard from "./AuthorClaimCaseCard";
-import React, { useEffect, ReactElement, useState } from "react";
+import React, { useEffect, ReactElement, useState, Fragment } from "react";
+import colors from "../../config/themes/colors";
 
 type Props = {
   lastFetchTime: number;
@@ -17,17 +20,26 @@ const useEffectHandleCaseFetch = ({
   currRouter,
   lastFetchTime,
   setClaimCases,
+  setIsPageLoading,
 }: {
   currRouter: NextRouter;
   lastFetchTime: number;
   setClaimCases: React.Dispatch<React.SetStateAction<AuthorClaimCase[]>>;
+  setIsPageLoading: Function;
 }): void => {
   // @ts-ignore url-status is implicitly set with the constant
   const queriedStatus = AUTHOR_CLAIM_STATUS[currRouter.query.case_status];
   useEffect((): void => {
+    setIsPageLoading(true);
     getCases({
       caseStatus: queriedStatus,
-      onSuccess: setClaimCases,
+      onSuccess: (response): void => {
+        setClaimCases(response);
+        setIsPageLoading(false);
+      },
+      onError: (): void => {
+        setIsPageLoading(false);
+      },
     });
   }, [lastFetchTime, queriedStatus, setClaimCases]);
 };
@@ -38,10 +50,12 @@ export default function AuthorClaimCaseContainer({
 }: Props): ReactElement<"div"> {
   const router = useRouter();
   const [claimCases, setClaimCases] = useState<Array<AuthorClaimCase>>([]);
+  const [isPageLoading, setIsPageLoading] = useState<boolean>(true);
   useEffectHandleCaseFetch({
     lastFetchTime,
     currRouter: router,
     setClaimCases,
+    setIsPageLoading,
   });
 
   const caseCards = claimCases.map(
@@ -58,8 +72,20 @@ export default function AuthorClaimCaseContainer({
     }
   );
 
+  // @ts-ignore url-status is implicitly set with the constant
+  const caseStatusLabel = AUTHOR_CLAIM_STATUS_LABEL[router.query.case_status];
   return (
-    <div className={css(styles.authorClaimCaseContainer)}>{caseCards}</div>
+    <div className={css(styles.authorClaimCaseContainer)}>
+      {isPageLoading ? (
+        <Fragment />
+      ) : caseCards.length > 0 ? (
+        caseCards
+      ) : (
+        <div
+          className={css(styles.noCaseFound)}
+        >{`No ${caseStatusLabel} Cases Found`}</div>
+      )}
+    </div>
   );
 }
 
@@ -71,6 +97,15 @@ const styles = StyleSheet.create({
     height: "100%",
     justifyContent: "center",
     paddingTop: 24,
+    width: "100%",
+  },
+  noCaseFound: {
+    alignContent: "center",
+    color: colors.GREY(1),
+    display: "flex",
+    fontSize: 24,
+    height: "100%",
+    justifyContent: "center",
     width: "100%",
   },
 });
