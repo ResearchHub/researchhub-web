1a74f32ded6fb32b0ca640040c159d3c23a85d22
Brian Santoso <brian.santoso@berkeley.edu>
2021-05-25 13:18:51 -0700

    Paper preview now strictly contained in preview box (objectFit: contain)

--- components/PaperPageCard.js
+++ components/PaperPageCard.js
@@ -735,7 +735,7 @@ const styles = StyleSheet.create({
     marginLeft: "auto",
     display: "flex",
     flexDirection: "column",
-    maxWidth: "18%",
+    maxWidth: "8em",
 
     "@media only screen and (max-width: 767px)": {
       display: "none",
