f35f0b879bee1a6167055e51ffd492e7cd8ff325
Calvin Lee <calvinhlee23@gmail.com>
2021-05-25 14:06:17 -0700

    migration

--- components/AuthorClaimValidation/util.js
+++ components/AuthorClaimValidation/util.js
@@ -9,49 +9,38 @@ export const getPageBody = (validationState) => {
     case VALIDATION_STATE.DENIED_TOO_MANY_ATTEMPS:
       return (
         <div className={css(styles.pageBody)}>
-          <div className={css(styles.textRow)}>
+          <span>
             {
-              "Looks like this request was invalidated due to too many attempts. "
+              "Looks like this request was invalidated due to too many attempts. Please make a new request"
             }
-          </div>
-          <div className={css(styles.textRow)}>
-            {"Please make a new request"}
-          </div>
+          </span>
         </div>
       );
     case VALIDATION_STATE.DENIED_WRONG_USER:
       return (
         <div className={css(styles.pageBody)}>
-          <div className={css(styles.textRow)}>
-            {"Looks like you're logged in as a different account. "}
-          </div>
-          <div className={css(styles.textRow)}>
+          <span>
             {
-              "Please make sure you are logged in with ResearchHub account that you've made this request with."
+              "Looks like you're logged in as a different account. Please make sure you are logged in with ResearchHub account that you've made this request with."
             }
-          </div>
+          </span>
         </div>
       );
     case VALIDATION_STATE.REQUEST_NOT_FOUND:
       return (
         <div className={css(styles.pageBody)}>
-          <div className={css(styles.textRow)}>
-            {"We are unable to find this request. "}
-          </div>
-          <div className={css(styles.textRow)}>
+          <span>
             {
-              "Please make sure to only use link that's provided by the email that ResearchHub sent you"
+              "We are unable to find this request. Please make sure to only use link that's provided by the email that ResearchHub sent you"
             }
-          </div>
+          </span>
         </div>
       );
     case VALIDATION_STATE.VALIDATED:
       return (
         <div className={css(styles.pageBody)}>
-          <div className={css(styles.textRow)}>
-            {"You have successfully authenciated your request! "}
-          </div>
-          <div className={css(styles.textRow)}>
+          <div>{"You have successfully authenciated your request! "}</div>
+          <div>
             {"ResearchHub team will now review your author claim request"}
           </div>
         </div>
@@ -60,14 +49,10 @@ export const getPageBody = (validationState) => {
     default:
       return (
         <div className={css(styles.pageBody)}>
-          <div className={css(styles.textRow)}>
-            <span className={css(styles.marginRight8)}>
-              <Loader color={colors.BLUE(1)} loading size={16} />
-            </span>
-            <span>
-              {" Please be patient while we authenticate your request"}
-            </span>
-          </div>
+          <span className={css(styles.marginRight8)}>
+            <Loader color={colors.BLUE(1)} loading size={16} />
+          </span>
+          <span>{" Please be patient while we authenticate your request"}</span>
         </div>
       );
   }
@@ -78,17 +63,10 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   pageBody: {
-    alignItems: "center",
     display: "flex",
-    flexDirection: "column",
-    fontSize: 20,
     height: "100%",
-    overflowX: "auto",
-    paddingTop: 32,
+    justifyContent: "center",
     width: "100%",
-  },
-  textRow: {
-    height: 32,
-    textAlign: "center",
+    fontSize: 16,
   },
 });
