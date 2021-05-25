dc30834ffc7c97d8692b23f13e3523264607ad32
Calvin Lee <calvinhlee23@gmail.com>
2021-05-25 14:06:17 -0700

    migration

--- pages/author-claim-validation/index.js
+++ pages/author-claim-validation/index.js
@@ -1,49 +1,6 @@
-import { authenticateToken } from "./api/authorClaimValidateToken";
-import { css, StyleSheet } from "aphrodite";
-import { emptyFncWithMsg } from "~/config/utils/nullchecks";
-import { getPageBody } from "./util";
-import { isNullOrUndefined } from "~/config/utils/nullchecks";
-import { VALIDATION_STATE } from "./constants";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import AuthorClaimValidation from "~/components/AuthorClaimValidation/AuthorClaimValidation";
+import React from "react";
 
-export default function AuthorClaimValidation() {
-  const [validationState, setValidationState] = useState(null);
-
-  const pageBody = useMemo(() => getPageBody(validationState), [
-    getPageBody,
-    validationState,
-  ]);
-
-  const onValidationError = useCallback(
-    ({ error, validationState }) => {
-      emptyFncWithMsg(error);
-      setValidationState(validationState || VALIDATION_STATE.LOADING);
-    },
-    [setValidationState]
-  );
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search || "");
-    const token = urlParams.get("token");
-    if (isNullOrUndefined(token) || token.length < 1) {
-      setValidationState(VALIDATION_STATE.REQUEST_NOT_FOUND);
-    } else if (isNullOrUndefined(validationState)) {
-      setValidationState(VALIDATION_STATE.LOADING);
-      authenticateToken({
-        onError: onValidationError,
-        onSuccess: () => setValidationState(VALIDATION_STATE.VALIDATED),
-        token,
-      });
-    }
-  }, [setValidationState, validationState]);
-
-  return <div className={css(styles.authorClaimValidation)}>{pageBody}</div>;
+export default function AuthorClaimValidationPage() {
+  return <AuthorClaimValidation />;
 }
-
-const styles = StyleSheet.create({
-  authorClaimValidation: {
-    height: "100%",
-    padding: 16,
-    width: "100%",
-  },
-});
