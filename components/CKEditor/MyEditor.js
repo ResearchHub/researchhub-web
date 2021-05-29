9d75f56551b29924562a5db88c8ee884c27a5a6b
thomas-vu <thomas.vu@ucalgary.ca>
2021-05-28 20:23:21 -0600

    started ELN

---
+++ components/CKEditor/MyEditor.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, css } from "aphrodite";
+
+export function MyEditor() {
+  const editorRef = useRef();
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const { CKEditor, Editor } = editorRef.current || {};
+
+  const sidebarElementRef = useRef();
+  const presenceListElementRef = useRef();
+
+  const editorConfiguration = {
+    toolbar: [
+      "heading",
+      "|",
+      "fontsize",
+      "fontfamily",
+      "|",
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "removeFormat",
+      "highlight",
+      "|",
+      "alignment",
+      "|",
+      "numberedList",
+      "bulletedList",
+      "|",
+      "link",
+      "blockquote",
+      "imageUpload",
+      "insertTable",
+      "mediaEmbed",
+      "|",
+      "undo",
+      "redo",
+      "|",
+      "comment",
+      "|",
+      "trackChanges",
+    ],
+    cloudServices: {
+      tokenUrl:
+        "https://80957.cke-cs.com/token/dev/960c0bc0e3d16e2cd7f0526fa9839a08285b35601de07963c52090da8bf6",
+      uploadUrl: "https://80957.cke-cs.com/easyimage/upload/",
+      webSocketUrl: "wss://80957.cke-cs.com/ws",
+    },
+    collaboration: {
+      channelId: "2ncu8o1t3v8",
+    },
+    sidebar: {
+      container: sidebarElementRef.current,
+    },
+    presenceList: {
+      container: presenceListElementRef.current,
+    },
+  };
+
+  useEffect(() => {
+    editorRef.current = {
+      CKEditor: require("@ckeditor/ckeditor5-react").CKEditor,
+      Editor: require("ckeditor5-custom-build/build/ckeditor").Editor,
+    };
+    setEditorLoaded(true);
+    return () => {
+      //window.removeEventListener("resize", boundRefreshDisplayMode);
+      //window.removeEventListener("beforeunload", boundCheckPendingActions);
+    };
+  }, []);
+
+  const refreshDisplayMode = (editor) => {
+    const annotationsUIs = editor.plugins.get("AnnotationsUIs");
+    const sidebarElement = sidebarElementRef.current;
+
+    if (window.innerWidth < 1070) {
+      sidebarElement.classList.remove("narrow");
+      sidebarElement.classList.add("hidden");
+      annotationsUIs.switchTo("inline");
+    } else if (window.innerWidth < 1300) {
+      sidebarElement.classList.remove("hidden");
+      sidebarElement.classList.add("narrow");
+      annotationsUIs.switchTo("narrowSidebar");
+    } else {
+      sidebarElement.classList.remove("hidden", "narrow");
+      annotationsUIs.switchTo("wideSidebar");
+    }
+  };
+
+  const checkPendingActions = (editor, domEvt) => {
+    if (editor.plugins.get("PendingActions").hasAny) {
+      domEvt.preventDefault();
+      domEvt.returnValue = true;
+    }
+  };
+
+  const renderEditor = () => {
+    return (
+      <div className="row row-editor">
+        {editorLoaded && (
+          <CKEditor
+            onReady={(editor) => {
+              console.log("Editor is ready to use!", editor);
+
+              // Switch between inline and sidebar annotations according to the window size.
+              const boundRefreshDisplayMode = refreshDisplayMode.bind(
+                this,
+                editor
+              );
+              // Prevent closing the tab when any action is pending.
+              const boundCheckPendingActions = checkPendingActions.bind(
+                this,
+                editor
+              );
+
+              window.addEventListener("resize", boundRefreshDisplayMode);
+              window.addEventListener("beforeunload", boundCheckPendingActions);
+              refreshDisplayMode(editor);
+            }}
+            onChange={(event, editor) => console.log({ event, editor })}
+            editor={Editor}
+            config={editorConfiguration}
+            data={"hello world"}
+          />
+        )}
+        <div ref={sidebarElementRef} className="sidebar"></div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={css(styles.test)}>
+      <div className="centered">
+        <div className="row-presence">
+          <div ref={presenceListElementRef} className="presence"></div>
+        </div>
+        {renderEditor()}
+      </div>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  test: {},
+});
