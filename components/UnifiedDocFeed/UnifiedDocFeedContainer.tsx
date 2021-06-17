4dfcabe472557936bf8879109ef46030727f7064
Calvin Lee <calvinhlee23@gmail.com>
2021-06-16 23:51:29 -0700

    added killswitch with basic nav

---
+++ components/UnifiedDocFeed/UnifiedDocFeedContainer.tsx
@@ -0,0 +1,87 @@
+import { css, StyleSheet } from "aphrodite";
+import { NextRouter, useRouter } from "next/router";
+import React, { ReactElement, useEffect, useMemo, useState } from "react";
+import UnifiedDocFeedFilterButton from "./UnifiedDocFeedFilterButton";
+import {
+  UnifiedDocFilterLabels,
+  UnifiedDocFilters,
+} from "./constants/UnifiedDocFilters";
+import { ValueOf } from "../../config/types/root_types";
+
+const useCallbackFetchFeed = ({
+  currFilter,
+  setIsPageLoading,
+}: {
+  currFilter: string;
+  setIsPageLoading: Function;
+}): void => {
+  const filters = useEffect((): void => {
+    // make fetch call here.
+  });
+};
+
+export default function UnifiedDocFeedContainer(): ReactElement<"div"> {
+  const router = useRouter();
+  const [currFilter, setCurrFilter] = useState<string>(
+    // @ts-ignore - below has to be a string
+    router.query.filter || UnifiedDocFilters.ALL
+  );
+  const [isPageLoading, setIsPageLoading] = useState<boolean>(true);
+
+  useCallbackFetchFeed({ currFilter, setIsPageLoading });
+
+  const filterButtons = useMemo(() => {
+    return Object.keys(UnifiedDocFilters).map(
+      (filterKey): ReactElement<typeof UnifiedDocFeedFilterButton> => {
+        const filterValue = UnifiedDocFilters[filterKey];
+        return (
+          <UnifiedDocFeedFilterButton
+            key={filterKey}
+            onClick={(): void => {
+              setCurrFilter(filterValue);
+              router.push({
+                pathname: router.pathname,
+                query: { filter: filterValue },
+              });
+            }}
+            isActive={currFilter === filterValue}
+            label={UnifiedDocFilterLabels[filterKey]}
+          />
+        );
+      }
+    );
+  }, [currFilter]);
+
+  return (
+    <div className={css(styles.unifiedDocFeedContainer)}>
+      <div className={css(styles.buttonGroup)}>{filterButtons}</div>
+      <div>HI this is UnifiedDocFeedContainer</div>
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  unifiedDocFeedContainer: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    maxWidth: 1200,
+    width: "100%",
+    "@media only screen and (min-width: 1920px)": {
+      minWidth: 1200,
+    },
+    "@media only screen and (max-width: 990px)": {
+      width: "100%",
+    },
+    "@media only screen and (max-width: 415px)": {
+      padding: 0,
+      width: "100%",
+    },
+  },
+  buttonGroup: {
+    alignItems: "center",
+    display: "flex",
+    height: 120,
+    width: "100%",
+  },
+});
