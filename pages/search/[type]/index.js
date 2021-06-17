6cbabff2283c058dac12b2fdc63c462465e7127b
Kobe Attias <kobeattias@gmail.com>
2021-06-17 12:22:47 -0400

    Rendering counts

--- pages/search/[type]/index.js
+++ pages/search/[type]/index.js
@@ -4,67 +4,78 @@ import { AUTH_TOKEN } from "~/config/constants";
 
 import Link from "next/link";
 import Error from "next/error";
-import { pick } from "underscore";
+import { pick, keys } from "underscore";
 import { useRouter } from "next/router";
 
 import { get } from "lodash";
 import nookies from "nookies";
 
-const FILTERS = {
-  papers: ["hub", "start_date", "end_date", "post_type", "query"],
-  hubs: [],
-  authors: [],
+const SEARCH_TYPES = {
+  paper: ["hub", "start_date", "end_date", "post_type", "query"],
+  hub: [],
+  author: [],
 };
 
-const isAllowedSearchEntityType = (type) => FILTERS.hasOwnProperty(type);
+const isAllowedSearchEntityType = (type) => SEARCH_TYPES.hasOwnProperty(type);
 
-const getAllowedSearchFilters = ({ searchEntityType, queryParams }) => {
-  const allowedFilters = get(FILTERS, `${searchEntityType}`, []);
+const getAllowedSearchFilters = ({ searchType, queryParams }) => {
+  const allowedFilters = get(SEARCH_TYPES, `${searchType}`, []);
   return pick(queryParams, ...allowedFilters);
 };
 
-const Index = (props) => {
-  const router = useRouter();
-  const searchEntityType = get(router, "query.type");
+const Index = ({ resp }) => {
+  console.log(resp);
 
-  if (!isAllowedSearchEntityType(searchEntityType)) {
+  // console.log(JSON.stringify(resp, null, 2));
+
+  const router = useRouter();
+  const searchType = get(router, "query.type");
+
+  if (!isAllowedSearchEntityType(searchType)) {
     return <Error statusCode={404} />;
   }
 
-  return (
-    <div>
-      <Link href="/search/papers">
-        <a>Papers</a>
-      </Link>
-      <Link href="/search/authors">
-        <a>Authors</a>
-      </Link>
-      <Link href="/search/hubs">
-        <a>Hubs</a>
-      </Link>
-    </div>
-  );
+  const htmlLinks = keys(SEARCH_TYPES).map((type) => (
+    <Link href={`/search/${type}`} key={type}>
+      <a>
+        {type + "s"} [{get(resp, `${type}_count`, "")}]
+      </a>
+    </Link>
+  ));
+
+  return <div>{htmlLinks}</div>;
 };
 
-export const getServerSideProps = async (ctx) => {
+Index.getInitialProps = async (ctx) => {
   const cookies = nookies.get(ctx);
   const authToken = cookies[AUTH_TOKEN];
 
   const filters = getAllowedSearchFilters({
-    searchEntityType: ctx.query.type,
+    searchType: ctx.query.type,
     queryParams: ctx.query,
   });
 
   const config = {
-    route: "",
+    route: "all",
   };
 
   return fetch(API.SEARCH({ filters, config }), API.GET_CONFIG(authToken))
     .then(Helpers.checkStatus)
     .then(Helpers.parseJSON)
     .then((resp) => {
-      console.log(resp);
-      return { props: {} };
+      //       const initial = keys(SEARCH_TYPES).reduce((map, k) => { map[k] = []; return map } , {})
+      //
+      //       const hitsByIdxMap = resp.results.reduce((map, hit) => {
+      //         const idx = get(hit,"meta.index", "undefined");
+      //         if (!map[idx]) {
+      //           map[idx] = []
+      //         }
+      //
+      //         map[idx].push(hit);
+      //         return map
+      //       }, initial);
+
+      return { resp };
     });
 };
 
