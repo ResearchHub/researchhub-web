c4b88e241f192f7bb18262265dde33ef595962b9
Kobe Attias <kobeattias@gmail.com>
2021-06-16 21:51:13 -0400

    fetching results in server side

--- pages/search/[type]/index.js
+++ pages/search/[type]/index.js
@@ -1,54 +1,35 @@
-import HubPage from "~/components/Hubs/HubPage";
 import API from "~/config/api";
 import { Helpers } from "@quantfive/js-web-config";
-import parseUrl from "parse-url";
-import { useRouter } from "next/router";
-import { keys, has } from "underscore";
+import { AUTH_TOKEN } from "~/config/constants";
+
 import Link from "next/link";
 import Error from "next/error";
+import { pick } from "underscore";
+import { useRouter } from "next/router";
+
+import { get } from "lodash";
+import nookies from "nookies";
+
+const FILTERS = {
+  papers: ["hub", "start_date", "end_date", "post_type", "query"],
+  hubs: [],
+  authors: [],
+};
+
+const isAllowedSearchEntityType = (type) => FILTERS.hasOwnProperty(type);
+
+const getAllowedSearchFilters = ({ searchEntityType, queryParams }) => {
+  const allowedFilters = get(FILTERS, `${searchEntityType}`, []);
+  return pick(queryParams, ...allowedFilters);
+};
 
 const Index = (props) => {
   const router = useRouter();
+  const searchEntityType = get(router, "query.type");
 
-  const FILTERS = {
-    papers: ["hub", "start_date", "end_date", "post_type", "query"],
-    hubs: [],
-    users: [],
-  };
-
-  const getParamsForSearch = ({ entityType, url }) => {
-    const parsedUrl = parseUrl(url);
-    const params = {};
-
-    for (let i = 0; i < FILTERS[entityType].length; i++) {
-      const whiteListedFilter = FILTERS[entityType][i];
-
-      if (has(parsedUrl.query, whiteListedFilter)) {
-        params[whiteListedFilter] = parsedUrl.query[whiteListedFilter];
-      }
-    }
-
-    return params;
-  };
-
-  let queryParams = {};
-  if (typeof window !== "undefined") {
-    queryParams = getParamsForSearch({
-      entityType: router.query.type,
-      url: window.location.href,
-    });
+  if (!isAllowedSearchEntityType(searchEntityType)) {
+    return <Error statusCode={404} />;
   }
-
-  const config = {
-    route: "",
-  };
-
-  fetch(API.SEARCH({ queryParams, config }), API.GET_CONFIG())
-    .then(Helpers.checkStatus)
-    .then(Helpers.parseJSON)
-    .then((resp) => {
-      console.log(resp);
-    });
 
   return (
     <div>
@@ -65,8 +46,26 @@ const Index = (props) => {
   );
 };
 
-export async function getServerSideProps(context) {
-  return { props: {} };
-}
+export const getServerSideProps = async (ctx) => {
+  const cookies = nookies.get(ctx);
+  const authToken = cookies[AUTH_TOKEN];
+
+  const filters = getAllowedSearchFilters({
+    searchEntityType: ctx.query.type,
+    queryParams: ctx.query,
+  });
+
+  const config = {
+    route: "",
+  };
+
+  return fetch(API.SEARCH({ filters, config }), API.GET_CONFIG(authToken))
+    .then(Helpers.checkStatus)
+    .then(Helpers.parseJSON)
+    .then((resp) => {
+      console.log(resp);
+      return { props: {} };
+    });
+};
 
 export default Index;
