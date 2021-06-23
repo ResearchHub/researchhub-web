3f570095a404999d78cb2b4604a0d1ed023a550a
Kobe Attias <kobeattias@gmail.com>
2021-06-22 22:13:53 -0400

    Refactor Search into SearchResults component

--- pages/search/[type]/index.js
+++ pages/search/[type]/index.js
@@ -1,280 +1,43 @@
 import API from "~/config/api";
-import { Helpers } from "@quantfive/js-web-config";
 import { AUTH_TOKEN } from "~/config/constants";
-import FormSelect from "~/components/Form/FormSelect";
-import colors from "~/config/themes/colors";
-import Loader from "~/components/Loader/Loader";
-import { fetchURL } from "~/config/fetch";
-import PaperEntryCard from "~/components/Hubs/PaperEntryCard";
+import { Helpers } from "@quantfive/js-web-config";
+import SearchResults from "~/components/Search/SearchResults";
+import { searchTypes } from "~/config/utils/options";
 
-import Ripples from "react-ripples";
-import { useState, useEffect } from "react";
-import { StyleSheet, css } from "aphrodite";
-import Link from "next/link";
+import PropTypes from "prop-types";
 import Error from "next/error";
-import { pick, keys, isString, isArray } from "underscore";
+import { pick } from "underscore";
 import { useRouter } from "next/router";
-import * as moment from "dayjs";
-
-import { get, upperFirst } from "lodash";
+import { get } from "lodash";
 import nookies from "nookies";
-import parseUrl from "parse-url";
 
-const SEARCH_TYPES = {
-  paper: ["search", "hubs", "publish_date__gte", "post_type", "ordering"],
-  hub: ["ordering"],
-  author: ["ordering"],
-};
-
-const TIME_FILTER_OPTS = [
-  {
-    value: moment()
-      .startOf("day")
-      .format("YYYY-MM-DD"),
-    label: "Today",
-  },
-  {
-    value: moment()
-      .startOf("week")
-      .format("YYYY-MM-DD"),
-    label: "This Week",
-  },
-  {
-    value: moment()
-      .startOf("month")
-      .format("YYYY-MM-DD"),
-    label: "This Month",
-  },
-  {
-    value: moment()
-      .startOf("year")
-      .format("YYYY-MM-DD"),
-    label: "This Year",
-  },
-  {
-    value: null,
-    label: "All Time",
-  },
-];
-
-const SORT_OPTS = [
-  {
-    value: null,
-    label: "Relevance",
-  },
-  {
-    value: "-hot",
-    label: "Trending",
-  },
-  {
-    value: "-popularity",
-    label: "Top Rated",
-  },
-  {
-    value: "-publish_date",
-    label: "Newest",
-  },
-  {
-    value: "-discussion_count",
-    label: "Most Discussed",
-  },
-];
-
-const isAllowedSearchEntityType = (type) => SEARCH_TYPES.hasOwnProperty(type);
+const isAllowedSearchEntityType = (type) => searchTypes.hasOwnProperty(type);
 
 const getAllowedSearchFilters = ({ searchType, queryParams }) => {
-  const allowedFilters = get(SEARCH_TYPES, `${searchType}`, []);
+  const allowedFilters = get(searchTypes, `${searchType}`, []);
   return pick(queryParams, ...allowedFilters);
 };
 
-const Index = ({ currentSearchResponse }) => {
+// Facets specified will have their values returned
+// alongside counts in the search response.
+const getFacetsToAggregate = (query = {}) => {
+  let facet = [];
+  if (query.type.indexOf("paper") >= 0) {
+    facet = ["hubs"];
+  }
+
+  return facet;
+};
+
+const Index = ({ searchResultsResponse }) => {
   const router = useRouter();
   const currentSearchType = get(router, "query.type");
-
-  const [isLoadingMore, setIsLoadingMore] = useState(false);
-  const [nextResultsUrl, setNextResultsUrl] = useState(null);
-  const [numOfHits, setNumOfHIts] = useState(0);
-  const [results, setResults] = useState([]);
-
-  const [selectedHubs, setSelectedHubs] = useState([]);
-  const [selectedTimeRange, setSelectedTimeRange] = useState(null);
-  const [selectedSortOrder, setSelectedSortOrder] = useState(null);
-
-  useEffect(() => {
-    setSelectedHubs(getSelectedFacetValues({ forKey: "hubs" }));
-    setSelectedTimeRange(
-      getSelectedDropdownValue({ forKey: "publish_date__gte" })
-    );
-    setSelectedSortOrder(getSelectedDropdownValue({ forKey: "ordering" }));
-  }, [router.query]);
-
-  useEffect(() => {
-    setResults(get(currentSearchResponse, "results"));
-    setNextResultsUrl(get(currentSearchResponse, "next"));
-    setNumOfHIts(get(currentSearchResponse, "count"));
-  }, [currentSearchResponse]);
-
-  const loadMoreResults = () => {
-    setIsLoadingMore(true);
-
-    fetchURL(nextResultsUrl)
-      .then((res) => {
-        setResults([...results, ...res.results]);
-      })
-      .finally(() => {
-        setIsLoadingMore(false);
-      });
-  };
-
-  const getSelectedDropdownValue = ({ forKey }) => {
-    const urlParam = get(router, `query.${forKey}`, null);
-
-    if (forKey === "publish_date__gte") {
-      return TIME_FILTER_OPTS.find((opt) => opt.value === urlParam);
-    } else if (forKey === "ordering") {
-      return SORT_OPTS.find((opt) => opt.value === urlParam);
-    }
-
-    return null;
-  };
-
-  const getSelectedFacetValues = ({ forKey }) => {
-    let selected = [];
-    if (isArray(router.query[forKey])) {
-      selected = router.query[forKey];
-    } else if (isString(router.query[forKey])) {
-      selected = [router.query[forKey]];
-    }
-
-    return selected.map((v) => ({ label: v, value: v }));
-  };
-
-  const handleFilterSelect = (filterId, selected) => {
-    let query = {
-      ...router.query,
-    };
-
-    if (isArray(selected)) {
-      query[filterId] = selected.map((v) => v.value);
-    } else if (!selected || !selected.value) {
-      delete query[filterId];
-    } else {
-      query[filterId] = selected.value;
-    }
-
-    router.push({
-      pathname: "/search/[type]",
-      query,
-    });
-  };
-
-  const renderEntityTabs = () => {
-    return keys(SEARCH_TYPES).map((type) => (
-      <Link href={`/search/${type}`} key={type}>
-        {type === currentSearchType ? (
-          <a>
-            {type + "s"} [{numOfHits}]
-          </a>
-        ) : (
-          <a>{type + "s"}</a>
-        )}
-      </Link>
-    ));
-  };
-
-  const renderLoadMoreButton = () => {
-    if (nextResultsUrl !== null) {
-      return (
-        <div className={css(styles.buttonContainer)}>
-          {!isLoadingMore ? (
-            <Ripples
-              className={css(styles.loadMoreButton)}
-              onClick={loadMoreResults}
-            >
-              Load More Papers
-            </Ripples>
-          ) : (
-            <Loader
-              key={"paperLoader"}
-              loading={true}
-              size={25}
-              color={colors.BLUE()}
-            />
-          )}
-        </div>
-      );
-    }
-  };
-
-  const availFacetOpts = get(
-    currentSearchResponse,
-    "facets._filter_hubs.hubs.buckets",
-    []
-  ).map((b) => ({
-    label: `${b.key} (${b.doc_count})`,
-    value: b.key,
-  }));
 
   if (!isAllowedSearchEntityType(currentSearchType)) {
     return <Error statusCode={404} />;
   }
 
-  const entityTabsHtml = renderEntityTabs();
-  const loadMoreBtn = renderLoadMoreButton();
-
-  return (
-    <div>
-      <FormSelect
-        id={"hubs"}
-        options={availFacetOpts}
-        containerStyle={null}
-        inputStyle={null}
-        onChange={handleFilterSelect}
-        isSearchable={true}
-        placeholder={"Hubs"}
-        value={selectedHubs}
-        isMulti={true}
-        multiTagStyle={null}
-        multiTagLabelStyle={null}
-        isClearable={true}
-      />
-      <FormSelect
-        id={"publish_date__gte"}
-        options={TIME_FILTER_OPTS}
-        containerStyle={null}
-        inputStyle={null}
-        onChange={handleFilterSelect}
-        isSearchable={true}
-        placeholder={"Date Published"}
-        value={selectedTimeRange}
-        isMulti={false}
-        multiTagStyle={null}
-        multiTagLabelStyle={null}
-        isClearable={true}
-      />
-      <FormSelect
-        id={"ordering"}
-        options={SORT_OPTS}
-        value={selectedSortOrder}
-        containerStyle={null}
-        inputStyle={null}
-        onChange={handleFilterSelect}
-        isSearchable={false}
-      />
-      {entityTabsHtml}
-      {results.map((paper, index) => {
-        return (
-          <PaperEntryCard
-            paper={paper}
-            index={index}
-            key={paper.id}
-            voteCallback={() => null}
-          />
-        );
-      })}
-      {loadMoreBtn}
-    </div>
-  );
+  return <SearchResults initialResults={searchResultsResponse} />;
 };
 
 Index.getInitialProps = async (ctx) => {
@@ -286,10 +49,7 @@ Index.getInitialProps = async (ctx) => {
     queryParams: ctx.query,
   });
 
-  let facet = [];
-  if (ctx.query.type.indexOf("paper") >= 0) {
-    facet = ["hubs"];
-  }
+  const facet = getFacetsToAggregate(ctx.query);
 
   const config = {
     route: ctx.query.type,
@@ -301,42 +61,13 @@ Index.getInitialProps = async (ctx) => {
   )
     .then(Helpers.checkStatus)
     .then(Helpers.parseJSON)
-    .then((currentSearchResponse) => {
-      return { currentSearchResponse };
+    .then((searchResultsResponse) => {
+      return { searchResultsResponse };
     });
 };
 
-const styles = StyleSheet.create({
-  buttonContainer: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 25,
-    height: 45,
-    "@media only screen and (max-width: 768px)": {
-      marginTop: 15,
-      marginBottom: 15,
-    },
-  },
-  loadMoreButton: {
-    fontSize: 14,
-    border: `1px solid ${colors.BLUE()}`,
-    boxSizing: "border-box",
-    borderRadius: 4,
-    height: 45,
-    width: 155,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: colors.BLUE(),
-    cursor: "pointer",
-    userSelect: "none",
-    ":hover": {
-      color: "#FFF",
-      backgroundColor: colors.BLUE(),
-    },
-  },
-});
+Index.propTypes = {
+  searchResultsResponse: PropTypes.object,
+};
 
 export default Index;
