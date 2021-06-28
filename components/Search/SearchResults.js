4c6e17410aeb4e1b5b985808b87f8bc562576a12
Kobe Attias <kobeattias@gmail.com>
2021-06-28 16:36:49 -0400

    Fixing missing valueForApi for sort

--- components/Search/SearchResults.js
+++ components/Search/SearchResults.js
@@ -56,22 +56,27 @@ const timeFilterOpts = [
 
 const sortOpts = [
   {
+    valueForApi: null,
     value: null,
     label: "Relevance",
   },
   {
+    valueForApi: "-hot_score",
     value: "-hot_score",
     label: "Trending",
   },
   {
+    valueForApi: "-score",
     value: "-score",
     label: "Top Rated",
   },
   {
+    valueForApi: "-publish_date",
     value: "-publish_date",
     label: "Newest",
   },
   {
+    valueForApi: "-discussion_count",
     value: "-discussion_count",
     label: "Most Discussed",
   },
