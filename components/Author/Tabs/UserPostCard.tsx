2d9fcd5b316b6422934e15e1a231132c00f21f6f
thomas-vu <thomas.vu@ucalgary.ca>
2021-07-01 15:44:54 -0600

    constrained ripple effect to post card when it's clicked

--- components/Author/Tabs/UserPostCard.tsx
+++ components/Author/Tabs/UserPostCard.tsx
@@ -299,7 +299,6 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     borderRadius: 3,
-    overflow: "visible",
     ":hover": {
       backgroundColor: "#FAFAFA",
     },
