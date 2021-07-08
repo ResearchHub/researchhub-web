4901ed69daa303ec1150eeef73363dfd39437a4e
Calvin Lee <calvinhlee23@gmail.com>
2021-07-08 15:09:46 -0700

    first commit

---
+++ components/Paper/Upload/PaperUploadV2.tsx
@@ -0,0 +1,15 @@
+import { css, StyleSheet } from "aphrodite";
+import React, { Fragment, ReactElement } from "react";
+
+type Props = {};
+
+function PaperUploadV2({  }: Props): ReactElement<"div"> {
+  return (
+    <div>
+      Hi this is paper upload v2
+      <a>nice to meet you</a>
+    </div>
+  );
+}
+
+export default PaperUploadV2;
