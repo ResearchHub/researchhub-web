1bbe67bc75201d3704a3f564d6e571e222fb008d
Calvin Lee <calvinhlee23@gmail.com>
2021-07-11 23:43:11 -0700

    setting

--- components/Paper/Upload/PaperUploadV2Create.tsx
+++ components/Paper/Upload/PaperUploadV2Create.tsx
@@ -1,6 +1,10 @@
 import { css, StyleSheet } from "aphrodite";
-import React, { Fragment, ReactElement, useEffect } from "react";
+import React, { Fragment, ReactElement, useEffect, useState } from "react";
+import { useEffectFetchSuggestedHubs } from "./api/useEffectGetSuggestedHubs";
 
-export default function PaperuploadV2Create() {
-  return <div></div>;
+export default function PaperuploadV2Create(): ReactElement<typeof Fragment> {
+  const [suggestedHubs, setSuggestedHubs] = useState<any>(null);
+  useEffectFetchSuggestedHubs({ setSuggestedHubs });
+
+  return <Fragment>Hi this is Create</Fragment>;
 }
