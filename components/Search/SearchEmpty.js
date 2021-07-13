46f38c1700b6323c28de78d49a46972a5b7331a2
Kobe Attias <kobeattias@gmail.com>
2021-07-13 17:30:53 -0400

    Rendering search highlights for posts

--- components/Search/SearchEmpty.js
+++ components/Search/SearchEmpty.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 import colors from "~/config/themes/colors";
 import { breakpoints } from "~/config/themes/screen";
 
-const SearchEmpty = ({ title }) => {
+const SearchEmpty = ({ title, subtitle }) => {
   return (
     <div className={css(styles.wrapper)}>
       <img
@@ -14,7 +14,8 @@ const SearchEmpty = ({ title }) => {
         loading="lazy"
         alt={title}
       />
-      <span className={css(styles.emptyPlaceholderText)}>{title}</span>
+      <span className={css(styles.title)}>{title}</span>
+      {subtitle && <span className={css(styles.subtitle)}>{subtitle}</span>}
     </div>
   );
 };
@@ -35,13 +36,26 @@ const styles = StyleSheet.create({
       width: "70%",
     },
   },
-  emptyPlaceholderText: {
+  title: {
     textAlign: "center",
     fontSize: 22,
-    color: "#241F3A",
+    color: colors.BLACK(0.8),
     marginTop: 20,
     [`@media only screen and (max-width: ${breakpoints.medium.str})`]: {
       fontSize: 16,
+    },
+    [`@media only screen and (max-width: ${breakpoints.xsmall.str})`]: {
+      width: "85%",
+    },
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    color: colors.BLACK(0.5),
+    marginTop: 10,
+    marginBottom: 15,
+    [`@media only screen and (max-width: ${breakpoints.medium.str})`]: {
+      fontSize: 14,
     },
     [`@media only screen and (max-width: ${breakpoints.xsmall.str})`]: {
       width: "85%",
@@ -51,6 +65,7 @@ const styles = StyleSheet.create({
 
 SearchEmpty.propTypes = {
   title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
 };
 
 SearchEmpty.defaultProps = {
