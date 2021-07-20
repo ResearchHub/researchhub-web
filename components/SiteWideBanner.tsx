8a33c35bc5faeff71d228e1c205e644ec3fdb3d2
Calvin Lee <calvinhlee23@gmail.com>
2021-07-20 15:33:45 -0700

    abc

--- components/SiteWideBanner.tsx
+++ components/SiteWideBanner.tsx
@@ -32,27 +32,43 @@ export default function SiteWideBanner({
 
 const styles = StyleSheet.create({
   siteWideBanner: {
-    display: "flex",
-    width: "100%",
-    height: 120,
-    boxSizing: "border-box",
     backgroundColor: "rgb(65 114 239)",
+    boxSizing: "border-box",
+    display: "flex",
     fontFamily: "Roboto",
+    height: 120,
+    width: "100%",
+  },
+  bannerImg: {
+    height: 116,
+    maxHeight: "100%",
+    width: 330,
+  },
+  bodyText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: 400,
+    "@media only screen and (max-width: 1199px)": {
+      fontSize: 14,
+    },
+    "@media only screen and (max-width: 1399px)": {
+      fontSize: 15,
+    },
   },
   contentWrap: {
-    display: "flex",
-    justifyContent: "space-between",
     alignItems: "center",
+    display: "flex",
     height: "100%",
+    justifyContent: "space-between",
     margin: "auto",
     width: "80%",
-    "@media only screen and (max-width: 1399px)": {
-      width: "90%",
-    },
     "@media only screen and (max-width: 1199px)": {
       margin: "none",
       padding: "0 12px",
       width: "100%",
+    },
+    "@media only screen and (max-width: 1399px)": {
+      width: "90%",
     },
   },
   headerText: {
@@ -60,45 +76,29 @@ const styles = StyleSheet.create({
     fontWeight: 500,
     fontSize: 24,
     marginBottom: 16,
-    "@media only screen and (max-width: 1399px)": {
-      fontSize: 22,
-    },
     "@media only screen and (max-width: 1199px)": {
       fontSize: 18,
     },
-  },
-  bodyText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: 400,
     "@media only screen and (max-width: 1399px)": {
-      fontSize: 15,
+      fontSize: 22,
     },
-    "@media only screen and (max-width: 1199px)": {
-      fontSize: 14,
-    },
-  },
-  textSection: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    width: "calc(100% - 330px)",
-    padding: 16,
-    "@media only screen and (max-width: 767px)": {
-      width: "100%",
-      padding: "0 16px",
-    },
-  },
-  bannerImg: {
-    height: 116,
-    maxHeight: "100%",
-    width: 330,
   },
   imgWrap: {
     display: "unset",
     "@media only screen and (max-width: 767px)": {
       // ipad-size
       display: "none",
+    },
+  },
+  textSection: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 16,
+    width: "calc(100% - 330px)",
+    "@media only screen and (max-width: 767px)": {
+      width: "100%",
+      padding: "0 16px",
     },
   },
 });
