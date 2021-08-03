ec2fd1e38f49de9e96576a7f65ad82f1b2854bf1
thomas-vu <thomas.vu@ucalgary.ca>
2021-08-03 14:50:00 -0600

    started hypothesis feature

---
+++ pages/hypothesis/[documentId]/index.js
@@ -0,0 +1,41 @@
+import { Fragment } from "react";
+import Router from "next/router";
+import Error from "next/error";
+import Loader from "~/components/Loader/Loader";
+import API from "~/config/api";
+
+// Redux
+import helpers from "@quantfive/js-web-config/helpers";
+
+function Hypothesis(props) {
+  // TODO: Does this need to be a dynamic route or hard refresh?
+  if (props.error) {
+    return <Error statusCode={404} />;
+  }
+
+  if (props.redirectPath && typeof window !== "undefined") {
+    Router.push(props.redirectPath);
+  }
+
+  return (
+    <Error title={<Fragment>Redirecting to page</Fragment>} statusCode={301} />
+  );
+}
+
+Hypothesis.getInitialProps = async (ctx) => {
+  const { store, res, query } = ctx;
+  //let hypotheses = await fetch(
+  //  API.HYPOTHESIS({ hypothesis_id: query.documentId }),
+  //  API.GET_CONFIG()
+  //).then(helpers.parseJSON);
+  //const hypothesis = hypotheses.results[0];
+  //const slug = hypothesis.slug;
+
+  //const redirectPath = `/hypothesis/${hypothesis.id}/${slug}`;
+  const redirectPath = `/hypothesis/1/test`;
+
+  //return { redirectPath, hypothesis };
+  return { redirectPath };
+};
+
+export default Hypothesis;
