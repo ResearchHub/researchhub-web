17ce4ad6f9291b250a0429dcfa39ea683167ed6c
thomas-vu <thomas.vu@ucalgary.ca>
2021-08-05 18:54:29 -0600

    fixed "Prop `style` did not match" error

--- components/AuthorAvatar.js
+++ components/AuthorAvatar.js
@@ -3,10 +3,17 @@ import { useState } from "react";
 import Link from "next/link";
 
 import icons from "~/config/themes/icons";
-import colors from "../config/themes/colors";
+import colors from "~/config/themes/colors";
+import { useHasMounted } from "~/config/utils/hooks";
 
 const AuthorAvatar = (props) => {
   const [error, setError] = useState(false);
+  const hasMounted = useHasMounted();
+
+  if (!hasMounted) {
+    return null;
+  }
+
   const {
     author,
     size = 30,
