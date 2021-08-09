2696db83a4cef711a949256afbc0e6303eb2e935
Calvin Lee <calvinhlee23@gmail.com>
2021-08-09 12:23:13 -0700

    setting up structure

--- components/Hypothesis/Citation/Citation.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from "react";
-
-type Props = {};
-
-export default function Citation({  }: Props): ReactElement<"div"> {
-  return <div>{"hihihi"}</div>;
-}
--- components/Hypothesis/Citation/CitationContainer.tsx
+++ components/Hypothesis/Citation/CitationContainer.tsx
@@ -1,9 +1,31 @@
+import { css, StyleSheet } from "aphrodite";
 import React, { ReactElement } from "react";
+import CitationAddNewButton from "./CitationAddNewButton";
+import CitationTable from "./CitationTable";
 
 export default function CitationContainer(): ReactElement<"div"> {
   return (
-    <div>
-      <Citation />
+    <div className={css(styles.citationContainer)}>
+      <div className={css(styles.header)}>Relevant Papers</div>
+      <CitationTable />
+      <CitationAddNewButton />
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  citationContainer: {
+    border: "1px solid red",
+    borderRadius: 3,
+    display: "flex",
+    flexDirection: "column",
+    height: 524,
+    padding: 16,
+    width: 1278,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 500,
+    width: "100%",
+  },
+});
