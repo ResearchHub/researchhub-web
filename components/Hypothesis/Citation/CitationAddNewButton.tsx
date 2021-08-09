f746d59d1d4b2121aa100299268ff71708c70e23
Calvin Lee <calvinhlee23@gmail.com>
2021-08-09 12:23:13 -0700

    setting up structure

---
+++ components/Hypothesis/Citation/CitationAddNewButton.tsx
@@ -0,0 +1,8 @@
+import { css, StyleSheet } from "aphrodite";
+import React, { ReactElement } from "react";
+
+type Props = {};
+
+export default function CitationAddNewButton({  }: Props): ReactElement<"div"> {
+  return <div>Add New</div>;
+}
