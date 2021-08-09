2dcabf92acc1f13b92f884cc60a0b0d0d40f156f
Calvin Lee <calvinhlee23@gmail.com>
2021-08-09 11:40:19 -0700

    setup for thomas

--- pages/hypothesis/[documentId]/[title]/index.js
+++ pages/hypothesis/[documentId]/[title]/index.js
@@ -1,22 +1,13 @@
-import React, { Fragment } from "react";
-import { StyleSheet, css } from "aphrodite";
+import React from "react";
 import Router from "next/router";
 import killswitch from "~/config/killswitch/killswitch";
 
 const isServer = () => typeof window === "undefined";
 
-const Hypothesis = (props) => {
+export default function Hypothesis(props) {
   if (!killswitch("hypothesis") && !isServer()) {
     Router.push("/");
   }
 
-  return <div className={css(styles.container)}>{/* TODO */}</div>;
-};
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-  },
-});
-
-export default Hypothesis;
+  return <HypothesisContainer />;
+}
