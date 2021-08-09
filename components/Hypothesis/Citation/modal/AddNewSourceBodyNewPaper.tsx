488cd7c4166e7ea80a0a461f837331dafa6490be
Calvin Lee <calvinhlee23@gmail.com>
2021-08-09 14:37:45 -0700

    standby done

---
+++ components/Hypothesis/Citation/modal/AddNewSourceBodyNewPaper.tsx
@@ -0,0 +1,6 @@
+import { css, StyleSheet } from "aphrodite";
+import React, { ReactElement, useState } from "react";
+
+export default function AddNewSourceBodyNewPaper() {
+  return <div>NewPaper Placeholder</div>;
+}
