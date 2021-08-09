769a6429ebb00fcf59d2c27ddb6104a2c40f12ae
Calvin Lee <calvinhlee23@gmail.com>
2021-08-09 13:49:59 -0700

    ..

--- components/Hypothesis/Citation/modal/AddNewSourceBodyStandBy.tsx
+++ components/Hypothesis/Citation/modal/AddNewSourceBodyStandBy.tsx
@@ -1,8 +1,12 @@
 import { css, StyleSheet } from "aphrodite";
-import { NEW_SOURCE_BODY_TYPES } from "./modalBodyTypes";
 import { ValueOf } from "../../../../config/types/root_types";
 import BaseModal from "../../../Modals/BaseModal";
 import React, { ReactElement, useState } from "react";
+import { BodyTypeVals } from "./modalBodyTypes";
+
+type Props = {
+  onSelectType: (bodyType: BodyTypeVals) => void;
+};
 
 export default function AddNewSourceBodyStandBy() {
   return <div></div>;
--- components/Hypothesis/Citation/modal/modalBodyTypes.tsx
+++ components/Hypothesis/Citation/modal/modalBodyTypes.tsx
@@ -1,5 +1,9 @@
+import { ValueOf } from "../../../../config/types/root_types";
+
 export const NEW_SOURCE_BODY_TYPES = {
   NEW_PAPAER_UPLOAD: "NEW_PAPAER_UPLOAD",
   SEARCH: "SEARCH",
   STAND_BY: "STAND_BY",
 };
+
+export type BodyTypeVals = ValueOf<typeof NEW_SOURCE_BODY_TYPES>;
