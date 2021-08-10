50b07e70cb701f64b8dfe9868e833c67ec9cac00
Calvin Lee <calvinhlee23@gmail.com>
2021-08-09 17:38:55 -0700

    mocked

--- components/Hypothesis/Citation/table/CitationTableRowItem.tsx
+++ components/Hypothesis/Citation/table/CitationTableRowItem.tsx
@@ -1,7 +1,65 @@
+import { css, StyleSheet } from "aphrodite";
 import React, { ReactElement } from "react";
+import colors from "../../../../config/themes/colors";
+import { ID } from "../../../../config/types/root_types";
+import { tableWidths } from "./constants/tableWidths";
 
-export type CitationTableRowItemProps = {};
+export type CitationTableRowItemProps = {
+  citationID: ID;
+  consensus: number;
+  notes: string;
+  source: string;
+  type: string;
+  year: string;
+};
 
-export default function CitationTableRowItem(): ReactElement<"div"> {
-  return <div>ROW</div>;
+type ItemColumnProps = {
+  value: number | string;
+  width: string;
+};
+
+function ItemColumn({ value, width }) {
+  return (
+    <div
+      className={css(styles.itemColumn)}
+      style={{ maxWidth: width, minWidth: width, width }}
+    >
+      {value}
+    </div>
+  );
 }
+
+export default function CitationTableRowItem({
+  consensus,
+  notes,
+  source,
+  type,
+  year,
+}: CitationTableRowItemProps): ReactElement<"div"> {
+  return (
+    <div className={css(styles.tableRowItem)}>
+      <ItemColumn value={source} width={tableWidths.SOURCE} />
+      <ItemColumn value={type} width={tableWidths.TYPE} />
+      <ItemColumn value={year} width={tableWidths.YEAR} />
+      <ItemColumn value={consensus} width={tableWidths.CONSENSUS} />
+      <ItemColumn value={notes} width={tableWidths.NOTES} />
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemColumn: {
+    // NOTE: this needs to match headerItem of CitationTableHeaderItem
+    alignItems: "center",
+    display: "flex",
+    fontSize: 14,
+    height: 56,
+    justifyContent: "flex-start",
+    padding: "0 8px",
+  },
+  tableRowItem: {
+    borderBottom: `1px solid ${colors.GREY(1)}`,
+    display: "flex",
+    width: "100%",
+  },
+});
