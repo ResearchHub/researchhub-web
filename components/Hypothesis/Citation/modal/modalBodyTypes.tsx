07c00514bcb6af8573919f2e06a77f4cbaa98344
Calvin Lee <calvinhlee23@gmail.com>
2021-08-10 11:47:01 -0700

    update typo

--- components/Hypothesis/Citation/modal/modalBodyTypes.tsx
+++ components/Hypothesis/Citation/modal/modalBodyTypes.tsx
@@ -1,7 +1,7 @@
 import { ValueOf } from "../../../../config/types/root_types";
 
 export const NEW_SOURCE_BODY_TYPES = {
-  NEW_PAPAER_UPLOAD: "NEW_PAPAER_UPLOAD",
+  NEW_PAPER_UPLOAD: "NEW_PAPER_UPLOAD",
   SEARCH: "SEARCH",
   STAND_BY: "STAND_BY",
 };
