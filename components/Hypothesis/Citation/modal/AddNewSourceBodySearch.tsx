949be25c105e18c97195c1a6537d05fdd2a44267
Calvin Lee <calvinhlee23@gmail.com>
2021-08-12 17:06:35 -0700

    ..

--- components/Hypothesis/Citation/modal/AddNewSourceBodySearch.tsx
+++ components/Hypothesis/Citation/modal/AddNewSourceBodySearch.tsx
@@ -2,5 +2,81 @@ import { css, StyleSheet } from "aphrodite";
 import React, { ReactElement, useState } from "react";
 
 export default function AddNewSourceBodySearch() {
-  return <div>AddNewSourceBodySearch</div>;
+  return (
+    <div className={css(styles.addNewSourceBodySearch)}>
+      <img
+        className={css(styles.bannerImg)}
+        src="/static/icons/site-wide-banner.png"
+      />
+      <div className={css(styles.textSection)}>
+        <div className={css(styles.headerText)}>
+          {"This Feature is still work in progress"}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const styles = StyleSheet.create({
+  addNewSourceBodySearch: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    height: 200,
+    margin: "30px 24px 16px",
+    justifyContent: "center",
+    width: 400,
+  },
+  headerText: {
+    alignItems: "center",
+    display: "flex",
+    fontSize: 20,
+    fontWeight: 500,
+    justifyContent: "center",
+    marginBottom: 16,
+    width: "100%",
+    "@media only screen and (max-width: 1199px)": {
+      fontSize: 18,
+    },
+    "@media only screen and (max-width: 1399px)": {
+      fontSize: 22,
+    },
+  },
+  textSection: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: 12,
+    height: "70%",
+    width: "100%",
+    boxSizing: "border-box",
+    "@media only screen and (max-width: 767px)": {
+      width: "100%",
+      padding: "12px 0px",
+    },
+  },
+  bannerImg: {
+    width: 330,
+    objectFit: "contain",
+  },
+  bodyText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: 400,
+    "@media only screen and (max-width: 1199px)": {
+      fontSize: 14,
+    },
+    "@media only screen and (max-width: 1399px)": {
+      fontSize: 15,
+    },
+  },
+  imgWrap: {
+    display: "unset",
+    height: "100%",
+    width: "100%",
+    "@media only screen and (max-width: 767px)": {
+      // ipad-size
+      display: "none",
+    },
+  },
+});
