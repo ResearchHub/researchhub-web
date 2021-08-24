9f877f5c7945700ae6ce7a814da51a9aab5be9e7
Kobe Attias <kobeattias@gmail.com>
2021-08-24 13:58:16 -0400

    CTX: Latex, fixing indentation

--- config/utils/latex.js
+++ config/utils/latex.js
@@ -1,106 +1,104 @@
-import { InlineMath, BlockMath } from 'react-katex';
-
+import { InlineMath } from 'react-katex';
 
 // Imported from katex package since it cannot be imported directly
 const findEndOfMath = function(delimiter, text, startIndex) {
-    // Adapted from
-    // https://github.com/Khan/perseus/blob/master/src/perseus-markdown.jsx
-    let index = startIndex;
-    let braceLevel = 0;
+  // Adapted from
+  // https://github.com/Khan/perseus/blob/master/src/perseus-markdown.jsx
+  let index = startIndex;
+  let braceLevel = 0;
 
-    const delimLength = delimiter.length;
+  const delimLength = delimiter.length;
 
-    while (index < text.length) {
-        const character = text[index];
+  while (index < text.length) {
+    const character = text[index];
 
-        if (braceLevel <= 0 &&
-            text.slice(index, index + delimLength) === delimiter) {
-            return index;
-        } else if (character === "\\") {
-             index++;
-        } else if (character === "{") {
-            braceLevel++;
-        } else if (character === "}") {
-            braceLevel--;
-        }
-
-        index++;
+    if (braceLevel <= 0 &&
+        text.slice(index, index + delimLength) === delimiter) {
+        return index;
+    } else if (character === "\\") {
+         index++;
+    } else if (character === "{") {
+        braceLevel++;
+    } else if (character === "}") {
+        braceLevel--;
     }
 
-    return -1;
+    index++;
+  }
+
+  return -1;
 };
 
 // Imported from katex package since it cannot be imported directly
 const splitAtDelimiters = function(startData, leftDelim, rightDelim, display) {
-    const finalData = [];
+  const finalData = [];
 
-    for (let i = 0; i < startData.length; i++) {
-        if (startData[i].type === "text") {
-            const text = startData[i].data;
+  for (let i = 0; i < startData.length; i++) {
+    if (startData[i].type === "text") {
+      const text = startData[i].data;
 
-            let lookingForLeft = true;
-            let currIndex = 0;
-            let nextIndex;
+      let lookingForLeft = true;
+      let currIndex = 0;
+      let nextIndex;
 
-            nextIndex = text.indexOf(leftDelim);
-            if (nextIndex !== -1) {
-                currIndex = nextIndex;
-                finalData.push({
-                    type: "text",
-                    data: text.slice(0, currIndex),
-                });
-                lookingForLeft = false;
-            }
+      nextIndex = text.indexOf(leftDelim);
+      if (nextIndex !== -1) {
+        currIndex = nextIndex;
+        finalData.push({
+            type: "text",
+            data: text.slice(0, currIndex),
+        });
+        lookingForLeft = false;
+      }
 
-            while (true) {
-                if (lookingForLeft) {
-                    nextIndex = text.indexOf(leftDelim, currIndex);
-                    if (nextIndex === -1) {
-                        break;
-                    }
+      while (true) {
+        if (lookingForLeft) {
+          nextIndex = text.indexOf(leftDelim, currIndex);
+          if (nextIndex === -1) {
+              break;
+          }
 
-                    finalData.push({
-                        type: "text",
-                        data: text.slice(currIndex, nextIndex),
-                    });
+          finalData.push({
+              type: "text",
+              data: text.slice(currIndex, nextIndex),
+          });
 
-                    currIndex = nextIndex;
-                } else {
-                    nextIndex = findEndOfMath(
-                        rightDelim,
-                        text,
-                        currIndex + leftDelim.length);
-                    if (nextIndex === -1) {
-                        break;
-                    }
-
-                    finalData.push({
-                        type: "math",
-                        data: text.slice(
-                            currIndex + leftDelim.length,
-                            nextIndex),
-                        rawData: text.slice(
-                            currIndex,
-                            nextIndex + rightDelim.length),
-                        display: display,
-                    });
-
-                    currIndex = nextIndex + rightDelim.length;
-                }
-
-                lookingForLeft = !lookingForLeft;
-            }
-
-            finalData.push({
-                type: "text",
-                data: text.slice(currIndex),
-            });
+          currIndex = nextIndex;
         } else {
-            finalData.push(startData[i]);
-        }
-    }
+          nextIndex = findEndOfMath(
+            rightDelim,
+            text,
+            currIndex + leftDelim.length);
+          if (nextIndex === -1) {
+            break;
+          }
 
-    return finalData;
+          finalData.push({
+            type: "math",
+            data: text.slice(
+              currIndex + leftDelim.length,
+              nextIndex),
+            rawData: text.slice(
+              currIndex,
+              nextIndex + rightDelim.length),
+            display: display,
+          });
+
+          currIndex = nextIndex + rightDelim.length;
+        }
+          lookingForLeft = !lookingForLeft;
+        }
+
+        finalData.push({
+          type: "text",
+          data: text.slice(currIndex),
+        });
+    } else {
+      finalData.push(startData[i]);
+    }
+  }
+
+  return finalData;
 };
 
 export const parseMath = (str) => {
