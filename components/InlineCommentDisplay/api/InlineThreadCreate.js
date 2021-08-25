44e5376a81cd675751aa7ba1bac2b9bf96fc5e00
Calvin Lee <calvinhlee23@gmail.com>
2021-08-25 14:31:39 -0700

    abc

--- components/InlineCommentDisplay/api/InlineThreadCreate.js
+++ components/InlineCommentDisplay/api/InlineThreadCreate.js
@@ -14,8 +14,8 @@ export function saveThreadToBackend({
 }) {
   fetch(
     API.DISCUSSION({
-      documentType: "paper",
       documentId: params.paper,
+      documentType: "paper",
       source: INLINE_COMMENT_DISCUSSION_URI_SOUCE,
       twitter: null,
     }),
