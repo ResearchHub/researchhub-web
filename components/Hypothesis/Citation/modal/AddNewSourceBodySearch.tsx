78684016c049ffea414c59f1daa4203f2513b606
Calvin Lee <calvinhlee23@gmail.com>
2021-08-25 03:59:49 -0700

    ui setup done

--- components/Hypothesis/Citation/modal/AddNewSourceBodySearch.tsx
+++ components/Hypothesis/Citation/modal/AddNewSourceBodySearch.tsx
@@ -1,19 +1,45 @@
 import { css, StyleSheet } from "aphrodite";
 import { ReactElement } from "react";
+import FormSelect from "../../../Form/FormSelect";
+import { formGenericStyles } from "../../../Paper/Upload/styles/formGenericStyles";
+import {
+  SearchFilterDocType,
+  SearchFilterDocTypeLabel,
+} from "../util/sourceSearchHandler";
 
+const { PAPER: PAPER_KEY } = SearchFilterDocType;
+const docTypeOptions = [
+  { label: SearchFilterDocTypeLabel[PAPER_KEY], value: PAPER_KEY },
+];
 export default function AddNewSourceBodySearch(): ReactElement<"div"> {
+  console.warn("docTypeOptions: ", docTypeOptions);
   return (
-    <div className={css(styles.addNewSourceBodySearch)}>
-      <img
-        className={css(styles.bannerImg)}
-        src="/static/icons/site-wide-banner.png"
-      />
-      <div className={css(styles.textSection)}>
-        <div className={css(styles.headerText)}>
-          {"This feature is still work in progress"}
-        </div>
+    <form
+      autoComplete={"off"}
+      className={css(formGenericStyles.form)}
+      // onSubmit={onFormSubmit}
+    >
+      <div
+        className={css(
+          styles.addNewSourceBodySearch,
+          formGenericStyles.pageContent,
+          formGenericStyles.noBorder
+        )}
+      >
+        <FormSelect
+          // error={formErrors.year}
+          id="doc-search-type"
+          inputStyle={formGenericStyles.inputStyle}
+          label="Type"
+          labelStyle={formGenericStyles.labelStyle}
+          // onChange={handleInputChange}
+          options={docTypeOptions}
+          placeholder="Select search type"
+          required
+          value={docTypeOptions[0]}
+        />
       </div>
-    </div>
+    </form>
   );
 }
 
@@ -23,9 +49,8 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     height: 200,
-    margin: "30px 24px 16px",
     justifyContent: "center",
-    width: 400,
+    width: 600,
   },
   headerText: {
     alignItems: "center",
