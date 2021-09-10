7540e6c969d479ffac74c2b63c77a3af6b4f188b
Kobe Attias <kobeattias@gmail.com>
2021-09-10 16:44:53 -0400

    CTX: ISR, Fixing voting bug

--- config/utils/reputation.js
+++ config/utils/reputation.js
@@ -1,6 +1,7 @@
 import { doesNotExist } from "./nullchecks";
 import { getCurrentUser } from "./user";
 import { getNestedValue } from "./misc";
+import { userVoteToConstant } from "~/config/constants";
 
 export function currentUserHasMinimumReputation(stateObject, minimum) {
   let reputation = getCurrentUserReputation(stateObject);
@@ -31,5 +32,8 @@ export function getMinimumReputation(storeState, key) {
 }
 
 export function getVoteType(vote) {
+  if (vote?.vote_type && parseInt(vote.vote_type)) {
+    return userVoteToConstant(vote);
+  }
   return vote && vote.voteType;
 }
