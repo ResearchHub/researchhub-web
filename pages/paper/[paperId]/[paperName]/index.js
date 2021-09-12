8b8c33b36fb46f9e1371406ab2d393bab49a2ddd
Kobe Attias <kobeattias@gmail.com>
2021-09-12 11:50:28 -0400

    CTX: ISR, Test for 1 revalidate

--- pages/paper/[paperId]/[paperName]/index.js
+++ pages/paper/[paperId]/[paperName]/index.js
@@ -646,7 +646,7 @@ export async function getStaticProps(ctx) {
     return {
       props,
       // Static page will be regenerated after specified seconds.
-      revalidate: 60,
+      revalidate: 1,
     };
   }
 }
