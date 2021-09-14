7c98451449caec0715f29a2c5921358ad7fc2fd6
Calvin Lee <calvinhlee23@gmail.com>
2021-09-13 19:14:43 -0700

    error

--- components/Modals/PaperFeatureModal.js
+++ components/Modals/PaperFeatureModal.js
@@ -68,7 +68,8 @@ class PaperFeatureModal extends Component {
     let { props, isOpen } = this.props.modals.openPaperFeatureModal;
     if (
       !prevProps.modals.openPaperFeatureModal.isOpen &&
-      (isOpen && props.tab === "summary")
+      isOpen &&
+      props.tab === "summary"
     ) {
       this.initializeSummary();
     }
@@ -296,8 +297,9 @@ class PaperFeatureModal extends Component {
       .then((res) => {
         showMessage({ show: false });
         let { paper } = this.props;
-        let localStorageKey = `editorState-${paper.id}-${paper.summary &&
-          paper.summary.id}`;
+        let localStorageKey = `editorState-${paper.id}-${
+          paper.summary && paper.summary.id
+        }`;
         if (localStorage.getItem(localStorageKey)) {
           localStorage.removeItem(localStorageKey);
         }
@@ -643,7 +645,7 @@ class PaperFeatureModal extends Component {
           return (
             <div className={css(styles.dndContainer)}>
               <FormDND
-                urlView:false
+                urlView={false}
                 handleDrop={this.handleFileDrop}
                 // onSearch={checkSearchResults}
               />
