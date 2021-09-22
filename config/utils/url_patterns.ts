60eeb6111f7bbefd5086d7b1e2a1940caf09cf66
Calvin Lee <calvinhlee23@gmail.com>
2021-09-22 10:49:15 -0700

    citation link done

---
+++ config/utils/url_patterns.ts
@@ -0,0 +1,27 @@
+import { ID } from "../types/root_types";
+import { getUnifiedDocType } from "./getUnifiedDocTypes";
+import { emptyFncWithMsg, isNullOrUndefined, nullthrows } from "./nullchecks";
+
+export const UNIFIED_DOC_PAGE_URL_PATTERN =
+  "/[documentType]/[documentId]/[slug]";
+
+type FormatUnifiedDocPageUrlArgs = {
+  docType: string;
+  documentID: ID;
+  slug?: string | null;
+};
+
+export function formatUnifiedDocPageUrl({
+  docType,
+  documentID,
+  slug,
+}: FormatUnifiedDocPageUrlArgs): string {
+  const uriResolvedDocType = getUnifiedDocType(docType);
+  if (isNullOrUndefined(uriResolvedDocType) || isNullOrUndefined(documentID)) {
+    nullthrows(
+      uriResolvedDocType,
+      `Unable to resolve document type ${docType} with ID ${documentID}`
+    );
+  }
+  return `/${uriResolvedDocType}/${documentID}/${slug ?? ""}/`;
+}
