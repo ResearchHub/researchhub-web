b07d85f61b8cf16e58013c68974a227a08ce9c26
thomas-vu <thomas.vu@ucalgary.ca>
2021-09-29 00:55:03 -0600

    added popover menu for notes

--- components/CKEditor/ELNEditor.js
+++ components/CKEditor/ELNEditor.js
@@ -1,14 +1,19 @@
 import API from "~/config/api";
+import Link from "next/link";
+import ResearchHubPopover from "~/components/ResearchHubPopover";
+import SidebarSectionContent from "~/components/CKEditor/SidebarSectionContent";
 import colors from "~/config/themes/colors";
-
+import dynamic from "next/dynamic";
+import icons from "~/config/themes/icons";
 import { AUTH_TOKEN } from "~/config/constants";
 import { Helpers } from "@quantfive/js-web-config";
 import { breakpoints } from "~/config/themes/screen";
 import { css, StyleSheet } from "aphrodite";
-import { isNullOrUndefined, isEmpty } from "~/config/utils/nullchecks";
+import { isNullOrUndefined } from "~/config/utils/nullchecks";
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-import OrgSidebar from "~/components/Org/OrgSidebar";
+
+const NoteTemplateModal = dynamic(() => import("~/components/Modals/NoteTemplateModal"));
 
 const saveData = (editor, noteId) => {
   const noteParams = {
@@ -28,17 +33,22 @@ const saveData = (editor, noteId) => {
     .then(Helpers.parseJSON);
 };
 
-export const ELNEditor = ({ currentNoteId, user, notes, titles, setTitles }) => {
+export const ELNEditor = ({ user }) => {
   const router = useRouter();
   const editorRef = useRef();
   const presenceListElementRef = useRef();
   const sidebarElementRef = useRef();
-
   const { CKEditor, Editor, CKEditorInspector } = editorRef.current || {};
-  const [editorLoaded, setEditorLoaded] = useState(false); 
-
-console.log('currentNoteId', currentNoteId);  
-console.log('notes', notes);  
+  const [currentNoteId, setCurrentNoteId] = useState(router.query.noteId);
+  const [currentOrganizationId, setCurrentOrganizationId] = useState(null);
+  const [editorLoaded, setEditorLoaded] = useState(false);
+  const [hideNotes, setHideNotes] = useState(false);
+  const [isNoteTemplateModalOpen, setIsNoteTemplateModalOpen] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [organizations, setOrganizations] = useState([]);
+  const [refetchNotes, setRefetchNotes] = useState(false);
+  const [titles, setTitles] = useState({});
 
   useEffect(() => {
     editorRef.current = {
@@ -53,6 +63,83 @@ console.log('notes', notes);
     };
   }, []);
 
+  useEffect(() => {
+    fetch(API.ORGANIZATION({ userId: user.id }), API.GET_CONFIG())
+      .then(Helpers.checkStatus)
+      .then(Helpers.parseJSON)
+      .then((data) => {
+        setOrganizations(data);
+        setCurrentOrganizationId(getCurrentOrganizationId(data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!isNullOrUndefined(currentOrganizationId)) {
+      fetch(API.NOTE({ orgId: currentOrganizationId }), API.GET_CONFIG())
+        .then(Helpers.checkStatus)
+        .then(Helpers.parseJSON)
+        .then((data) => {
+          const sortedNotes = data.sort((a, b) => new Date(b.created_date) - new Date(a.created_date));
+          setNotes(sortedNotes);
+          const updatedTitles = {};
+          for (const note of sortedNotes) {
+            updatedTitles[note.id.toString()] = note.title;
+          }
+          setTitles(updatedTitles);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [refetchNotes, currentOrganizationId]);
+
+  useEffect(() => {
+    setCurrentOrganizationId(getCurrentOrganizationId(organizations));
+    router.push(`/notebook/${router.query.orgName}/${router.query.noteId}`);
+  }, [router.query.orgName]);
+
+  useEffect(() => {
+    setCurrentNoteId(router.query.noteId);
+  }, [router.query.noteId]);
+
+  const getCurrentOrganizationId = (orgs) => {
+    const orgName = router.query.orgName;
+    if (orgName === "personal") {
+      return 0; // orgId of 0 for personal notes
+    } else {
+      for (const org of orgs) {
+        if (orgName === org.slug) {
+          return org.id;
+        }
+      }
+    }
+  };
+
+  const toggleSidebarSection = () => {
+    setHideNotes(!hideNotes);
+  };
+
+  const handleCreateNewNote = () => {
+    const params = {
+      organization: currentOrganizationId,
+      title: "Untitled",
+    };
+
+    fetch(API.NOTE({}), API.POST_CONFIG(params))
+      .then(Helpers.checkStatus)
+      .then(Helpers.parseJSON)
+      .then((note) => {
+        setRefetchNotes(!refetchNotes);
+        setTitles({
+          [note.id.toString()]: note.title,
+          ...titles
+        });
+        router.push(`/notebook/${router.query.orgName}/${note.id}`);
+      });
+  };
 
   const handleInput = (editor) => {
     const updatedTitles = {};
@@ -153,29 +240,136 @@ console.log('notes', notes);
     );
   });
 
+  let organizationName;
+  let organizationImage;
+  if (currentOrganizationId === 0) {
+    organizationName = "Personal Notes";
+    organizationImage = user.author_profile.profile_image;
+  } else if (currentOrganizationId > 0) {
+    for (const org of organizations) {
+      if (org.id === currentOrganizationId) {
+        organizationName = org.name;
+        organizationImage = org.cover_image;
+      }
+    }
+  }
+
   return (
     <div className={css(styles.container)}>
+      <NoteTemplateModal
+        currentOrganizationId={currentOrganizationId}
+        isOpen={isNoteTemplateModalOpen}
+        setIsOpen={setIsNoteTemplateModalOpen}
+        user={user}
+        refetchNotes={refetchNotes}
+        setRefetchNotes={setRefetchNotes}
+      />
       <div className={css(styles.presenceList)}>
         <div ref={presenceListElementRef} className="presence"></div>
-      </div>  
+      </div>
+      <div className={css(styles.sidebar)}>
+        <div>
+          <ResearchHubPopover
+            containerStyle={{ marginLeft: "10px", marginTop: "-10px" }}
+            isOpen={isPopoverOpen}
+            popoverContent={
+              <div className={css(styles.popoverBodyContent)}>
+                <Link href={`/notebook/personal/200`}>
+                  <a className={css(styles.popoverBodyItem)} onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
+                    <img className={css(styles.popoverBodyItemImage)} draggable="false" src={user.author_profile.profile_image} />
+                    <div className={css(styles.popoverBodyItemTitle)}>Personal Notes</div>
+                  </a>
+                </Link>
+                {organizations.map(org => (
+                  <Link href={`/notebook/${org.slug}/200`} key={org.id.toString()}>
+                    <a className={css(styles.popoverBodyItem)} onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
+                      <img className={css(styles.popoverBodyItemImage)} draggable="false" src={org.cover_image} />
+                      <div className={css(styles.popoverBodyItemText)}>
+                        <div className={css(styles.popoverBodyItemTitle)}>{org.name}</div>
+                        <div className={css(styles.popoverBodyItemSubtitle)}>{"1 member"}</div>
+                      </div>
+                    </a>
+                  </Link>
+                ))}
+              </div>
+            }
+            positions={["bottom"]}
+            setIsPopoverOpen={setIsPopoverOpen}
+            targetContent={
+              <div className={css(styles.popoverTarget)} onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
+                <img className={css(styles.popoverBodyItemImage)} draggable="false" src={organizationImage} />
+                {organizationName}
+                <span className={css(styles.sortIcon)}>
+                  {icons.sort}
+                </span>
+              </div>
+            }
+          />
+        </div>
+        <div
+          className={css(styles.sidebarSection, hideNotes && styles.showBottomBorder)}
+          onClick={toggleSidebarSection}
+        >
+          Notes
+          <span className={css(styles.chevronIcon)}>
+            {hideNotes ? icons.chevronDown : icons.chevronUp}
+          </span>
+        </div>
+        {!hideNotes && (
+          <div>
+            {notes.map(note => (
+              <SidebarSectionContent
+                currentNoteId={currentNoteId}
+                noteId={note.id.toString()}
+                orgName={router.query.orgName}
+                title={titles[note.id.toString()]}
+              />
+            ))}
+          </div>
+        )}
+        <div className={css(styles.sidebarButtonsContainer)}>
+          <div
+            className={css(styles.sidebarButton)}
+            onClick={() => setIsNoteTemplateModalOpen(true)}
+          >
+            {icons.shapes}
+            <span className={css(styles.sidebarButtonText)}>
+              Templates
+            </span>
+          </div>
+          <div className={css(styles.sidebarButton)}>
+            {icons.fileImport}
+            <span className={css(styles.sidebarButtonText)}>
+              Import
+            </span>
+          </div>
+        </div>
+        <div
+          className={css(styles.sidebarNewNote)}
+          onClick={handleCreateNewNote}
+        >
+          <div className={css(styles.actionButton)}>{icons.plus}</div>
+          <div className={css(styles.newNoteText)}>Create New Note</div>
+        </div>
+      </div>
       <div className={css(styles.editorContainer)}>
         {editorLoaded && editors}
       </div>
-      <div ref={sidebarElementRef} className="sidebar"></div>  
-    </div>  
-  )
+      <div ref={sidebarElementRef} className="sidebar"></div>
+    </div>
+  );
 };
-
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+  },
+  editorContainer: {
     marginLeft: "max(min(16%, 300px), 240px)",
     width: "100%",
     [`@media only screen and (max-width: ${breakpoints.medium.str})`]: {
       marginLeft: 0,
     },
-  },
-  editorContainer: {
   },
   editor: {
     height: "100%",
@@ -183,11 +377,154 @@ const styles = StyleSheet.create({
   hideEditor: {
     display: "none",
   },
+  sidebar: {
+    background: "#f9f9fc",
+    borderRight: `1px solid ${colors.GREY(0.3)}`,
+    display: "flex",
+    flexDirection: "column",
+    height: "calc(100vh - 80px)",
+    left: 0,
+    maxWidth: 300,
+    minWidth: 240,
+    position: "fixed",
+    top: 80,
+    width: "16%",
+    [`@media only screen and (max-width: ${breakpoints.medium.str})`]: {
+      display: "none",
+    },
+  },
+  popoverTarget: {
+    alignItems: "center",
+    color: colors.BLACK(0.6),
+    cursor: "pointer",
+    display: "flex",
+    fontSize: 14,
+    fontWeight: 500,
+    letterSpacing: 1.2,
+    padding: 20,
+    textTransform: "uppercase",
+    userSelect: "none",
+    wordBreak: "break-word",
+    ":hover": {
+      backgroundColor: colors.GREY(0.3),
+    },
+  },
+  popoverBodyContent: {
+    backgroundColor: "#fff",
+    borderRadius: 4,
+    boxShadow: "0px 0px 10px 0px #00000026",
+    display: "flex",
+    flexDirection: "column",
+    userSelect: "none",
+    width: 270,
+  },
+  popoverBodyItem: {
+    alignItems: "center",
+    cursor: "pointer",
+    display: "flex",
+    padding: 16,
+    textDecoration: "none",
+    wordBreak: "break-word",
+    ":hover": {
+      backgroundColor: colors.GREY(0.2),
+    },
+    ":first-child": {
+      borderRadius: "4px 4px 0px 0px",
+    },
+    ":last-child": {
+      borderRadius: "0px 0px 4px 4px",
+    },
+  },
+  popoverBodyItemImage: {
+    borderRadius: "50%",
+    height: 30,
+    marginRight: 10,
+    objectFit: "cover",
+    width: 30,
+  },
+  popoverBodyItemText: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  popoverBodyItemTitle: {
+    color: colors.BLACK(),
+    fontWeight: 500,
+  },
+  popoverBodyItemSubtitle: {
+    color: colors.BLACK(0.5),
+    fontSize: 13,
+    marginTop: 2,
+  },
+  sidebarSection: {
+    borderTop: `1px solid ${colors.GREY(0.3)}`,
+    color: colors.BLACK(),
+    cursor: "pointer",
+    display: "flex",
+    fontSize: 18,
+    fontWeight: 500,
+    padding: 20,
+    userSelect: "none",
+  },
+  sidebarNewNote: {
+    borderTop: `1px solid ${colors.GREY(0.3)}`,
+    color: colors.BLUE(),
+    cursor: "pointer",
+    display: "flex",
+    marginTop: "auto",
+    padding: 20,
+    ":hover": {
+      color: "#3E43E8",
+    },
+  },
+  newNoteText: {
+    fontSize: 18,
+    fontWeight: 500,
+    margin: "auto",
+  },
+  actionButton: {
+    alignItems: "center",
+    background: colors.LIGHT_GREY(),
+    border: "1px solid #ddd",
+    borderRadius: "50%",
+    display: "flex",
+    fontSize: 16,
+    height: 35,
+    justifyContent: "center",
+    marginLeft: 5,
+    marginRight: 5,
+    transition: "all ease-in-out 0.1s",
+    width: 35,
+  },
+  showBottomBorder: {
+    borderBottom: `1px solid ${colors.GREY(0.3)}`,
+  },
+  sidebarButtonsContainer: {
+    margin: 10,
+  },
+  sidebarButton: {
+    border: "none",
+    color: colors.BLACK(0.5),
+    cursor: "pointer",
+    fontSize: 14,
+    fontWeight: 500,
+    maxWidth: "fit-content",
+    padding: 10,
+    ":hover": {
+      color: colors.BLUE(),
+    },
+  },
+  sidebarButtonText: {
+    marginLeft: 10,
+  },
+  sortIcon: {
+    marginLeft: 10,
+  },
+  chevronIcon: {
+    marginLeft: "auto",
+  },
   presenceList: {
     display: "none",
     marginBottom: 10,
     marginTop: 10,
   },
 });
-
-export default ELNEditor;
