e0f3f8ff3ab7665f615503d1b6ad8a211195b733
Kobe Attias <kobeattias@gmail.com>
2021-09-29 19:35:42 -0400

    CTX: Feed, adding missing sentry dp

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from "next/app";
+import * as Sentry from "@sentry/browser";
 import Router, { useRouter } from "next/router";
 import withRedux from "next-redux-wrapper";
 import { Provider } from "react-redux";
