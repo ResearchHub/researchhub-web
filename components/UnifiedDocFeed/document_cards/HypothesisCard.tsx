eafe540c7b49dbe6a0ad925a2b4451ae1f6eacde
Calvin Lee <calvinhlee23@gmail.com>
2021-09-30 12:11:11 -0700

    update

--- components/UnifiedDocFeed/document_cards/HypothesisCard.tsx
+++ components/UnifiedDocFeed/document_cards/HypothesisCard.tsx
@@ -314,7 +314,7 @@ function HypothesisCard({
               </div>
               <div className={css(styles.consensusContainer)}>
                 <CitationConsensusItem
-                  citationID={null}
+                  citationID={`${id}-citation-placeholder`}
                   consensusMeta={{
                     downCount: aggreCitationCons?.down_count ?? 0,
                     neutralCount: aggreCitationCons?.neutral_count ?? 0,
