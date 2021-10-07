6cdf543e87cd71077315ac955616970a2f2c6e18
lightninglu10 <lightning.lu7@gmail.com>
2021-10-07 11:26:21 -0700

    adding in clip loading back

--- components/CKEditor/ELNEditor.js
+++ components/CKEditor/ELNEditor.js
@@ -50,6 +50,7 @@ const ELNEditor = ({
   const channelId = `${router.query.orgSlug}-${
     currentOrganizationId > 0 ? currentOrganizationId : user.id
   }-${currentNoteId}`;
+
   const INITIAL_CONTEXT_CONFIGURATION = {
     // The configuration for real-time collaboration features, shared between the editors:
     cloudServices: {
