c6640b34f902c609ece7d8d09744459ce7a4db28
thomas-vu <thomas.vu@ucalgary.ca>
2021-10-08 12:58:19 -0600

    cleaned up fetching and added readonly editor

--- components/CKEditor/ELNEditor.js
+++ components/CKEditor/ELNEditor.js
@@ -1,18 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import { Helpers } from "@quantfive/js-web-config";
-import { css, StyleSheet } from "aphrodite";
-import { useRouter } from "next/router";
-import { CKEditor, CKEditorContext } from "@ckeditor/ckeditor5-react";
-import {
-  Context,
-  ELNEditor as CKELNEditor,
-} from "@thomasvu/ckeditor5-custom-build";
-
-import { AUTH_TOKEN } from "~/config/constants";
 import API from "~/config/api";
+import { AUTH_TOKEN } from "~/config/constants";
+import { CKEditor, CKEditorContext } from "@ckeditor/ckeditor5-react";
+import { Context, ELNEditor as CKELNEditor, SimpleBalloonEditor } from "@thomasvu/ckeditor5-custom-build";
+import { Helpers } from "@quantfive/js-web-config";
 import { breakpoints } from "~/config/themes/screen";
-import api from "~/config/api";
-import Loader from "../Loader/Loader";
+import { css, StyleSheet } from "aphrodite";
+import { useRef, useState, useCallback } from "react";
+import { useRouter } from "next/router";
 
 const saveData = (editor, noteId) => {
   const noteParams = {
@@ -33,80 +27,29 @@ const saveData = (editor, noteId) => {
 };
 
 const ELNEditor = ({
+  currentNote,
+  currentNoteId,
   currentOrganizationId,
+  isCollaborativeReady,
+  setIsCollaborativeReady,
+  setReadOnlyEditorInstance,
   setTitles,
   titles,
   user,
-  currentNote,
-  currentNoteId,
-  onReady,
-  disableELN,
-  noteLoading,
 }) => {
   const router = useRouter();
-  const presenceListElementRef = useRef();
   const sidebarElementRef = useRef();
+  const [presenceListElement, setPresenceListElement] = useState(null);
 
   const channelId = `${router.query.orgSlug}-${
     currentOrganizationId > 0 ? currentOrganizationId : user.id
   }-${currentNoteId}`;
-  const INITIAL_CONTEXT_CONFIGURATION = {
-    // The configuration for real-time collaboration features, shared between the editors:
-    cloudServices: {
-      tokenUrl: () => {
-        return new Promise((resolve, reject) => {
-          const xhr = new XMLHttpRequest();
-          xhr.open("GET", API.CKEDITOR_TOKEN());
 
-          xhr.addEventListener("load", () => {
-            const statusCode = xhr.status;
-            const xhrResponse = xhr.response;
-
-            if (statusCode < 200 || statusCode > 299) {
-              return reject(new Error("Cannot download a new token!"));
-            }
-
-            return resolve(xhrResponse);
-          });
-
-          xhr.addEventListener("error", () =>
-            reject(new Error("Network error"))
-          );
-          xhr.addEventListener("abort", () => reject(new Error("Abort")));
-          xhr.setRequestHeader(
-            "Authorization",
-            "Token " +
-              (typeof window !== "undefined"
-                ? window.localStorage[AUTH_TOKEN]
-                : "")
-          );
-          xhr.send();
-        });
-      },
-      webSocketUrl: "wss://80957.cke-cs.com/ws",
-    },
-    // Collaboration configuration for the context:
-    collaboration: {
-      channelId,
-    },
-    sidebar: {
-      container: sidebarElementRef.current,
-    },
-    presenceList: {
-      container: presenceListElementRef.current,
-      onClick: (user, element) => console.log(user, element),
-    },
-  };
-
-  const [ELNLoaded, setELNLoaded] = useState(false);
-
-  useEffect(() => {
-    if (disableELN) {
-      setELNLoaded(false);
-    } else {
-      setELNLoaded(true);
+  const onRefChange = useCallback((node) => {
+    if (node !== null) {
+      setPresenceListElement(node);
     }
-  }, [disableELN]);
+  }, []);
 
   const handleInput = (editor) => {
     const updatedTitles = {};
@@ -119,79 +62,133 @@ const ELNEditor = ({
     setTitles(updatedTitles);
   };
 
-  const noteId = currentNoteId;
-
-  if (!process.browser) {
-    return null;
-  }
-
-  const CKConfig = {
-    title: {
-      placeholder: "Untitled",
-    },
-    placeholder:
-      "Start typing to continue with an empty page, or pick a template",
-    initialData: currentNote.latest_version?.src ?? "",
-    simpleUpload: {
-      // The URL that the images are uploaded to.
-      uploadUrl: API.SAVE_IMAGE,
-
-      // Headers sent along with the XMLHttpRequest to the upload server.
-      headers: {
-        Authorization:
-          "Token " +
-          (typeof window !== "undefined"
-            ? window.localStorage[AUTH_TOKEN]
-            : ""),
-      },
-    },
-    collaboration: {
-      channelId,
-    },
-    autosave: {
-      save(editor) {
-        return saveData(editor, noteId);
-      },
-    },
-  };
-
   return (
     <div className={css(styles.container)}>
       <div className={css(styles.presenceListContainer)}>
         <div
-          ref={presenceListElementRef}
-          className={css(styles.presenceList) + " presence"}
+          ref={onRefChange}
+          className={css(styles.presenceList, isCollaborativeReady && styles.green) + " presence"}
         />
       </div>
-      {ELNLoaded && (
+      <div className={css(isCollaborativeReady && styles.hideReadOnlyEditor)}>
+        <CKEditor
+          config={{
+            title: {
+              placeholder: "Untitled",
+            },
+            placeholder:
+              "Start typing to continue with an empty page, or pick a template",
+            initialData: currentNote.latest_version?.src ?? "",
+          }}
+          editor={SimpleBalloonEditor}
+          onReady={(editor) => {
+            setReadOnlyEditorInstance(editor);
+            editor.isReadOnly = true
+            editor.editing.view.change((writer) => {
+              writer.setStyle(
+                "background",
+                "#fff",
+                editor.editing.view.document.getRoot()
+              );
+            });
+          }}
+        />
+      </div>
+      {presenceListElement !== null && (
         <CKEditorContext
-          config={INITIAL_CONTEXT_CONFIGURATION}
+          config={{
+            // The configuration for real-time collaboration features, shared between the editors:
+            cloudServices: {
+              tokenUrl: () => {
+                return new Promise((resolve, reject) => {
+                  const xhr = new XMLHttpRequest();
+                  xhr.open("GET", API.CKEDITOR_TOKEN());
+
+                  xhr.addEventListener("load", () => {
+                    const statusCode = xhr.status;
+                    const xhrResponse = xhr.response;
+
+                    if (statusCode < 200 || statusCode > 299) {
+                      return reject(new Error("Cannot download a new token!"));
+                    }
+
+                    return resolve(xhrResponse);
+                  });
+
+                  xhr.addEventListener("error", () =>
+                    reject(new Error("Network error"))
+                  );
+                  xhr.addEventListener("abort", () => reject(new Error("Abort")));
+                  xhr.setRequestHeader(
+                    "Authorization",
+                    "Token " +
+                      (typeof window !== "undefined"
+                        ? window.localStorage[AUTH_TOKEN]
+                        : "")
+                  );
+                  xhr.send();
+                });
+              },
+              webSocketUrl: "wss://80957.cke-cs.com/ws",
+            },
+            // Collaboration configuration for the context:
+            collaboration: {
+              channelId,
+            },
+            sidebar: {
+              container: sidebarElementRef.current,
+            },
+            presenceList: {
+              container: presenceListElement,
+              onClick: (user, element) => console.log(user, element),
+            },
+          }}
           context={Context}
         >
-          {/* {editors} */}
           <div
-            className={css(
-              styles.editor,
-              router.query.noteId !== noteId && styles.hideEditor
-            )}
-            key={noteId}
+            className={css(styles.editor)}
+            key={currentNoteId}
           >
             <CKEditor
-              config={CKConfig}
+              config={{
+                title: {
+                  placeholder: "Untitled",
+                },
+                placeholder:
+                  "Start typing to continue with an empty page, or pick a template",
+                initialData: currentNote.latest_version?.src ?? "",
+                simpleUpload: {
+                  // The URL that the images are uploaded to.
+                  uploadUrl: API.SAVE_IMAGE,
+
+                  // Headers sent along with the XMLHttpRequest to the upload server.
+                  headers: {
+                    Authorization:
+                      "Token " +
+                      (typeof window !== "undefined"
+                        ? window.localStorage[AUTH_TOKEN]
+                        : ""),
+                  },
+                },
+                collaboration: {
+                  channelId,
+                },
+                autosave: {
+                  save(editor) {
+                    return saveData(editor, currentNoteId);
+                  },
+                },
+              }}
               editor={CKELNEditor}
-              id={noteId}
+              id={currentNoteId}
               onChange={(event, editor) => handleInput(editor)}
               onReady={(editor) => {
-                onReady && onReady();
+                console.log("Editor is ready to use!", editor);
+                setIsCollaborativeReady(true);
               }}
             />
           </div>
         </CKEditorContext>
-      )}
-      {noteLoading && (
-        <div className={css(styles.loaderContainer)}>
-          <Loader type="clip" />
-        </div>
       )}
       <div ref={sidebarElementRef} className="sidebar"></div>
     </div>
@@ -216,7 +213,11 @@ const styles = StyleSheet.create({
   },
   presenceListContainer: {
     background: "#fff",
+    height: 80,
   },
+  //green: {
+  //  background: "#0f0",
+  //},
   loaderContainer: {
     height: "calc(100vh - 216px)",
     display: "flex",
@@ -233,6 +234,9 @@ const styles = StyleSheet.create({
     // display: "flex",
     // justifyContent: "flex-end",
     // marginRight: 60,
+  },
+  hideReadOnlyEditor: {
+    display: "none",
   },
 });
 
