8ab870aa0d752ecd1c6ea26c1b0c9c16e303b807
Calvin Lee <calvinhlee23@gmail.com>
2021-10-14 12:56:55 -0700

    done with example

--- components/PermissionsDashboard/PermissionsDashboard.tsx
+++ components/PermissionsDashboard/PermissionsDashboard.tsx
@@ -1,11 +1,11 @@
 import { css, StyleSheet } from "aphrodite";
 import { ReactElement, useState } from "react";
+import gateKeepCurrentUser from "~/config/gatekeeper/gateKeepCurrentUser";
 
 export default function PermissionsDashboard(): ReactElement<"div"> {
-  const [lastFetchTime, setLastFetchTime] = useState<number>(Date.now());
-  return (
-    <div className={css(styles.permissionsDashboard)}>{"Hello there!!!"}</div>
-  );
+  const [_lastFetchTime, _setLastFetchTime] = useState<number>(Date.now());
+  gateKeepCurrentUser("ELN");
+  return <div className={css(styles.permissionsDashboard)} />;
 }
 
 const styles = StyleSheet.create({
