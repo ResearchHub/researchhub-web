c776844637c27f467db854156b009b4f01a410dd
lightninglu10 <lightning.lu7@gmail.com>
2021-10-14 21:27:50 -0700

    adding title to join page

--- pages/[orgSlug]/notebook/[noteId]/index.js
+++ pages/[orgSlug]/notebook/[noteId]/index.js
@@ -1,9 +1,5 @@
 import Notebook from "~/components/Notebook/Notebook";
 import HeadComponent from "~/components/Head";
-import nookies from "nookies";
-import { AUTH_TOKEN } from "~/config/constants";
-import { fetchNote } from "~/config/fetch";
-import Error from "next/error";
 
 const Index = ({ note }) => {
   return (
