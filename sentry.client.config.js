d7e03356a27f7eec70c1534e3e3fa9d848c014d1
Kobe Attias <kobeattias@gmail.com>
2021-10-17 15:59:14 -0400

    CTX: Sentry, Running sentry conditionally

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -6,8 +6,11 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN || 'https://423f7b6ddcea48b9b50f7ba4baa0e750@o206202.ingest.sentry.io/1817918',
-  release: process.env.SENTRY_RELEASE,
-  environment: "development"  
-});
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: SENTRY_DSN || "https://423f7b6ddcea48b9b50f7ba4baa0e750@sentry.io/1817918",
+    release: process.env.SENTRY_RELEASE,
+    environment:
+      process.env.REACT_APP_ENV === "staging" ? "staging" : "production",
+  });
+}
