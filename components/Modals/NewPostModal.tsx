d6fa23061c8e3705a774753625b84888c13fa9a5
Dragan Okanovic <dragan.okan@gmail.com>
2021-10-20 15:04:25 +0200

    Fix paths between post and question creation

--- components/Modals/NewPostModal.tsx
+++ components/Modals/NewPostModal.tsx
@@ -21,7 +21,7 @@ const items = [
     description:
       "Non-published work, that must be scientific in nature. Ideas, theories, and questions to the community are all welcome.",
     imgSrc: "/static/icons/askQuestion.png",
-    route: "/post/create/question",
+    route: "/post/create",
   },
   {
     header: "Create a Hypothesis",
