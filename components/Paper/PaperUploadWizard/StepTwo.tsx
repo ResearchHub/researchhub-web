ba4190730348273e4413946191d3c640efb0cccc
Dragan Okanovic <dragan.okan@gmail.com>
2021-10-21 23:48:45 +0200

    Upload wizard: part 3.5

--- components/Paper/PaperUploadWizard/StepTwo.tsx
+++ components/Paper/PaperUploadWizard/StepTwo.tsx
@@ -7,6 +7,8 @@ import FormTextArea from "../../Form/FormTextArea";
 import { PaperDetails } from "./PaperUploadWizard";
 import { HubPicker } from "./HubPicker";
 import { AuthorsPicker } from "./AuthorsPicker";
+import { css } from "aphrodite";
+import { formGenericStyles } from "../Upload/styles/formGenericStyles";
 
 type StepTwoProps = {
   details: PaperDetails;
@@ -35,6 +37,42 @@ export const StepTwo: FC<StepTwoProps> = ({
   return (
     <div>
       <FormInput
+        id="ed_title"
+        label="Editorialized Title (optional)"
+        placeholder="Jargon free version of the title that the average person would understand"
+        value={editorializedTitle}
+        onChange={handleFieldChange("editorializedTitle")}
+      />
+      <AuthorsPicker
+        id="authors"
+        value={authors}
+        onChange={handleFieldChange("authors")}
+      />
+      <div className={css(formGenericStyles.row)}>
+        <FormSelect
+          // error={formErrors.year}
+          containerStyle={formGenericStyles.smallContainer}
+          id="published.year"
+          label="Year of Publication"
+          onChange={handleFieldChange("pubYear")}
+          options={Options.range(1960, new Date().getFullYear())}
+          placeholder="yyyy"
+          required={false}
+          value={pubYear}
+        />
+        <FormSelect
+          // error={formErrors.month}
+          containerStyle={formGenericStyles.smallContainer}
+          id="published.month"
+          label="Month of Publication"
+          onChange={handleFieldChange("pubMonth")}
+          options={Options.months}
+          placeholder="month"
+          required={false}
+          value={pubMonth}
+        />
+      </div>
+      <FormInput
         id="doi"
         label="DOI"
         placeholder="DOI"
@@ -43,45 +81,13 @@ export const StepTwo: FC<StepTwoProps> = ({
         required={true}
         spellCheck={false}
       />
-      <FormInput
-        id="ed_title"
-        label="Editorialized Title (optional)"
-        placeholder="Jargon free version of the title that the average person would understand"
-        value={editorializedTitle}
-        onChange={handleFieldChange("editorializedTitle")}
-      />
+      <HubPicker value={hubs} onChange={handleFieldChange("hubs")} id="hubs" />
       <FormTextArea
         id="abstract"
         label="Abstract"
         onChange={handleFieldChange("abstract")}
         placeholder="Enter the paper"
         value={abstract}
-      />
-      <FormSelect
-        // error={formErrors.year}
-        id="published.year"
-        label="Year of Publication"
-        onChange={handleFieldChange("pubYear")}
-        options={Options.range(1960, new Date().getFullYear())}
-        placeholder="yyyy"
-        required={false}
-        value={pubYear}
-      />
-      <FormSelect
-        // error={formErrors.month}
-        id="published.month"
-        label="Month of Publication"
-        onChange={handleFieldChange("pubMonth")}
-        options={Options.months}
-        placeholder="month"
-        required={false}
-        value={pubMonth}
-      />
-      <HubPicker value={hubs} onChange={handleFieldChange("hubs")} id="hubs" />
-      <AuthorsPicker
-        id="authors"
-        value={authors}
-        onChange={handleFieldChange("authors")}
       />
     </div>
   );
