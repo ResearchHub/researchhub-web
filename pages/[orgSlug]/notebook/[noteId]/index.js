aaeb10d5981c01e4b304096f20f04a16b9aa198d
Thomas Vu <thomas.vu@ucalgary.ca>
2021-11-03 11:45:01 -0600

    Added LaTeX support to ELN (#1007)
    * added LaTeX support to ELN
    
    * updated package version

--- pages/[orgSlug]/notebook/[noteId]/index.js
+++ pages/[orgSlug]/notebook/[noteId]/index.js
@@ -1,11 +1,19 @@
-import Notebook from "~/components/Notebook/Notebook";
 import HeadComponent from "~/components/Head";
+import Notebook from "~/components/Notebook/Notebook";
+import Script from 'next/script'
 
 const Index = ({ note }) => {
   return (
     <>
       <HeadComponent title={"ResearchHub Notebook"} />
       <Notebook />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/contrib/mhchem.min.js"
+        strategy="lazyOnload" // this script needs to load after the main katex script
+      />
     </>
   );
 };
