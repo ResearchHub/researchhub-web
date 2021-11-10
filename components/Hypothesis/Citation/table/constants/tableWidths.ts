3c9c988f4cd65d5bf945febe7b83410ccdbde5dc
Calvin Lee <calvinhlee23@gmail.com>
2021-11-09 17:11:33 -0800

    update

--- components/Hypothesis/Citation/table/constants/tableWidths.ts
+++ components/Hypothesis/Citation/table/constants/tableWidths.ts
@@ -1,7 +1,7 @@
 export const tableWidths = {
-  CITED_BY: "14%",
-  CONSENSUS: "17%",
-  SOURCE: "45%",
+  CITED_BY: "8%",
+  CONSENSUS: "4%",
+  SOURCE: "50%",
   TYPE: "10%",
   COMMENTS: "6%",
 };
