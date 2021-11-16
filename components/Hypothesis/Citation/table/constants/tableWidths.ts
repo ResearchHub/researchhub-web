3e670600be38b3b8bc627a65c7208ddac10934b4
Calvin Lee <calvinhlee23@gmail.com>
2021-11-16 14:02:32 -0800

    relocating type on table

--- components/Hypothesis/Citation/table/constants/tableWidths.ts
+++ components/Hypothesis/Citation/table/constants/tableWidths.ts
@@ -1,8 +1,8 @@
 export const tableWidths = {
   CITED_BY: "15%",
   CONSENSUS: "5.5%",
-  SOURCE: "40%",
-  TYPE: "15%",
+  SOURCE: "50%",
+  // TYPE: "15%",
   COMMENTS: "15%",
 };
 
@@ -10,6 +10,6 @@ export const tableMaxWidths = {
   CITED_BY: "120px",
   CONSENSUS: "100px",
   SOURCE: "800px",
-  TYPE: "120px",
+  // TYPE: "120px",
   COMMENTS: "120px",
 };
