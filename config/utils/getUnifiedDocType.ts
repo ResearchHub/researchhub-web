0fbad423266416e48352e0a5558c954b5063bd09
Calvin Lee <calvinhlee23@gmail.com>
2021-12-07 15:18:47 -0800

    update FE & BE doc resolve

--- config/utils/getUnifiedDocType.ts
+++ config/utils/getUnifiedDocType.ts
@@ -1,14 +1,33 @@
-type ReturnType = "hypothesis" | "post" | "paper" | null;
+type FEReturnType = "hypothesis" | "post" | "paper" | null;
+type BEReturnType = "discussion" | "hypothesis" | "paper" | null;
 
 // this function is used to resolve BE model name discrepencies with FE naming conventions
 // the return type is intentionally kept strict.
 export function getUnifiedDocType(
   input: string | null | undefined
-): ReturnType {
+): FEReturnType {
   const lowerCasedInput = input?.toLowerCase() ?? null;
   switch (lowerCasedInput) {
     case "discussion":
       return "post";
+    case "hypothesis":
+      return "hypothesis";
+    case "paper":
+      return "paper";
+    default:
+      return null;
+  }
+}
+
+// this function is used to resolve FE type name to with BE naming conventions
+export function getBEUnifiedDocType(
+  input: string | null | undefined
+): BEReturnType {
+  const lowerCasedInput = input?.toLowerCase() ?? null;
+  switch (lowerCasedInput) {
+    case "post":
+    case "posts":
+      return "discussion";
     case "hypothesis":
       return "hypothesis";
     case "paper":
