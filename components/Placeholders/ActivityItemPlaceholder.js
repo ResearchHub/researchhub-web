17c57e40c9c22adf7886d33012e02b29f2ef5e5a
Kobe Attias <kobeattias@gmail.com>
2021-12-11 21:52:18 -0500

    [User Activity] Placeholder

---
+++ components/Placeholders/ActivityItemPlaceholder.js
@@ -0,0 +1,46 @@
+import { StyleSheet, css } from "aphrodite";
+import { RectShape, RoundShape } from "react-placeholder/lib/placeholders";
+import colors from "~/config/themes/colors";
+import PaperPlaceholder from "./PaperPlaceholder";
+
+const ActivityItemPlaceholder = ({ color = "#d3d3d3", rows = 1 }) => {
+  return Array.from({ length: rows }).map((k, i) => (
+    <div className={css(styles.container)}>
+      <div
+        key={`activity-summary-placholder-${i}`}
+        className={css(styles.summaryContainer)}
+      >
+        <RoundShape className={css(styles.round)} color={color} />
+        <RectShape
+          className={css(styles.textRow)}
+          color={color}
+          style={{ width: "100%", height: "1em" }}
+        />
+      </div>
+      <PaperPlaceholder color={color} />
+    </div>
+  ));
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 30,
+  },
+  summaryContainer: {
+    borderRadius: 3,
+    display: "flex",
+    width: "50%",
+    marginLeft: -50,
+  },
+  round: {
+    height: 40,
+    width: 40,
+    minWidth: 40,
+  },
+  textRow: {
+    marginTop: 12,
+    marginLeft: 10,
+  },
+});
+
+export default ActivityItemPlaceholder;
