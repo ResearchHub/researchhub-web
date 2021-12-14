05a8c086c8bc6110f20a79328c1116ee7337ac7d
Kobe Attias <kobeattias@gmail.com>
2021-12-14 15:13:42 -0500

    [User Activity] flex instead of negative margins

--- components/Placeholders/FeedItemPlaceholder.js
+++ components/Placeholders/FeedItemPlaceholder.js
@@ -9,15 +9,19 @@ const FeedItemPlaceholder = ({ color = "#EFEFEF", rows = 1 }) => {
       className={css(styles.container)}
       key={`activity-summary-placholder-${i}`}
     >
-      <div className={css(styles.summaryContainer)}>
+      <div className={css(styles.avatarWrapper)}>
         <RoundShape className={css(styles.round)} color={color} />
-        <RectShape
-          className={css(styles.textRow)}
-          color={color}
-          style={{ width: "100%", height: "1em" }}
-        />
       </div>
-      <PaperPlaceholder color={color} />
+      <div className={css(styles.contentWrapper)}>
+        <div className={css(styles.summaryContainer)}>
+          <RectShape
+            className={css(styles.textRow)}
+            color={color}
+            style={{ width: "100%", height: "1em" }}
+          />
+        </div>
+        <PaperPlaceholder color={color} />
+      </div>
     </div>
   ));
 };
@@ -25,21 +29,34 @@ const FeedItemPlaceholder = ({ color = "#EFEFEF", rows = 1 }) => {
 const styles = StyleSheet.create({
   container: {
     marginBottom: 30,
+    display: "flex",
+    alignItems: "flex-start",
+    width: "100%",
   },
+  avatarWrapper: {
+    marginTop: -5,
+  },
+  contentWrapper: {
+    width: "calc(100% - 50px)",
+    marginLeft: 15,
+  },
+
   summaryContainer: {
     borderRadius: 3,
     display: "flex",
     width: "50%",
-    marginLeft: -50,
   },
   round: {
-    height: 40,
-    width: 40,
-    minWidth: 40,
+    height: 35,
+    width: 35,
+    minWidth: 35,
   },
   textRow: {
-    marginTop: 12,
+    marginTop: 5,
     marginLeft: 10,
+    ":first-child": {
+      marginLeft: 0,
+    },
   },
 });
 
