fadc2a9b80b5f88a9f360e9b50aa4fabe7f41e01
Calvin Lee <calvinhlee23@gmail.com>
2021-12-15 15:23:50 -0800

    update more

--- components/DiscussionComment.js
+++ components/DiscussionComment.js
@@ -144,6 +144,7 @@ class DiscussionComment extends Component {
   formatDiscussionPostMeta = () => {
     const { username, date } = this.state;
     const { data, commentStyles, replyCardStyle } = this.props;
+    console.warn("this.props: ", this.props);
     const authorProfile = data.created_by.author_profile;
 
     return {
@@ -437,15 +438,9 @@ const mapStateToProps = (state) => {
   };
 };
 
-export const Comment = connect(
-  mapStateToProps,
-  null
-)(CommentClass);
+export const Comment = connect(mapStateToProps, null)(CommentClass);
 
-export const Reply = connect(
-  mapStateToProps,
-  null
-)(ReplyClass);
+export const Reply = connect(mapStateToProps, null)(ReplyClass);
 
 const styles = StyleSheet.create({
   commentContainer: {},
