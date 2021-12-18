b76efc1d2c56c47b522d4e3d64658ae1f3117d0e
Patrick Lu <lightning.lu7@gmail.com>
2021-12-17 19:06:39 -0500

    fix issue with comment

--- components/Threads/CommentEntry.js
+++ components/Threads/CommentEntry.js
@@ -599,7 +599,7 @@ class CommentEntry extends Component {
                     "created_by",
                     "author_profile",
                   ])}
-                  isCreatedByEditor={data?.is_created_by_editor}
+                  isCreatedByEditor={comment?.is_created_by_editor}
                   data={comment}
                   username={username}
                   date={date}
