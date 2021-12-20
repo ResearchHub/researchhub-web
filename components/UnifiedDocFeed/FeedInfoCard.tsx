e721812b6efb77ae1a3153aa1f0d680ee434fd0c
Calvin Lee <calvinhlee23@gmail.com>
2021-12-20 13:32:29 -0800

    updating facephile

--- components/UnifiedDocFeed/FeedInfoCard.tsx
+++ components/UnifiedDocFeed/FeedInfoCard.tsx
@@ -72,6 +72,7 @@ export default function FeedInfoCard({
                 <AuthorFacePile
                   authorProfiles={editorProfiles}
                   imgSize={16}
+                  horizontal
                   withAuthorName
                 />
               ) : (
