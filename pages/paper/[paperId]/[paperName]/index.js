f77e6995b60e35c3536f7440d9b4df587fba72c3
Kobe Attias <kobeattias@gmail.com>
2022-01-03 09:55:36 -0500

    [Paper] Auto Scroll to comments section

--- pages/paper/[paperId]/[paperName]/index.js
+++ pages/paper/[paperId]/[paperName]/index.js
@@ -1,7 +1,7 @@
 import { connect, useDispatch, useStore } from "react-redux";
 import { isUserEditorOfHubs } from "~/components/UnifiedDocFeed/utils/getEditorUserIDsFromHubs";
 import { StyleSheet, css } from "aphrodite";
-import { useEffect, useState, Fragment, useMemo } from "react";
+import { useEffect, useState, Fragment, useMemo, useRef } from "react";
 import { useRouter } from "next/router";
 import { Waypoint } from "react-waypoint";
 import * as Sentry from "@sentry/browser";
@@ -133,11 +133,31 @@ const Paper = ({
   const currUserID = auth?.user?.id ?? null;
   const isSubmitter = uploaded_by && uploaded_by?.id === currUserID;
   const isEditorOfHubs = isUserEditorOfHubs({ currUserID, hubs });
+  const commentsRef = useRef(null);
 
   const structuredDataForSEO = useMemo(
     () => buildStructuredDataForSEO(),
     [paper]
   );
+
+  useEffect(() => {
+    if (!process.browser) return;
+
+    const idx = window.location.hash.indexOf("#comments");
+    if (idx > -1 && commentsRef.current) {
+      const elem = commentsRef.current;
+      const pos = elem.getBoundingClientRect().top + window.scrollY;
+      window.scroll({
+        top: pos,
+        behavior: "instant",
+      });
+
+      // Remove #comments from URL to avoid scrolling on user page refresh
+      setTimeout(() => {
+        history.replaceState(null, null, " ");
+      }, 250);
+    }
+  }, [commentsRef.current]);
 
   // Typically, this if statement would be created with a useEffect clause
   // but since useEffect does not work with SSG, we need a standard if statement.
@@ -492,7 +512,7 @@ const Paper = ({
                 bottomOffset={"95%"}
               >
                 <div className={css(styles.space)}>
-                  <a name="comments" />
+                  <a name="comments" ref={commentsRef} />
                   {
                     <DiscussionTab
                       hostname={process.env.HOST}
