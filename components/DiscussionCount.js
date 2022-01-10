52adce7ab1913d5a4650e93a4bfbca0d78990d5a
Kobe Attias <kobeattias@gmail.com>
2022-01-10 14:44:13 -0500

    Updated comment count styling and included in paper,post,hypothesis pages

---
+++ components/DiscussionCount.js
@@ -0,0 +1,74 @@
+import { StyleSheet, css } from "aphrodite";
+import Link from "next/link";
+import icons, { PaperDiscussionIcon } from "~/config/themes/icons";
+import colors, { discussionPageColors } from "~/config/themes/colors";
+import { breakpoints } from "~/config/themes/screen";
+
+const DiscussionCount = ({ docType, count, id, slug }) => {
+  if (!count || !id) {
+    return null;
+  }
+
+  return (
+    <Link href={`/${docType}/${id}/${slug}#comments`}>
+      <a
+        className={css(styles.link)}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div className={css(styles.discussion)}>
+          <div className={css(styles.discussionIcon)}>
+            {PaperDiscussionIcon({})}
+          </div>
+          <div className={css(styles.discussionCount)}>{count}</div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  discussionIcon: {
+    color: colors.BLUE(),
+  },
+  link: {
+    textDecoration: "none",
+    color: "inherit",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-end",
+    width: "100%",
+  },
+  discussion: {
+    cursor: "pointer",
+    position: "relative",
+    fontSize: 14,
+    background: discussionPageColors.ICON,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+    borderRadius: 40,
+    width: 48,
+    boxSizing: "border-box",
+    [`@media only screen and (max-width: ${breakpoints.medium.str})`]: {
+      minWidth: "unset",
+    },
+    [`@media only screen and (max-width: ${breakpoints.small.str})`]: {
+      fontSize: 13,
+    },
+    ":hover": {
+      background: discussionPageColors.ICON_HOVER,
+    },
+  },
+  discussionCount: {
+    color: colors.BLUE(),
+    fontWeight: "bold",
+    marginLeft: 3,
+    fontSize: 14,
+  },
+});
+
+export default DiscussionCount;
