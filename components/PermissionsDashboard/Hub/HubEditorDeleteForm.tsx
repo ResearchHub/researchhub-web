1e929fb84411a142b0b9c2f20add9de4c1ac8a63
Calvin Lee <calvinhlee23@gmail.com>
2022-01-14 15:37:03 -0800

    setting up

---
+++ components/PermissionsDashboard/Hub/HubEditorDeleteForm.tsx
@@ -0,0 +1,108 @@
+import {
+  customStyles,
+  formGenericStyles,
+} from "~/components/Paper/Upload/styles/formGenericStyles";
+import { emptyFncWithMsg, nullthrows } from "~/config/utils/nullchecks";
+import { hubEditorCreate } from "../api/hubEditorCreate";
+import { ID } from "~/config/types/root_types";
+import { Fragment, ReactElement, SyntheticEvent, useState } from "react";
+import { useEffectFetchSuggestedHubs } from "~/components/Paper/Upload/api/useEffectGetSuggestedHubs";
+import { verifStyles } from "~/components/AuthorClaimModal/AuthorClaimPromptEmail";
+import Button from "~/components/Form/Button";
+import FormInput from "~/components/Form/FormInput";
+import FormSelect from "~/components/Form/FormSelect";
+import Loader from "~/components/Loader/Loader";
+
+type FormState = {
+  selectedHub: any;
+  editorEmail: string | null;
+};
+
+const DEFAULT_FORM_STATE: FormState = {
+  selectedHub: null,
+  editorEmail: null,
+};
+
+export default function HubEditorDeleteForm(): ReactElement<typeof Fragment> {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [suggestedHubs, setSuggestedHubs] = useState<any>([]);
+  const [formState, setFormState] = useState<FormState>(DEFAULT_FORM_STATE);
+
+  useEffectFetchSuggestedHubs({ setSuggestedHubs });
+
+  const { selectedHub, editorEmail } = formState;
+  const handleSubmit = (event: SyntheticEvent): void => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    hubEditorCreate({
+      editorEmail: nullthrows(editorEmail, "Editor email cannot be empty"),
+      onSuccess: (): void => {
+        setFormState(DEFAULT_FORM_STATE);
+        setIsSubmitting(false);
+        alert(
+          `User with email ${editorEmail} is now removed from ${selectedHub.name}`
+        );
+      },
+      onError: (error: Error): void => {
+        emptyFncWithMsg(error);
+        setFormState(DEFAULT_FORM_STATE);
+        setIsSubmitting(false);
+        alert(`Oops, something went wrong: ${error.message}`);
+      },
+      selectedHubID: nullthrows(
+        selectedHub?.id ?? null,
+        "Hub needs to be selected"
+      ),
+    });
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <h1>{"Remove Hub Editor"}</h1>
+      </div>
+      <form encType="multipart/form-data" onSubmit={handleSubmit}>
+        <FormSelect
+          containerStyle={formGenericStyles.container}
+          id="hubs"
+          label="Hubs"
+          inputStyle={customStyles.input}
+          labelStyle={formGenericStyles.labelStyle}
+          onChange={(_id: ID, selectedHub: any): void =>
+            setFormState({ ...formState, selectedHub })
+          }
+          options={suggestedHubs}
+          placeholder="Search Hubs"
+          required
+          value={selectedHub}
+        />
+        <FormInput
+          containerStyle={formGenericStyles.container}
+          disable={isSubmitting}
+          id="editorEmail"
+          label="New Editor's Email"
+          onChange={(_id: ID, editorEmail: string): void =>
+            setFormState({ ...formState, editorEmail })
+          }
+          placeholder="example@university.edu"
+          required
+          type="email"
+          value={editorEmail}
+        />
+        <div
+          style={{ display: "flex", width: "100%", justifyContent: "center" }}
+        >
+          <Button
+            label={
+              isSubmitting ? <Loader size={8} loading color="#fff" /> : "Submit"
+            }
+            type="submit"
+            customButtonStyle={verifStyles.buttonCustomStyle}
+            rippleClass={verifStyles.rippleClass}
+            disabled={isSubmitting}
+          />
+        </div>
+      </form>
+    </Fragment>
+  );
+}
---
+++ components/PermissionsDashboard/api/hubEditorDelete.ts
@@ -0,0 +1,33 @@
+import { Helpers } from "@quantfive/js-web-config";
+import { ID } from "~/config/types/root_types";
+import API from "../../../config/api";
+
+type HubEditorDeleteArgs = {
+  editorEmail: any;
+  onError: (error: Error) => void;
+  onSuccess: () => void;
+  selectedHubID: NonNullable<ID>;
+};
+
+export function hubEditorDelete({
+  editorEmail,
+  onError,
+  onSuccess,
+  selectedHubID,
+}: HubEditorDeleteArgs): void {
+  fetch(
+    API.HUB_NEW_EDITOR,
+    API.POST_CONFIG({
+      editor_email: editorEmail,
+      selected_hub_id: selectedHubID,
+    })
+  )
+    .then(Helpers.checkStatus)
+    .then(Helpers.parseJSON)
+    .then((response: any): void => {
+      onSuccess();
+    })
+    .catch((err) => {
+      onError(err);
+    });
+}
