b0ddbb63ad6d62156f28d5a2ad80d0e7ca1e1da6
Calvin Lee <calvinhlee23@gmail.com>
2022-01-14 16:29:35 -0800

    ready to push

--- config/api.js
+++ config/api.js
@@ -595,7 +595,7 @@ const routes = (BASE_URL) => {
       return url;
     },
     HUB_NEW_EDITOR: BASE_URL + "hub/create_new_editor/",
-    HUB_DELETE_EDITOR: BASE_URL + "hub/delete_editor",
+    HUB_DELETE_EDITOR: BASE_URL + "hub/delete_editor/",
     SORTED_HUB: (params = {}) => {
       // hard codedlimit to 10
       let url = BASE_URL + `hub/?ordering=-score&page_limit=10`;
