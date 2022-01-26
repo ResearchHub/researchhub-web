430444f120d0c0bbaff42c1d2770662737f35830
Kobe Attias <kobeattias@gmail.com>
2022-01-25 20:31:58 -0500

    logging env var

--- pages/_app.js
+++ pages/_app.js
@@ -72,6 +72,7 @@ const MyApp = ({ Component, pageProps, store }) => {
   console.log("WEB3_INFURA_PROJECT_ID", process.env.WEB3_INFURA_PROJECT_ID);
   console.log("RECAPTCHA_CLIENT_KEY", process.env.RECAPTCHA_CLIENT_KEY);
   console.log("SIFT_BEACON_KEY", process.env.SIFT_BEACON_KEY);
+  console.log("REACT_APP_ENV", process.env.REACT_APP_ENV);
   console.log("TEST");
 
   const [prevPath, setPrevPath] = useState(router.asPath);
