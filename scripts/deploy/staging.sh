1cc4149fa2cfc0694af45278244d1a521d1b402b
Kobe Attias <kobeattias@gmail.com>
2022-01-26 20:21:07 -0500

    TEST 10

--- scripts/deploy/staging.sh
+++ scripts/deploy/staging.sh
@@ -16,14 +16,14 @@ echo Step 2/3: Creating new production image;
     docker tag researchhub-web-staging:latest 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-web-staging:latest
     docker push 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-web-staging:latest
 echo Step 3/3: Creating elastic beanstalk environment;
-    mv Dockerfile Dockerfile.staging.off;
-    mv Dockerrun.aws.json.staging Dockerrun.aws.json;
-    git add Dockerfile.staging.off Dockerfile;
-    git add Dockerrun.aws.json.staging Dockerrun.aws.json;
+    # mv Dockerfile Dockerfile.staging.off;
+    # mv Dockerrun.aws.json.staging Dockerrun.aws.json;
+    # git add Dockerfile.staging.off Dockerfile;
+    # git add Dockerrun.aws.json.staging Dockerrun.aws.json;
     eb deploy staging-web --profile researchhub --staged;
-    git reset;
-    mv Dockerfile.staging.off Dockerfile;
-    mv Dockerrun.aws.json Dockerrun.aws.json.staging;
+    # git reset;
+    # mv Dockerfile.staging.off Dockerfile;
+    # mv Dockerrun.aws.json Dockerrun.aws.json.staging;
 else
     echo Please commit your changes first.;
 fi
