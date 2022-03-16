79d53b7c6334a5bde11ae24ee21e5f84b00410da
Calvin Lee <calvinhlee23@gmail.com>
2022-03-16 03:53:48 -0700

    url body done

--- components/Paper/UploadWizard/shared/PaperUploadWizardInput.tsx
+++ components/Paper/UploadWizard/shared/PaperUploadWizardInput.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, ReactElement, SyntheticEvent } from "react";
 import colors from "~/config/themes/colors";
 
 type Props = {
+  error?: boolean;
   label: string;
   onChange: (inputValue: null | string) => void;
   placeholder?: string;
@@ -11,6 +12,7 @@ type Props = {
 };
 
 export default function PaperUploadWizardInput({
+  error,
   label,
   onChange,
   placeholder,
@@ -18,7 +20,7 @@ export default function PaperUploadWizardInput({
   value,
 }): ReactElement<"div"> {
   const valueLength = value !== null ? value.length : 0;
-
+  console.warn("error: ", error);
   return (
     <div className={css(styles.paperUploadWizardInput)}>
       <div className={css(styles.label)}>
@@ -27,14 +29,14 @@ export default function PaperUploadWizardInput({
       </div>
       <div className={css(styles.inputWrap)}>
         <input
-          className={css(styles.input)}
+          className={css(styles.input, error && styles.inputError)}
           maxLength={250}
           onChange={(event: ChangeEvent<HTMLInputElement>): void =>
             onChange(event.target?.value)
           }
           placeholder={placeholder ?? ""}
           required={required}
-          type="url"
+          type="string"
           value={value}
         />
         <div className={css(styles.valueLengthIndicator)}>
@@ -57,6 +59,9 @@ const styles = StyleSheet.create({
     padding: "0 80px 0 16px",
     width: "100%",
     ":focus": { outline: "none" },
+  },
+  inputError: {
+    border: `1px solid ${colors.RED(1)}`,
   },
   inputWrap: { boxSizing: "border-box", position: "relative", width: "100%" },
   label: { fontWeight: 500, fontSize: 18, paddingBottom: 8 },
---
+++ config/utils/isStringURL.ts
@@ -0,0 +1,13 @@
+import { isString } from "./string";
+
+const urlRegEx = new RegExp(
+  /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+);
+
+export const isStringURL = (str: string | null): boolean => {
+  if (!isString(str)) {
+    return false;
+  }
+
+  return urlRegEx.test(String(str).toLowerCase());
+};
