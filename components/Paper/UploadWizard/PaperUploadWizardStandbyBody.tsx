1a742d715d15562447677aa133cb68b985064fce
Calvin Lee <calvinhlee23@gmail.com>
2022-03-17 03:13:19 -0700

    animation loop setup done

--- components/Paper/UploadWizard/PaperUploadWizardStandbyBody.tsx
+++ components/Paper/UploadWizard/PaperUploadWizardStandbyBody.tsx
@@ -1,9 +1,38 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+
+const ANIMATION_1 = "https://giphy.com/embed/xTiN0uzf6bno09mXrG";
+const ANIMATION_2 = "https://giphy.com/embed/xTiN0yAuTXvtIH3EiI";
+const FOUR_SEC = 4000;
+const TWENTY_SEC = 20000;
+
+const timeLoop = ({
+  src,
+  setSrc,
+  time,
+}: {
+  src: string;
+  setSrc: (str: string) => void;
+  time: number;
+}) => {
+  setTimeout((): void => {
+    const nextSrc = src === ANIMATION_1 ? ANIMATION_2 : ANIMATION_1;
+    setSrc(nextSrc);
+    timeLoop({ src: nextSrc, setSrc, time });
+  }, time);
+};
 
 export default function PaperUploadWizardStandbyBody() {
-  
-  const [src, setSrc] = useState("https://giphy.com/embed/xTiN0uzf6bno09mXrG");
-  setTimeout(() => setSrc("https://giphy.com/embed/xTiN0yAuTXvtIH3EiI"), 4000);
+  const [src, setSrc] = useState<string>(ANIMATION_1);
+  const [askRedirect, setAskRedirect] = useState<boolean>(false);
+
+  useEffect(() => {
+    timeLoop({
+      src,
+      setSrc,
+      time: FOUR_SEC,
+    });
+    setTimeout((): void => setAskRedirect(true), TWENTY_SEC);
+  }, []);
 
   return (
     <Fragment>
