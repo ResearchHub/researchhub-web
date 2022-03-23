a830bd935edb05df341c920025f7dffc14bbcfbd
Calvin Lee <calvinhlee23@gmail.com>
2022-03-23 01:09:26 -0700

    update is successful

---
+++ components/Paper/UploadWizard/api/updateSubmittedPaper.ts
@@ -0,0 +1,14 @@
+import { ID } from "~/config/types/root_types";
+
+
+export function updateSubmittedPaper({
+  formState,
+  onError,
+  onSuccess,
+}: {
+  formState: FormState;
+  onError: (error: Error) => void;
+  onSuccess: () => void;
+}): void {
+
+}
