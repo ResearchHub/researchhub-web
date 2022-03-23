8bd51e93dc0d7160386c5368981ad15ce5b7c4bd
Calvin Lee <calvinhlee23@gmail.com>
2022-03-23 01:09:26 -0700

    update is successful

--- components/Paper/UploadWizard/PaperUploadWizardUpdatePaper.tsx
+++ components/Paper/UploadWizard/PaperUploadWizardUpdatePaper.tsx
@@ -1,31 +1,47 @@
-import { css, StyleSheet } from "aphrodite";
-import { useRouter } from "next/router";
-import { SyntheticEvent, useState } from "react";
-import { verifStyles } from "~/components/AuthorClaimModal/AuthorClaimPromptEmail";
-import Button from "~/components/Form/Button";
-import FormInput from "~/components/Form/FormInput";
-import FormSelect from "~/components/Form/FormSelect";
-import { useEffectFetchSuggestedHubs } from "../Upload/api/useEffectGetSuggestedHubs";
 import {
   customStyles,
   formGenericStyles,
 } from "../Upload/styles/formGenericStyles";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { ID } from "~/config/types/root_types";
+import { isEmpty, isNullOrUndefined } from "~/config/utils/nullchecks";
+import { PaperActions } from "~/redux/paper";
+import { SyntheticEvent, useState } from "react";
+import { useEffectFetchSuggestedHubs } from "../Upload/api/useEffectGetSuggestedHubs";
+import { useRouter } from "next/router";
+import { verifStyles } from "~/components/AuthorClaimModal/AuthorClaimPromptEmail";
+import Button from "~/components/Form/Button";
+import FormInput from "~/components/Form/FormInput";
+import FormSelect from "~/components/Form/FormSelect";
+import { buildSlug } from "~/config/utils/buildSlug";
+import Loader from "~/components/Loader/Loader";
 
-type FormError = {
+export type FormErrors = {
   paperID: boolean;
   selectedHubs: boolean;
   title: boolean /* editorialized title */;
   doi: boolean;
 };
 
-type FormState = {
+export type FormState = {
   doi: null | string;
-  paperID: null;
+  paperID: ID;
   selectedHubs: any[];
   title: null | string /* editorialized title */;
 };
 
-const defaulError: FormError = {
+type Props = {
+  onExit: () => void;
+  paperActions: any /* redux */;
+  paperID: ID;
+};
+
+type GetIsFormValidArgs = {
+  formState: FormState;
+};
+
+const defaulError: FormErrors = {
   doi: false,
   paperID: false,
   selectedHubs: false,
@@ -39,54 +55,88 @@ const defaultFormState: FormState = {
   title: null,
 };
 
-export default function PaperUploadWizardUpdatePaper({ onExit, paperID }) {
+const getIsFormValid = ({
+  formState,
+}: GetIsFormValidArgs): [verdict: boolean, errors: FormErrors] => {
+  let verdict = true;
+  const errorResult = { ...defaulError };
+  const { doi, paperID, selectedHubs, title } = formState;
+  if (isNullOrUndefined(formState.paperID)) {
+    verdict = false;
+    errorResult.paperID = true;
+  }
+  if (isEmpty(selectedHubs ?? [])) {
+    verdict = false;
+    errorResult.selectedHubs = true;
+  }
+  return [verdict, errorResult];
+};
+
+function PaperUploadWizardUpdatePaper({ onExit, paperActions }: Props) {
+  const paperID = 36;
   // TODO: calvinhlee - migrate this to pure typeahead
-  const [formError, setFormError] = useState<FormError>(defaulError);
-  const [formState, setFormState] = useState<FormState>(defaultFormState);
+  const router = useRouter();
+  const [formErrors, setFormErrors] = useState<FormErrors>(defaulError);
+  const [formState, setFormState] = useState<FormState>({
+    ...defaultFormState,
+    paperID,
+  });
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [suggestedHubs, setSuggestedHubs] = useState<any>([]);
   const { doi, title, selectedHubs } = formState;
   useEffectFetchSuggestedHubs({ setSuggestedHubs });
 
-  // const onFormSubmit = (event: SyntheticEvent): void => {
-  //   event.preventDefault();
-  //   const isFormValid = getIsFormValid({
-  //     formState,
-  //     formErrors,
-  //     setFormErrors,
-  //   });
-  //   if (isFormValid) {
-  //     messageActions.showMessage({ load: true, show: true });
-  //   } else {
-  //     messageActions.setMessage("Required fields must be filled.");
-  //     messageActions.showMessage({
-  //       load: false,
-  //       show: true,
-  //       error: true,
-  //     });
-  //   }
-  // };
+  const onFormSubmit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    const [verdict, formErrors] = getIsFormValid({
+      formState,
+    });
+    if (verdict) {
+      //  calvinhlee - Refer to NOTE(100)
+      const formattedPayload: any = {
+        ...formState,
+        authors: null,
+        publishDate: null,
+        hubs: selectedHubs.map((hub): ID => hub.id),
+      };
 
-  // const handleHubSelection = (_id: ID, selectedHubs: any): void => {
-  //   if (isNullOrUndefined(selectedHubs)) {
-  //     setFormState({ ...formState, hubs: [] });
-  //     setFormErrors({ ...formErrors, hubs: true });
-  //   } else {
-  //     setFormState({ ...formState, hubs: selectedHubs });
-  //     setFormErrors({ ...formErrors, hubs: selectedHubs.length < 1 });
-  //   }
-  // };
-  const router = useRouter();
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        router.push("/paper/[paperId]/", `/paper/${paperID}/`);
+      const response = await paperActions.patchPaper(paperID, formattedPayload);
+      const { payload: resPayload } = response;
+      if (resPayload.success) {
+        const { postedPaper } = resPayload;
+        const { id: paperID, paper_title, slug, title } = postedPaper || {};
+        setIsSubmitting(false);
+        const paperSlug = !isEmpty(slug)
+          ? slug
+          : buildSlug(paper_title ? paper_title : title);
+        router.push(`/paper/${paperID}/${paperSlug}`);
         onExit();
-      }}
-    >
+      } else {
+        setIsSubmitting(false);
+        alert("ERROR");
+        console.warn("resPayload: ", resPayload);
+      }
+    } else {
+      setFormErrors(formErrors);
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleHubSelection = (_id: ID, selectedHubs: any): void => {
+    if (isNullOrUndefined(selectedHubs)) {
+      setFormState({ ...formState, selectedHubs: [] });
+    } else {
+      setFormState({ ...formState, selectedHubs });
+    }
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
       <FormSelect
         containerStyle={formGenericStyles.container}
-        error={formError.selectedHubs}
+        disabled={isSubmitting}
+        error={formErrors.selectedHubs}
         id="hubs"
         isMulti
         label="Hubs"
@@ -95,33 +145,35 @@ export default function PaperUploadWizardUpdatePaper({ onExit, paperID }) {
           selectedHubs.length > 0 && customStyles.capitalize)
         }
         labelStyle={formGenericStyles.labelStyle}
-        onChange={(hubs: any[]) =>
-          setFormState({ ...formState, selectedHubs: hubs })
-        }
+        onChange={handleHubSelection}
         options={suggestedHubs}
         placeholder="Search Hubs"
         required
-        // value={selectedHubs}
+        value={selectedHubs}
       />
       <FormInput
-        label="DOI"
-        placeholder="Jargon free version of the title that the average person would understand"
         containerStyle={formGenericStyles.container}
-        labelStyle={formGenericStyles.labelStyle}
-        // value={doi}
+        disabled={isSubmitting}
         id="doi"
-        onChange={(doi: string | string) => setFormState({ ...formState, doi })}
+        label="DOI (optional)"
+        labelStyle={formGenericStyles.labelStyle}
+        onChange={(_id: ID, doi: string): void =>
+          setFormState({ ...formState, doi: isEmpty(doi) ? null : doi })
+        }
+        placeholder="DOI"
+        value={doi}
       />
       <FormInput
-        label="Editorialized Title (optional)"
-        placeholder="Jargon free version of the title that the average person would understand"
         containerStyle={formGenericStyles.container}
-        labelStyle={formGenericStyles.labelStyle}
-        // value={title}
+        disabled={isSubmitting}
         id="title"
-        onChange={(title: string | string) =>
-          setFormState({ ...formState, title })
+        label="Editorialized Title (optional)"
+        labelStyle={formGenericStyles.labelStyle}
+        onChange={(_id: ID, title: string): void =>
+          setFormState({ ...formState, title: isEmpty(title) ? null : title })
         }
+        placeholder="Jargon free version of the title that the average person would understand"
+        value={title}
       />
       <div
         style={{
@@ -142,13 +194,15 @@ export default function PaperUploadWizardUpdatePaper({ onExit, paperID }) {
           onClick={(event: SyntheticEvent): void => {
             event.preventDefault();
             onExit();
-            // logical ordering
           }}
         />
         <Button
           customButtonStyle={verifStyles.buttonCustomStyle}
+          disabled={isSubmitting}
           key="upload-wizard-button"
-          label="Save"
+          label={
+            !isSubmitting ? "Save" : <Loader size={8} loading color="#fff" />
+          }
           rippleClass={verifStyles.rippleClass}
           size="xxsmall"
           type="submit"
@@ -157,3 +211,16 @@ export default function PaperUploadWizardUpdatePaper({ onExit, paperID }) {
     </form>
   );
 }
+
+const mapStateToProps = (state: any) => ({
+  paperRedux: state.paper,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  paperActions: bindActionCreators(PaperActions, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PaperUploadWizardUpdatePaper);
