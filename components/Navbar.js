e6c83d28b056e38ba9c11caae830af2c3860e88e
Calvin Lee <calvinhlee23@gmail.com>
2022-03-24 16:55:32 -0700

    formatting

--- components/Navbar.js
+++ components/Navbar.js
@@ -71,6 +71,7 @@ const Navbar = (props) => {
   const [openCaseCounts, setOpenCaseCounts] = useState(0);
   const [showReferral, setShowReferral] = useState(false);
   const [forceOpenPaperUpload, setForceOpenPaperUpload] = useState(undefined);
+  const [isNewPostModalOpen, setNewButtonOpen] = useState(false);
   const { numNavInteractions } = useContext(NavbarContext);
   const {
     isLoggedIn,
@@ -356,6 +357,7 @@ const Navbar = (props) => {
           <NewPostButton
             customButtonStyle={[styles.newPostButton]}
             forceOpen={forceOpenPaperUpload}
+            setOpen={setNewButtonOpen}
             onClick={() => setSideMenu(!sideMenu)}
           />
         )}
@@ -583,11 +585,15 @@ const Navbar = (props) => {
               </div>
             )}
           </div>
-          <NewPostButton />
+          <NewPostButton
+            forceOpen={forceOpenPaperUpload}
+            setOpen={setNewButtonOpen}
+          />
           <PaperUploadStateNotifier
             wsAuth
             wsUrl={WS_ROUTES.PAPER_SUBMISSION(user.id)}
             setForceOpenPaperUpload={setForceOpenPaperUpload}
+            isNewPostModalOpen={isNewPostModalOpen}
           />
         </div>
 
