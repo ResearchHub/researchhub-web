701ea7c1a6245abb0c1f5e1ca59ddbb674d059d1
Kobe Attias <kobeattias@gmail.com>
2022-04-15 21:46:47 -0400

    #1158 Peer review request card styling, button

--- components/Form/DropdownButton.js
+++ components/Form/DropdownButton.js
@@ -14,17 +14,17 @@ const DropdownButton = ({
   dropdownClassName,
   customButtonClassName,
   selected,
+  labelAsHtml,
+  overridePopoverStyle,
+  overrideOptionsStyle,
+  overrideTargetStyle,
+  overrideTitleStyle,
+  overrideDownIconStyle,
+  htmlBefore, // HTML to be injected before the list
+  htmlAfter, // HTML to be injected after the list
+  closeAfterSelect = true,
   positions = ["bottom", "top"],
   isOpen = false,
-  labelAsHtml = null,
-  overridePopoverStyle = null,
-  overrideOptionsStyle = null,
-  overrideTargetStyle = null,
-  overrideTitleStyle = null,
-  overrideDownIconStyle = null,
-  closeAfterSelect = true,
-  htmlBefore = null, // HTML to be injected before the list
-  htmlAfter = null, // HTML to be injected after the list
 }) => {
   return (
     <ResearchHubPopover
--- components/PeerReviews/PeerReviewRequestCard.tsx
+++ components/PeerReviews/PeerReviewRequestCard.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { PeerReviewRequest } from "./config/PeerReviewTypes";
 import { css, StyleSheet } from "aphrodite";
 import ALink from "~/components/ALink";
@@ -6,6 +6,8 @@ import AuthorAvatar from "~/components/AuthorAvatar";
 import colors from "~/config/themes/colors";
 import { getUrlToUniDoc } from "~/config/utils/routing";
 import { timeSince } from "~/config/utils/dates";
+import DropdownButton from "~/components/Form/DropdownButton";
+import { ID } from "~/config/types/root_types";
 
 
 type Props = {
@@ -16,9 +18,23 @@ function PeerReviewRequestCard({
   peerReviewRequest
 }: Props): ReactElement {
 
+  const opts = [{
+    label: "Invite",
+    value: "invite"
+  }, {
+    label: "Decline",
+    value: "decline"
+  }, {
+    label: "Close",
+    value: "close"    
+  }];
+
+  const [manageDropdownOpenFor, setManageDropdownOpenFor] = useState<ID>(null)
+  const author = peerReviewRequest?.requestedByUser?.authorProfile
+
   return (
     <div className={css(styles.PeerReviewRequestCard)}>
-      <div className={css(styles.mainTextRow)}>
+      <div className={css(styles.detailsContainer)}>
         <span className={css(styles.avatarContainer)}>
           <AuthorAvatar
             author={(peerReviewRequest?.requestedByUser ?? {})["authorProfile"]}
@@ -29,13 +45,33 @@ function PeerReviewRequestCard({
           // withAuthorName
           />
         </span>
-        <span>has requested a peer review on </span>
-        <ALink href={getUrlToUniDoc(peerReviewRequest?.unifiedDocument)}>
-          {peerReviewRequest?.unifiedDocument?.document?.title}
-        </ALink>
+        <div className={css(styles.mainTextRow)}>
+          <ALink href={getUrlToUniDoc(peerReviewRequest?.unifiedDocument)}>
+            {author?.firstName} {author?.lastName}
+          </ALink>
+          <span> has requested a peer review on </span>
+          <ALink href={getUrlToUniDoc(peerReviewRequest?.unifiedDocument)}>
+            {peerReviewRequest?.unifiedDocument?.document?.title}
+          </ALink>
+        </div>
+        <div className={css(styles.dateRow)}>
+          {timeSince(peerReviewRequest.createdDate)}
+        </div>
       </div>
-      <div className={css(styles.dateRow)}>
-        {timeSince(peerReviewRequest.createdDate)}
+      <div className={css(styles.buttonContainer)}>
+        <DropdownButton
+          opts={opts}
+          label={`Manage`}
+          isOpen={manageDropdownOpenFor === peerReviewRequest.id}
+          onClick={() => setManageDropdownOpenFor(peerReviewRequest.id)}
+          dropdownClassName="managePeerReview"
+          positions={["bottom", "right"]}
+          customButtonClassName={styles.d}
+          overrideTargetStyle={null}
+          onSelect={() => {
+            setManageDropdownOpenFor(null);
+          } }
+          onClose={() => setManageDropdownOpenFor(null)} onClickOutside={undefined} selected={undefined} labelAsHtml={undefined} overridePopoverStyle={undefined} overrideOptionsStyle={undefined} overrideTitleStyle={undefined} overrideDownIconStyle={undefined} htmlBefore={undefined} htmlAfter={undefined}        />        
       </div>
     </div>
   )
@@ -46,10 +82,33 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: "30px 30px",
     border: `1px solid ${colors.GREY(0.5)}`,
-    borderRadius: "2px",
+    borderRadius: "4px",
+    display: "flex",
+    maxWidth: 1200,
+    boxSizing: "border-box",
+    alignItems: "center",
+    justifyContent: "space-between",
+    fontSize: 14,
+    marginBottom: 16,
+  },
+  "buttonContainer": {
+
+  },
+  "detailsContainer": {
+    display: "flex",
+    alignItems: "center",
+  },
+  "d": {
+    background: colors.NEW_BLUE(),
+    color: "white",
+    borderRadius: "4px",
+    ":hover": {
+      background: colors.NEW_BLUE(),
+      opacity: 0.9,  
+    }
   },
   "mainTextRow": {
-
+    marginLeft: 10,
   },
   "dateRow": {
 
