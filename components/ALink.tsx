f51ec25ba240f2b9d008625ba026f519e3dcee89
Kobe Attias <kobeattias@gmail.com>
2022-04-18 10:04:32 -0400

    #1558 icons and more styling for review request person

--- components/ALink.tsx
+++ components/ALink.tsx
@@ -1,40 +1,40 @@
-import { FunctionComponent,  ReactElement } from "react"
+import { FunctionComponent, ReactElement } from "react"
 import Link from "next/link";
 import { css, StyleSheet } from "aphrodite";
 import colors from "~/config/themes/colors";
 
 const themes = {
-    default: "linkThemeDefault",
+  default: "linkThemeDefault",
 }
 
 interface Props {
-    theme?: string,
-    href: any,
-    as?: any,
-    children?: React.ReactNode,
+  theme?: string,
+  href: any,
+  as?: any,
+  children?: React.ReactNode,
 }
 
 const ALink: FunctionComponent<Props> = ({ href, as, children, theme = themes.default }): ReactElement => {
-    return (
-        <Link href={href} as={as}>
-            <a className={css(styles.ALink, styles[theme])}>
-                {children}
-            </a>
-        </Link>
-    )
+  return (
+    <Link href={href} as={as}>
+      <a className={css(styles.ALink, styles[theme])}>
+        {children}
+      </a>
+    </Link>
+  )
 }
 
 const styles = StyleSheet.create({
-    "ALink": {
-        color: colors.BLACK(),
-        fontWeight: 500,
-        textDecoration: "none",
-        ":hover": {
-            color: colors.NEW_BLUE()
-        }
-    },
-    "linkThemeDefault": {
+  "ALink": {
+    color: colors.BLACK(),
+    fontWeight: 500,
+    textDecoration: "none",
+    ":hover": {
+      color: colors.NEW_BLUE()
     }
+  },
+  "linkThemeDefault": {
+  }
 })
 
 export default ALink;
