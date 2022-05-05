2731a0a825d38bbf110c453223c7f076165490f3
Kobe Attias <kobeattias@gmail.com>
2022-05-04 22:12:58 -0400

    flagged content dash

---
+++ pages/moderators/audit/flagged.tsx
@@ -0,0 +1,23 @@
+import { ReactElement } from "react";
+import { FlaggedContentDashboard } from "~/components/AuditContent/FlaggedContentDashboard";
+import ContentPage from "~/components/ContentPage/ContentPage";
+import SideColumn from "~/components/Home/SideColumn";
+import ModeratorDashboardSidebar from "~/components/shared/ModeratorDashboardSidebar";
+
+export default function FlaggedContentPage(): ReactElement<
+  typeof ContentPage
+> | null {
+
+  return (
+    <ContentPage
+      mainFeed={<FlaggedContentDashboard />}
+      sidebar={
+        <SideColumn
+          listItems={<ModeratorDashboardSidebar />}
+          ready={true}
+          title={"Admin"}
+        />
+      }
+    />
+  );
+}
