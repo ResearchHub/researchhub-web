8034238f0f6751188dc66456b0c8d2ad423ba0ef
Kobe Attias <kobeattias@gmail.com>
2022-05-11 22:07:46 -0400

    more styling, show message

--- components/Admin/api/includeDocInFeed.ts
+++ components/Admin/api/includeDocInFeed.ts
@@ -3,31 +3,21 @@ import { Helpers } from "@quantfive/js-web-config";
 import { captureEvent } from "~/config/utils/events";
 import { ID } from "~/config/types/root_types";
 
-type Params = {
-  excludeFromHomepage: Boolean;
-  excludeFromHubs: Boolean;
-}
-
 type Args = {
   unifiedDocumentId: ID;
-  params: Params;
   onError?: Function;
   onSuccess: Function;
 }
 
 export default function excludeFromFeed({
   unifiedDocumentId,
-  params,
   onError,
   onSuccess,
 }: Args) {
 
   return fetch(
-    API.EXCLUDE_FROM_FEED({ unifiedDocumentId }),
-    API.POST_CONFIG({
-      exclude_from_homepage: params.excludeFromHomepage,
-      exclude_from_hubs: params.excludeFromHubs,
-    })
+    API.INCLUDE_IN_FEED({ unifiedDocumentId }),
+    API.POST_CONFIG()
   )
     .then(Helpers.checkStatus)
     .then((response) => onSuccess(response))
@@ -35,7 +25,7 @@ export default function excludeFromFeed({
       captureEvent({
         error,
         msg: "Failed to exclude from feed",
-        data: { unifiedDocumentId, params },
+        data: { unifiedDocumentId },
       });
       onError && onError(error)
     })
