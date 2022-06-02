f10a26b3f91e809f4ee1f713a90aa750dbe52497
Kobe Attias <kobeattias@gmail.com>
2022-06-02 19:53:52 -0400

    #1190 post page styling

--- pages/post/[documentId]/[title]/index.js
+++ pages/post/[documentId]/[title]/index.js
@@ -128,7 +128,7 @@ const Post = (props) => {
         <PaperTransactionModal post={post} updatePostState={updatePostState} />
         <div className={css(styles.container)}>
           <div className={css(styles.main)}>
-            <div className={css(styles.paperPageContainer, styles.top)}>
+            <div>
               <PostPageCard
                 isEditorOfHubs={isEditorOfHubs}
                 isModerator={isModerator}
@@ -137,16 +137,6 @@ const Post = (props) => {
                 removePost={removePost}
                 restorePost={restorePost}
                 shareUrl={process.browser && window.location.href}
-              />
-            </div>
-            <div className={css(styles.paperMetaContainerMobile)}>
-              <AuthorStatsDropdown
-                authors={
-                  post.authors.length > 0 ? post.authors : getCreatedByAuthor()
-                }
-                paper={post}
-                hubs={post.hubs}
-                paperId={post.id}
               />
             </div>
             <div className={css(styles.space)}>
@@ -161,17 +151,6 @@ const Post = (props) => {
                 isCollapsible={false}
               />
             </div>
-          </div>
-          <div className={css(styles.sidebar)}>
-            <PaperSideColumn
-              authors={
-                post.authors.length > 0 ? post.authors : getCreatedByAuthor()
-              }
-              paper={post}
-              hubs={post.hubs}
-              paperId={post.id}
-              isPost={true}
-            />
           </div>
         </div>
       </div>
@@ -193,391 +172,24 @@ Post.getInitialProps = async (ctx) => {
 };
 
 const styles = StyleSheet.create({
-  componentWrapperStyles: {
-    width: "100%",
-    paddingLeft: 0,
-    paddingRight: 0,
-    "@media only screen and (max-width: 415px)": {
-      width: "100%",
-      paddingLeft: 0,
-      paddingRight: 0,
-    },
-  },
   root: {
-    // display: "flex",
-    // justifyContent: "center",
-    // alignItems: "flex-start",
-    // width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    width: "100%",
   },
   container: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    boxSizing: "border-box",
-    borderCollapse: "separate",
-    borderSpacing: "30px 40px",
-    "@media only screen and (max-width: 767px)": {
-      width: "100%",
-      borderSpacing: "0",
-      display: "flex",
-      flexDirection: "column",
-    },
-    "@media only screen and (min-width: 768px)": {
-      display: "flex",
-      marginTop: 16,
-      width: "90%",
-    },
-    "@media only screen and (min-width: 1024px)": {
-      width: "100%",
-      display: "table",
-    },
-    "@media only screen and (min-width: 1200px)": {
-      width: "90%",
-    },
-  },
-  desktop: {
-    display: "none",
-    "@media only screen and (min-width: 1024px)": {
-      display: "block",
-    },
-  },
-  sidebar: {
-    display: "table-cell",
-    boxSizing: "border-box",
-    verticalAlign: "top",
-    position: "relative",
-    "@media only screen and (max-width: 767px)": {
-      display: "none",
-    },
-    "@media only screen and (min-width: 768px)": {
-      width: "20%",
-      marginLeft: 16,
-    },
-    "@media only screen and (min-width: 1024px)": {
-      minWidth: 250,
-      maxWidth: 280,
-      width: 280,
-      marginLeft: 0,
-    },
+    marginTop: 30,
   },
   main: {
-    display: "table-cell",
     boxSizing: "border-box",
-    position: "relative",
-    "@media only screen and (max-width: 767px)": {
-      width: "100%",
-    },
-    "@media only screen and (min-width: 768px)": {
-      width: "80%",
-      maxWidth: 600,
-    },
-    "@media only screen and (min-width: 1024px)": {
-      width: "unset",
-      maxWidth: 700,
-    },
-  },
-  contentContainer: {
-    padding: "30px 0px",
-    margin: "auto",
-    minHeight: "100vh",
-    position: "relative",
-    "@media only screen and (max-width: 415px)": {
-      paddingTop: 20,
-    },
-  },
-  title: {
-    fontSize: 33,
-    marginBottom: 10,
-    position: "relative",
-    wordBreak: "break-word",
-    "@media only screen and (max-width: 767px)": {
-      fontSize: 28,
-    },
-    "@media only screen and (max-width: 415px)": {
-      fontSize: 25,
-    },
-    "@media only screen and (max-width: 321px)": {
-      fontSize: 22,
-    },
-  },
-  infoSection: {
-    display: "flex",
-    marginTop: 10,
-    "@media only screen and (max-width: 767px)": {
-      display: "none",
-    },
+    width: 800,
+    margin: "0 auto",
   },
   info: {
     opacity: 0.5,
     fontSize: 14,
     marginRight: 20,
-    "@media only screen and (max-width: 321px)": {
-      fontSize: 12,
-    },
-  },
-  authors: {
-    display: "flex",
-    marginRight: 41,
-    "@media only screen and (max-width: 767px)": {
-      marginRight: 21,
-    },
-  },
-  authorContainer: {
-    marginRight: 5,
-  },
-  tags: {
-    display: "flex",
-    alignItems: "center",
-    "@media only screen and (max-width: 767px)": {
-      display: "none",
-    },
-  },
-  hubs: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-  tagline: {
-    fontSize: 16,
-    marginTop: 25,
-    marginBottom: 20,
-    color: "#4e4c5f",
-    fontFamily: "Roboto",
-    "@media only screen and (max-width: 767px)": {
-      marginTop: 20,
-      marginBottom: 20,
-    },
-  },
-  voting: {
-    position: "absolute",
-    width: 70,
-    left: -80,
-    top: 18,
-    "@media only screen and (max-width: 767px)": {
-      display: "none",
-    },
-  },
-  mobileVoting: {
-    display: "none",
-    "@media only screen and (max-width: 767px)": {
-      display: "flex",
-    },
-  },
-  buttonDivider: {
-    height: 5,
-  },
-  topHeader: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "flex-start",
-    "@media only screen and (max-width: 767px)": {
-      flexDirection: "column",
-      justifyContent: "flex-start",
-      alignItems: "flex-start",
-    },
-  },
-  paperProgress: {
-    marginTop: 16,
-  },
-  mobileRow: {
-    "@media only screen and (max-width: 767px)": {
-      display: "flex",
-      justifyContent: "space-between",
-      alignContent: "center",
-      width: "100%",
-    },
-  },
-  mobileInfoSection: {
-    display: "none",
-    "@media only screen and (max-width: 767px)": {
-      display: "flex",
-      marginTop: 17,
-      marginBottom: 20,
-      fontSize: 14,
-    },
-  },
-  mobileDoi: {
-    display: "none",
-    "@media only screen and (max-width: 767px)": {
-      display: "flex",
-      opacity: 0.5,
-      fontSize: 14,
-    },
-  },
-  mobileTags: {
-    display: "none",
-    "@media only screen and (max-width: 767px)": {
-      display: "flex",
-      alignItems: "center",
-      marginTop: 20,
-    },
-  },
-  actionButton: {
-    width: 46,
-    height: 46,
-    borderRadius: "100%",
-    background: colors.LIGHT_GREY(1),
-    color: colors.GREY(1),
-    cursor: "pointer",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: 18,
-    marginLeft: 5,
-    marginRight: 5,
-    display: "flex",
-    flexShrink: 0,
-    "@media only screen and (max-width: 767px)": {
-      width: 35,
-      height: 35,
-    },
-    "@media only screen and (max-width: 415px)": {
-      height: 33,
-      width: 33,
-    },
-    "@media only screen and (max-width: 321px)": {
-      height: 31,
-      width: 31,
-    },
-  },
-  hide: {
-    display: "none",
-  },
-  space: {
-    marginTop: 30,
-  },
-  paperMetaContainerMobile: {
-    display: "none",
-    "@media only screen and (max-width: 767px)": {
-      display: "flex",
-    },
-  },
-  stickyComponent: {
-    display: "none",
-    height: 0,
-    "@media only screen and (max-width: 767px)": {
-      // top: 65,
-      top: -2,
-      position: "sticky",
-      backgroundColor: "#FFF",
-      zIndex: 3,
-      display: "flex",
-      height: "unset",
-      width: "100%",
-      boxSizing: "border-box",
-    },
-  },
-  scrollPadding: {
-    paddingTop: 450,
-  },
-  citationContainer: {
-    backgroundColor: "#fff",
-    padding: 50,
-    border: "1.5px solid #F0F0F0",
-    boxSizing: "border-box",
-    boxShadow: "0px 3px 4px rgba(0, 0, 0, 0.02)",
-    borderRadius: 4,
-    marginTop: 30,
-    "@media only screen and (max-width: 767px)": {
-      padding: 25,
-    },
-  },
-  citations: {
-    display: "flex",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    minWidth: "100%",
-    width: "100%",
-    boxSizing: "border-box",
-    overflowX: "scroll",
-    paddingBottom: 10,
-  },
-  header: {
-    display: "flex",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    width: "100%",
-    marginBottom: 20,
-  },
-  citationTitle: {
-    fontSize: 22,
-    fontWeight: 500,
-    "@media only screen and (max-width: 415px)": {
-      fontSize: 20,
-    },
-  },
-  citationCount: {
-    color: "rgba(36, 31, 58, 0.5)",
-    fontSize: 17,
-    fontWeight: 500,
-    marginLeft: 15,
-  },
-  citationEmpty: {
-    fontSize: 20,
-    fontWeight: 500,
-    width: "100%",
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    "@media only screen and (max-width: 415px)": {
-      fontSize: 16,
-    },
-  },
-  icon: {
-    fontSize: 50,
-    color: "rgb(78, 83, 255)",
-    height: 50,
-    marginBottom: 25,
-  },
-  citationEmptySubtext: {
-    fontSize: 16,
-    color: "rgba(36, 31, 58, 0.8)",
-    fontWeight: 400,
-    marginTop: 10,
-    "@media only screen and (max-width: 415px)": {
-      fontSize: 12,
-    },
-  },
-  paperPageContainer: {
-    display: "flex",
-    flexDirection: "column",
-    border: "1.5px solid #F0F0F0",
-    backgroundColor: "#fff",
-    boxShadow: "0px 3px 4px rgba(0, 0, 0, 0.02)",
-    padding: "20px 30px 30px 90px",
-    boxSizing: "border-box",
-    borderRadius: 4,
-    "@media only screen and (max-width: 767px)": {
-      borderRadius: "0px",
-      borderTop: "none",
-      padding: 20,
-      width: "100%",
-    },
-  },
-  noMarginLeft: {
-    padding: 30,
-    marginTop: 30,
-  },
-  top: {
-    "@media only screen and (max-width: 767px)": {
-      borderBottom: "none",
-    },
-  },
-  bottom: {
-    // borderTop: "none",
-    paddingTop: 0,
-  },
-  componentWrapper: {
-    width: 1200,
-    boxSizing: "border-box",
-  },
-  abstractText: {
-    lineHeight: 1.6,
-  },
-  figuresContainer: {
-    marginTop: 32,
-  },
-  limitsContainer: {
-    marginTop: 30,
   },
 });
 
