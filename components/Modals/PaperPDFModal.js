9017d90175fda2e9ec00b6dc0541108c84310bbf
Kobe Attias <kobeattias@gmail.com>
2022-06-02 17:33:26 -0400

    #1190 fix for missing function

--- components/Modals/PaperPDFModal.js
+++ components/Modals/PaperPDFModal.js
@@ -13,17 +13,16 @@ import { convertHttpToHttps } from "~/config/utils/routing";
 const PaperPDFModal = (props) => {
   const { paper, modals, openPaperPDFModal, onClose } = props;
   const { file, pdf_url } = paper;
-  const fitToWidthFile =
-    file +
-    function closeModal() {
-      /**
-       * it is helpful to have onClose callback since there
-       * may be many PaperPDFModal's rendered, but modals.openPaperPDFModal is only a single boolean
-       * and cannot specify which one is open */
-      onClose && onClose();
 
-      return openPaperPDFModal && openPaperPDFModal(false);
-    };
+  function closeModal() {
+    /**
+     * it is helpful to have onClose callback since there
+     * may be many PaperPDFModal's rendered, but modals.openPaperPDFModal is only a single boolean
+     * and cannot specify which one is open */
+    onClose && onClose();
+
+    return openPaperPDFModal && openPaperPDFModal(false);
+  }
 
   return (
     <BaseModal
