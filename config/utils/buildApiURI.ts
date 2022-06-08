8ce99ce2048faf20ed5e21db57a1099ac841f52d
Calvin Lee <calvinhlee23@gmail.com>
2022-06-08 02:55:43 -0700

    ..

--- config/utils/buildApiURI.ts
+++
@@ -1,5 +0,0 @@
-import { getApiRootURI } from "~/config/utils/getApiRootURI";
-
-export function buildApiURI({ apiPath }: { apiPath: string }): string {
-  return `http://${getApiRootURI()}/api/${apiPath}/`;
-}
