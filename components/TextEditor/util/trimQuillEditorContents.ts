a8526deef9ab367feca3f41e350d191abc1db89a
Kobe Attias <kobeattias@gmail.com>
2022-07-20 14:43:25 -0400

    [Editor] Bug: Removing bullet point if last item

--- components/TextEditor/util/trimQuillEditorContents.ts
+++ components/TextEditor/util/trimQuillEditorContents.ts
@@ -1,9 +1,10 @@
 export default function trimQuillEditorContents ({ contents }) {
   const deltas = Array.isArray(contents) ? contents : contents?.ops ? contents.ops : [];
 
+  console.log('deltas', deltas)
   if (deltas.length > 0) {
     const firstDelta = deltas[0];
-    const isFirstDeltaString = typeof firstDelta?.insert === "string";
+    const isFirstDeltaString = (typeof firstDelta?.insert === "string" && !firstDelta.attributes);
 
     if (isFirstDeltaString) {
       let trimmedStr = firstDelta.insert.trimStart();
@@ -11,7 +12,7 @@ export default function trimQuillEditorContents ({ contents }) {
     }
 
     const lastDelta = deltas[deltas.length - 1];
-    const isLastDeltaString = typeof lastDelta?.insert === "string";
+    const isLastDeltaString = (typeof lastDelta?.insert === "string" && !lastDelta.attributes);
     if (isLastDeltaString) {
       let trimmedStr = lastDelta.insert.trimEnd();
       deltas[deltas.length-1].insert = trimmedStr;
