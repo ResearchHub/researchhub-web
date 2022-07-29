65731883964d9f7f05b8950df062144c8e69d164
Calvin Lee <calvinhlee23@gmail.com>
2022-07-29 09:03:24 -0700

    Show title of the note in tab & bugfix

--- config/utils/unescapeHtmlString.ts
+++ config/utils/unescapeHtmlString.ts
@@ -1,8 +1,11 @@
-export function unescapeHtmlString(htmlStr: string): string {
-  return htmlStr
-    .replace(/&lt;/g, "<")
-    .replace(/&gt;/g, ">")
-    .replace(/&quot;/g, '"')
+import { NullableString } from "../types/root_types";
+
+export function unescapeHtmlString(htmlStr: NullableString): string {
+  return (htmlStr ?? "")
     .replace(/&#39;/g, "'")
-    .replace(/&amp;/g, "&");
+    .replace(/&amp;/g, "&")
+    .replace(/&gt;/g, ">")
+    .replace(/&lt;/g, "<")
+    .replace(/&nbsp;/g, " ")
+    .replace(/&quot;/g, '"');
 }
