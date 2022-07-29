8b34da7e5ea82abeee5b1ce975f6edb742f80821
Calvin Lee <calvinhlee23@gmail.com>
2022-07-29 07:31:52 -0700

    unesacping html strings for titles

---
+++ config/utils/unescapeHtmlString.ts
@@ -0,0 +1,8 @@
+export function unescapeHtmlString(htmlStr: string): string {
+  return htmlStr
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&amp;/g, "&");
+}
