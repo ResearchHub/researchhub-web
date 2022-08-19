b44f1ec1032d7380b92332cf2803b191d60e87d6
Calvin Lee <calvinhlee23@gmail.com>
2022-08-19 00:01:03 -0700

    update buildApiUri so the queryParams are optional

--- config/utils/buildApiUri.ts
+++ config/utils/buildApiUri.ts
@@ -1,6 +1,6 @@
 import { getApiRootURI } from "~/config/utils/getApiRootURI";
 import { isDevEnv } from "./env";
 
-export function buildApiUri({ apiPath, queryString }: { apiPath: string, queryString: string }): string {
+export function buildApiUri({ apiPath, queryString }: { apiPath: string, queryString?: string }): string {
   return `${isDevEnv() ? "http:" : "https:"}//${getApiRootURI()}/api/${apiPath}/${queryString}`;
 }
