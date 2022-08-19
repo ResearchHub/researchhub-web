4e923cb1ffdc32cb053a19e3338fab31c241c15a
Calvin Lee <calvinhlee23@gmail.com>
2022-08-19 10:59:02 -0700

    update queryString for buildApiUri

--- config/utils/buildApiUri.ts
+++ config/utils/buildApiUri.ts
@@ -1,6 +1,14 @@
 import { getApiRootURI } from "~/config/utils/getApiRootURI";
 import { isDevEnv } from "./env";
 
-export function buildApiUri({ apiPath, queryString }: { apiPath: string, queryString?: string }): string {
-  return `${isDevEnv() ? "http:" : "https:"}//${getApiRootURI()}/api/${apiPath}/${queryString}`;
+export function buildApiUri({
+  apiPath,
+  queryString,
+}: {
+  apiPath: string;
+  queryString?: string;
+}): string {
+  return `${
+    isDevEnv() ? "http:" : "https:"
+  }//${getApiRootURI()}/api/${apiPath}/${queryString ?? ""}`;
 }
