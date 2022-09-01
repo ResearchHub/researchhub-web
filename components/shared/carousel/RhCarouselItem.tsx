bd25d030042efb9bea967e17d3356142f85354eb
Calvin Lee <calvinhlee23@gmail.com>
2022-09-01 12:18:29 -0700

    finalizing Carousel

--- components/shared/carousel/RhCarouselItem.tsx
+++ components/shared/carousel/RhCarouselItem.tsx
@@ -1,20 +1,51 @@
 import { css, StyleSheet } from "aphrodite";
-import { ReactElement } from "react";
+import { ReactElement, ReactNode } from "react";
+import { NullableString } from "~/config/types/root_types";
 
 export type RhCarouselItemProps = {
-  title: string;
-  body: string;
+  title?: ReactNode;
+  body: ReactNode;
 };
 
-export default function RhCarouselItem({}: RhCarouselItemProps): ReactElement {
-  return <div className={css(styles.rhCarouselItem)}>I am an Item</div>;
+export default function RhCarouselItem({
+  title,
+  body,
+}: RhCarouselItemProps): ReactElement {
+  return (
+    <div className={css(styles.rhCarouselItemRoot)}>
+      {title}
+      {body}
+    </div>
+  );
 }
 
 const styles = StyleSheet.create({
-  rhCarouselItem: {
-    background: "red",
+  rhCarouselItemRoot: {
+    background: "transparent",
+    display: "flex",
+    flexDirection: "column",
     height: "100%",
     minWidth: 248,
     width: "100%",
+  },
+});
+
+export const DEFAULT_ITEM_STYLE = StyleSheet.create({
+  rhCarouselItemTitle: {
+    alignItems: "center",
+    display: "flex",
+    fontSize: 16,
+    fontWeight: 500,
+    height: "100%",
+    marginBottom: 4,
+    textOverflow: "ellipsis",
+    width: "100%",
+  },
+  rhCarouselItemBody: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    fontSize: 12,
+    textOverflow: "ellipsis",
   },
 });
