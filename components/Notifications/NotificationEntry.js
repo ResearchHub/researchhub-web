1f46496c45dc2d3e0eb2cc6fc71841fcb5bf2bde
Leo <leo.sun@berkeley.edu>
2022-09-23 17:25:08 -0700

    removing notification formatting. code cleanup

--- components/Notifications/NotificationEntry.js
+++ components/Notifications/NotificationEntry.js
@@ -34,414 +34,88 @@ const NotificationEntry = (props) => {
     props.closeMenu();
   };
 
-  const truncateText = (title) => {
-    if (title && title.length >= 90) {
-      return title.slice(0, 90).trim() + "...";
-    }
-    return title;
-  };
-
-  const formatUsername = (userObject) => {
-    if (!doesNotExist(userObject)) {
-      let { first_name, last_name } = userObject;
-      return `${first_name} ${last_name}`;
-    }
-    return "";
-  };
-
   const handleNavigation = (e) => {
     e && e.stopPropagation();
-    const {
-      content_type,
-      document_id,
-      slug,
-      support_type,
-      parent_content_type,
-      paper_official_title,
-      document_type,
-      paper_title,
-    } = notification;
+    const { navigation_url } = notification;
 
-    if (content_type === "withdrawal") {
-      return null;
+    if (navigation_url == null) {
+      return;
     }
 
-    const formattedSlug = !isNullOrUndefined(slug)
-      ? slug
-      : buildSlug(paper_official_title ?? paper_title);
-    const isComment = ["thread", "comment", "reply"].includes(support_type);
-    // TODO: calvinhlee check with leo with parent_content_type
-    const documentType = isComment ? parent_content_type : document_type;
-    const hrefPattern = `/[document_type]/[document_id]/[formatted_slug]`;
-    const route =
-      `/${documentType}/${document_id}/${formattedSlug}` +
-      (isComment
-        ? "#comments"
-        : [
-            "thread",
-            "comment",
-            "reply",
-            "vote_thread",
-            "vote_reply",
-            "vote_comment",
-          ].includes(content_type)
-        ? "#comments"
-        : ["bullet_point", "vote_bullet"].includes(content_type)
-        ? "#takeaways"
-        : ["summary", "vote_summary"].includes(content_type)
-        ? "#summary"
-        : "");
-
-    Router.push(hrefPattern, route).then(() => {
+    Router.push(navigation_url).then(() => {
       markAsRead(props.data);
       props.closeMenu(); // added as a fallback
     });
   };
 
-  const renderMessage = () => {
-    const { notification } = props;
-    const {
-      action_item,
-      action_tip,
-      content_type,
-      created_by,
-      created_date,
-      document_id: documentId,
-      document_title,
-      document_type,
-      slug,
-      thread_id: threadId,
-      toAddress,
-      txHash,
-      withdrawnAmount,
-    } = notification;
-
-    if (content_type === "verdict" && !action_item?.is_content_removed) {
-      return;
+  const formatStyles = (customStyles) => {
+    if (customStyles != null) {
+      const parsedStyles = JSON.parse(customStyles);
+      const formatedStyles = parsedStyles.map((key) => {
+        switch (key) {
+          case "bold":
+            return styles.bold;
+          case "italic":
+            return styles.italic;
+          case "link":
+            return styles.link;
+          default:
+            return null;
+        }
+      });
+      return formatedStyles;
     }
+    return [];
+  };
 
-    const {
-      first_name: creatorFName,
-      last_name: creatorLName,
-      author_profile: creatorProfile,
-    } = created_by;
-
-    const authorId = creatorProfile?.id ?? null;
-    const formattedSlug = slug ?? buildSlug(document_title);
-    const creatorName = creatorFName ?? "" + creatorLName ?? "";
-
+  const formatBody = (body) => {
     const onClick = (e) => {
       e.stopPropagation();
       markAsRead(data);
       props.closeMenu();
     };
 
-    const authorLink = {
-      href: "/user/[authorId]/[tabName]",
-      as: `/user/${authorId}/overview`,
-    };
-
-    let documentLink;
-    let discussionLink;
-    if (document_type === "paper") {
-      documentLink = {
-        href: "/paper/[paperId]/[paperName]",
-        as: `/${document_type}/${documentId}/${formattedSlug}`,
-      };
-      discussionLink = {
-        href: "/paper/[paperId]/[paperName]",
-        as: `/${document_type}/${documentId}/${formattedSlug}`,
-      };
-    } else if (document_type === "question") {
-      documentLink = {
-        href: "/[document_type]/[documentId]/[title]",
-        as: `/post/${documentId}/${formattedSlug}`,
-      };
-      discussionLink = {
-        href: "/[document_type]/[documentId]/[title]",
-        as: `/post/${documentId}/${formattedSlug}`,
-      };
-    } else {
-      documentLink = {
-        href: "/[document_type]/[documentId]/[title]",
-        as: `/${document_type}/${documentId}/${formattedSlug}`,
-      };
-      discussionLink = {
-        href: "/[document_type]/[documentId]/[title]",
-        as: `/${document_type}/${documentId}/${formattedSlug}`,
-      };
+    if (body == null) {
+      return null;
     }
 
-    const sectionLink = (section) => {
-      let as = documentLink.as + "#" + section;
-      return { ...documentLink, as };
-    };
-
-    const timeStamp = <TimeStamp date={created_date} />;
-    const notifCreator = (
-      <HyperLink
-        link={authorLink}
-        onClick={onClick}
-        style={styles.username}
-        text={creatorName}
-      />
-    );
-
-    const etherscanLink = getEtherscanLink(txHash);
-    switch (content_type) {
-      case "withdrawal":
-        return (
-          <div className={css(styles.message)}>
-            Your withdrawal of {withdrawnAmount} RSC has now been completed!
-            <br />
-            <br />
-            View the transaction at{" "}
-            <a
-              href={etherscanLink}
-              target="_blank"
-              className={css(styles.metatext, styles.noUnderline)}
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              <div className={css(styles.metatext)}>{etherscanLink}</div>
-            </a>
-          </div>
-        );
-      case "verdict":
-        const flaggedContentName = action_item?.flagged_content_name;
-        const isDiscussionFlagged = ["comment", "thread", "reply"].includes(
-          flaggedContentName
-        );
-        return (
-          <div className={css(styles.message)}>
-            {` A ResearchHub Editor has removed your `}
-            <HyperLink
-              link={documentLink}
-              onClick={onClick}
-              style={styles.link}
-              text={flaggedContentName}
-            />
-            {`for ${
-              FLAG_REASON[action_item?.verdict_choice ?? ""]?.toLowerCase() ??
-              "spam"
-            }`}
-            {isDiscussionFlagged && (
-              <Fragment>
-                {" in "}
+    return (
+      <div>
+        {body.map((element) => {
+          const extraStyles = formatStyles(element.extra);
+          switch (element.type) {
+            case "link":
+              const link = { href: element.link };
+              return (
                 <HyperLink
-                  link={documentLink}
+                  link={link}
                   onClick={onClick}
-                  style={styles.link}
-                  text={truncateText(document_title)}
+                  style={extraStyles}
+                  text={element.value}
                 />
-              </Fragment>
-            )}
-            {timeStamp}
-          </div>
-        );
-      case "paper":
-      case "post":
-        return (
-          <div className={css(styles.message)}>
-            {notifCreator}
-            {` uploaded a new paper ${content_type}`}
-            <HyperLink
-              link={documentLink}
-              onClick={onClick}
-              style={styles.paper}
-              dataTip={document_title}
-              text={truncateText(document_title)}
-            />
-            {timeStamp}
-          </div>
-        );
-      case "thread":
-        return (
-          <div className={css(styles.message)}>
-            {notifCreator}
-            {" created a "}
-            <HyperLink
-              link={discussionLink}
-              onClick={onClick}
-              link={sectionLink("comments")}
-              style={styles.link}
-              text={"thread"}
-            />
-            {"in "}
-            <HyperLink
-              link={documentLink}
-              onClick={onClick}
-              style={styles.paper}
-              dataTip={document_title}
-              text={truncateText(document_title)}
-            />
-            {timeStamp}
-          </div>
-        );
-      case "comment":
-        return (
-          <div className={css(styles.message)}>
-            {notifCreator}
-            {" left a "}
-            <HyperLink
-              dataTip={action_tip}
-              link={sectionLink("comments")}
-              onClick={onClick}
-              style={styles.link}
-              text={"comment"}
-            />
-            {"in your thread: "}
-            <HyperLink
-              dataTip={action_tip}
-              link={sectionLink("comments")}
-              onClick={onClick}
-              style={styles.paper}
-              text={truncateText(action_tip)}
-            />
-            {timeStamp}
-          </div>
-        );
-      case "reply":
-        return (
-          <div className={css(styles.message)}>
-            {notifCreator}
-            {" left a "}
-            <HyperLink
-              dataTip={action_tip}
-              link={sectionLink("comments")}
-              onClick={onClick}
-              style={styles.link}
-              text={"reply"}
-            />
-            {`to a comment `}
-            <HyperLink
-              dataTip={action_tip}
-              link={sectionLink("comments")}
-              onClick={onClick}
-              style={styles.paper}
-              text={truncateText(action_tip)}
-            />
-            {"in "}
-            <HyperLink
-              link={documentLink}
-              onClick={onClick}
-              style={styles.paper}
-              dataTip={document_title}
-              text={truncateText(document_title)}
-            />
-            {timeStamp}
-          </div>
-        );
-      case "purchase": // synanomous to "support"
-        return renderContentSupportNotification();
-      case "bounty":
-        const expirationTimeStamp = (
-          <TimeStamp date={action_item.expiration_date} timeSince={true} />
-        );
-        return (
-          <div className={css(styles.message)}>
-            {" Your bounty is expiring in "}
-            <b>{" 24 hours."}</b>
-            {" Please award it to the best answer. "}
-            <HyperLink
-              link={documentLink}
-              onClick={onClick}
-              style={styles.paper}
-              dataTip={document_title}
-              text={truncateText(document_title)}
-            />
-            {expirationTimeStamp}
-          </div>
-        );
-      default:
-        return;
-    }
+              );
+            case "text":
+              return (
+                <span className={css(...extraStyles)}> {element.value} </span>
+              );
+            default:
+              return "ERROR";
+          }
+        })}
+      </div>
+    );
   };
 
-  const renderContentSupportNotification = () => {
-    const {
-      contribution_amount,
-      created_by,
-      created_date,
-      document_id: documentId,
-      document_type: documentType,
-      slug,
-    } = props.notification;
-
-    const {
-      first_name: creatorFName,
-      last_name: creatorLName,
-      author_profile: creatorProfile,
-    } = created_by;
-
-    const formattedSlug = slug ?? buildSlug(documentType);
-    const creatorName = creatorFName ?? "" + creatorLName ?? "";
-
-    const formatLink = () => {
-      const link = {
-        // href: "/[documentType]/[paperId]/[paperName]",
-        href: `/${documentType}/${documentId}/${formattedSlug}`,
-      };
-
-      switch (documentType) {
-        case "thread":
-        case "comment":
-        case "reply":
-          link.as += "#comments";
-          break;
-        default:
-          break;
-      }
-
-      return link;
-    };
+  const renderMessage = () => {
+    const { notification } = props;
+    const { body, created_date } = notification;
+    const timeStamp = <TimeStamp date={created_date} />;
+    const formatedBody = formatBody(body);
 
     return (
       <div className={css(styles.message)}>
-        {"Congrats! "}
-        <img
-          className={css(styles.iconCongrats)}
-          src={"/static/icons/party-popper.png"}
-          alt="Party Popper Icon"
-        />
-        {" Your "}
-        <Link {...formatLink()}>
-          <a
-            onClick={(e) => {
-              e.stopPropagation();
-              markAsRead(data);
-              props.closeMenu();
-            }}
-            className={css(styles.link)}
-          >
-            {documentType}
-          </a>
-        </Link>
-        {"has been awarded "}
-        <span className={css(styles.rsc)}>{`${Number(
-          contribution_amount
-        )} RSC`}</span>
-        {" by "}
-        <Link
-          href={"/user/[authorId]/[tabName]"}
-          as={`/user/${creatorProfile?.id ?? ""}/overview`}
-        >
-          <a
-            onClick={(e) => {
-              e.stopPropagation();
-              markAsRead(data);
-              props.closeMenu();
-            }}
-            className={css(styles.username)}
-          >
-            {creatorName + ". "}
-          </a>
-        </Link>
-        <span className={css(styles.timestamp)}>
-          <span className={css(styles.timestampDivider)}>•</span>
-          {timeAgoStamp(created_date)}
-        </span>
+        {formatedBody}
+        {timeStamp}
       </div>
     );
   };
@@ -465,6 +139,12 @@ const NotificationEntry = (props) => {
 };
 
 const styles = StyleSheet.create({
+  bold: {
+    fontWeight: "bold",
+  },
+  italic: {
+    fontStyle: "italic",
+  },
   container: {
     display: "flex",
     alignItems: "flex-start",
@@ -511,7 +191,6 @@ const styles = StyleSheet.create({
     color: colors.BLUE(),
     cursor: "pointer",
     textDecoration: "none",
-    paddingRight: 4,
     ":hover": {
       textDecoration: "underline",
     },
