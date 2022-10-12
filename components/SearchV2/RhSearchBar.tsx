a0be85b896f8956670e566bb8516513030303f1e
Calvin Lee <calvinhlee23@gmail.com>
2022-10-12 16:29:44 -0700

    display logic finished for RhSearchbar

--- components/SearchV2/RhSearchBar.tsx
+++ components/SearchV2/RhSearchBar.tsx
@@ -4,7 +4,14 @@ import { pickFiltersForApp, QUERY_PARAM } from "~/config/utils/search";
 import { trackEvent } from "~/config/utils/analytics";
 import { useRouter } from "next/router";
 import { useSelector, useStore } from "react-redux";
-import { useState, useEffect, useRef, Fragment, ReactElement } from "react";
+import {
+  useState,
+  useEffect,
+  useRef,
+  Fragment,
+  ReactElement,
+  RefObject,
+} from "react";
 import colors from "~/config/themes/colors";
 import icons from "~/config/themes/icons";
 import PropTypes from "prop-types";
@@ -15,35 +22,27 @@ const RETURN_KEY = 13;
 const DEFAULT_EXPANDED_SEARCH_HEIGHT = 66;
 
 type SearchProps = {
-  expendableSearchbarRef?: HTMLInputElement;
-  searchbarRef?: HTMLInputElement;
+  expendableSearchbarRef?: RefObject<HTMLInputElement>;
+  pushSearchToUrlAndTrack: () => void;
+  searchbarRef?: RefObject<HTMLInputElement>;
   searchString: NullableString;
   setSearchString: (query: NullableString) => void;
 };
-
-function RhSearchBarInput({}): ReactElement {
-  return <></>;
-}
-
-function RhSearchBarExpandableInput({}): ReactElement {
-  return <></>;
-}
 
 /* 
   The real execution of search is being done at the page level. 
   The only real responsibility of this component is to push the query to URL for the page to read
 */
-export default function RhSearchBar({}): ReactElement {
+export default function RhSearchBar(): ReactElement {
   const auth = useStore()?.getState()?.auth;
   const router = useRouter();
   const [searchString, setSearchString] = useState<NullableString>(
-    // Extracting query "q" from the URL
     ((router?.query ?? {})?.[QUERY_PARAM] ?? [])[0] ?? null
   );
   const expendableSearchbarRef = useRef<HTMLInputElement>(null);
   const searchbarRef = useRef<HTMLInputElement>(null);
 
-  const blurAndCloseDeviceKeyboard = () => {
+  const blurAndCloseDeviceKeyboard = (): void => {
     if (isServer()) {
       return;
     } else {
@@ -81,5 +80,45 @@ export default function RhSearchBar({}): ReactElement {
     });
   };
 
-  return <Fragment></Fragment>;
+  const searchProps: SearchProps = {
+    expendableSearchbarRef,
+    pushSearchToUrlAndTrack,
+    searchbarRef,
+    searchString,
+    setSearchString,
+  };
+
+  return (
+    <Fragment>
+      <div
+        children={<RhSearchBarInput {...searchProps} />}
+        style={{
+          display: "block",
+          width: "100%",
+          [`@media only screen and (max-width: ${breakpoints.large.str})`]: {
+            display: "none",
+            width: 0,
+          },
+        }}
+      />
+      <div
+        children={<RhSearchBarExpandableInput {...searchProps} />}
+        style={{
+          display: "none",
+          width: 0,
+          [`@media only screen and (max-width: ${breakpoints.large.str})`]: {
+            display: "block",
+          },
+        }}
+      />
+    </Fragment>
+  );
+}
+
+function RhSearchBarInput({}: SearchProps): ReactElement {
+  return <></>;
+}
+
+function RhSearchBarExpandableInput({}: SearchProps): ReactElement {
+  return <></>;
 }
