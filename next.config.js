5dbbf16946000f37f754aea22ca2f9f7cc4e23a5
Kobe Attias <kobeattias@gmail.com>
2022-11-28 15:39:22 -0500

    enabling sentry

--- next.config.js
+++ next.config.js
@@ -93,12 +93,12 @@ const nextConfig = {
 
 module.exports = withTM({ ...nextConfig });
 
-// const SentryWebpackPluginOptions = {
-//   silent: true,
-//   disableClientWebpackPlugin: true,
-// };
+const SentryWebpackPluginOptions = {
+  silent: true,
+  disableClientWebpackPlugin: true,
+};
 
-// module.exports = withSentryConfig(
-//   withTM({ ...nextConfig }),
-//   SentryWebpackPluginOptions
-// );
+module.exports = withSentryConfig(
+  withTM({ ...nextConfig }),
+  SentryWebpackPluginOptions
+);
