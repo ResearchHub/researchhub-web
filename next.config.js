04f0707f52578a0715456ce7f48a964d1c77e430
Kobe Attias <kobeattias@gmail.com>
2022-11-30 19:50:14 -0500

    reverting change

--- next.config.js
+++ next.config.js
@@ -14,9 +14,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  experimental: {
-    runtime: "experimental-edge", // 'node.js' (default) | 'experimental-edge'
-  },
+  // experimental: {
+  //   runtime: "experimental-edge", // 'node.js' (default) | 'experimental-edge'
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
