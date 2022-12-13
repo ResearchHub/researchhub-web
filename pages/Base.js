b799eee6aca52bcf7ba75df7830d1a81f3c5571d
Kobe Attias <kobeattias@gmail.com>
2022-12-13 14:38:39 -0500

    GA: Quotes

--- pages/Base.js
+++ pages/Base.js
@@ -89,7 +89,7 @@ function Base({
               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
               })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-              ga('create', ${process.env.GA_TRACKING_ID}, 'auto');
+              ga('create', '${process.env.GA_TRACKING_ID}', 'auto');
               ga('send', 'pageview');
             `}
           </Script>
