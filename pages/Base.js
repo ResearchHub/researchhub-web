36ea8604299ea8de68e737bdcea162d8176af449
Kobe Attias <kobeattias@gmail.com>
2022-12-13 13:37:02 -0500

    Testing GA4 setup via tag manager

--- pages/Base.js
+++ pages/Base.js
@@ -80,18 +80,22 @@ function Base({
 
   return (
     <AlertProvider template={DynamicAlertTemplate} {...options}>
-      {process.env.GA_TRACKING_ID && (
-        <Script id="google-analytics" strategy="afterInteractive">
+      {process.env.TAG_MANAGER_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.TAG_MANAGER_ID}`}
+            strategy="afterInteractive"
+          />
+          {/* <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-            ga('create', '${process.env.GA_TRACKING_ID}', 'auto');
-            ga('send', 'pageview');
+            gtag('config', '${process.env.TAG_MANAGER_ID}');
           `}
-        </Script>
+        </Script> */}
+        </>
       )}
 
       <NavbarContext.Provider
