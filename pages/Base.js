4fabecd66f6f496c99ee5ecca80c27918101ac68
Kobe Attias <kobeattias@gmail.com>
2022-12-13 15:01:14 -0500

    yet another GA test

--- pages/Base.js
+++ pages/Base.js
@@ -80,18 +80,20 @@ function Base({
 
   return (
     <AlertProvider template={DynamicAlertTemplate} {...options}>
-      {process.env.GA_TRACKING_ID && (
+      {true && (
         <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=G-QWCEK6DGHW`}
+            strategy="afterInteractive"
+          />
           <Script id="google-analytics" strategy="afterInteractive">
             {`
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-              ga('create', '${process.env.GA_TRACKING_ID}', 'auto');
-              ga('send', 'pageview');
-            `}
+            gtag('config', 'G-QWCEK6DGHW');
+          `}
           </Script>
         </>
       )}
