6514738d385a94c690df63737d8955a42f91fe8b
Leo <leo.sun@berkeley.edu>
2023-02-07 10:30:00 -0800

    changing bounty expiration date from 7 to 30 days

--- config/types/bounty.ts
+++ config/types/bounty.ts
@@ -111,8 +111,8 @@ export default class Bounty {
     // TODO: Change hard coded value
 
     const today = new Date();
-    const sevenDaysFromNow = new Date(
-      today.getTime() + 7 * 24 * 60 * 60 * 1000
+    const thirtyDaysFromNow = new Date(
+      today.getTime() + 30 * 24 * 60 * 60 * 1000
     );
 
     const data = {
@@ -120,7 +120,7 @@ export default class Bounty {
       item_content_type: itemContentType,
       item_object_id: itemObjectId,
       effort_level: effortLevel,
-      expiration_date: sevenDaysFromNow,
+      expiration_date: thirtyDaysFromNow,
     };
 
     return new Promise((resolve, reject) => {
