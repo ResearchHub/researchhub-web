93c3cf14f52ef4d22c2866869888ec640618f5b9
Calvin Lee <calvinhlee23@gmail.com>
2023-02-16 13:58:16 -0800

    migrating references container to main site repo

--- pages/reference-manager/components/references/ReferencesContainer.tsx
+++ pages/reference-manager/components/references/ReferencesContainer.tsx
@@ -1,16 +1,15 @@
-// import { css } from "aphrodite";
-// import { styles } from "./styles/ReferencesContainerStyle";
 import {
   Box,
   IconButton,
   InputAdornment,
-  TextField,
   Typography,
   OutlinedInput,
 } from "@mui/material";
 import { useState } from "react";
+import { ReferencesTabContextProvider } from "./context/ReferencesTabContext";
 import DropdownMenu from "../shared/menu/DropdownMenu";
 import ExpandMore from "@mui/icons-material/ExpandMore";
+import ReferenceItemTab from "./reference_item/ReferenceItemTab";
 import ReferencesTable from "./ReferencesTable";
 import TableChartOutlinedIcon from "@mui/icons-material/TableChartOutlined";
 import type { ReactElement } from "react";
@@ -20,101 +19,104 @@ interface Props {}
 export default function ReferencesContainer({}: Props): ReactElement {
   const [searchText, setSearchText] = useState<string | null>(null);
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", padding: "16px 0 0" }}>
-      <div style={{ marginBottom: 32 }}>
-        <Typography variant="h5" sx={{ fontWeight: 600 }}>
-          {"All References"}
-        </Typography>
-      </div>
-      <Box className="ReferencesContainerMain">
-        <Box
-          className="ReferencesContainerTitleSection"
-          sx={{
-            alignItems: "center",
-            display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
-            height: 44,
-            marginBottom: "20px",
-          }}
-        >
-          <DropdownMenu
-            menuItemProps={[
-              { itemLabel: "File(s) from computer", onClick: () => {} },
-              { itemLabel: "Import library", onClick: () => {} },
-            ]}
-            menuLabel={
-              <div
-                style={{
-                  alignItems: "center",
-                  color: "rgba(170, 168, 180, 1)",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: 68,
-                  height: 26,
-                  boxSizing: "border-box",
-                }}
-              >
-                <TableChartOutlinedIcon
-                  fontSize="medium"
-                  sx={{ color: "#7C7989" }}
-                />
-                <ExpandMore fontSize="medium" sx={{ color: "#AAA8B4" }} />
-              </div>
-            }
-            size="medium"
-          />
-          <div
-            className="ReferenceContainerSearchFieldWrap"
-            style={{
-              maxWidth: 400,
+    <ReferencesTabContextProvider>
+      <ReferenceItemTab />
+      <Box
+        sx={{ display: "flex", flexDirection: "column", padding: "16px 0 0" }}
+      >
+        <div style={{ marginBottom: 32 }}>
+          <Typography variant="h5" sx={{ fontWeight: 600 }}>
+            {"All References"}
+          </Typography>
+        </div>
+        <Box className="ReferencesContainerMain">
+          <Box
+            className="ReferencesContainerTitleSection"
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              justifyContent: "space-between",
               width: "100%",
+              height: 44,
+              marginBottom: "20px",
             }}
           >
-            <OutlinedInput
-              fullWidth
-              label={searchText && "Search"}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                // TODO: calvinhlee - create a MUI convenience function for handling target values
-                setSearchText(event.target.value);
-              }}
-              placeholder="Search..."
-              size="small"
-              sx={{
-                borderColor: "#E9EAEF",
-                background: "rgba(250, 250, 252, 1)",
-
-                "&:hover": {
-                  borderColor: "#E9EAEF",
-                },
-              }}
-              inputProps={{
-                sx: {
-                  border: "0px !important",
-
-                  "&:hover": {
-                    border: "0px",
-                  },
-                },
-              }}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    edge="end"
-                  >
-                    <i
-                      className="fa-regular fa-magnifying-glass"
-                      style={{ fontSize: 16 }}
-                    ></i>
-                  </IconButton>
-                </InputAdornment>
+            <DropdownMenu
+              menuItemProps={[
+                { itemLabel: "File(s) from computer", onClick: () => {} },
+                { itemLabel: "Import library", onClick: () => {} },
+              ]}
+              menuLabel={
+                <div
+                  style={{
+                    alignItems: "center",
+                    color: "rgba(170, 168, 180, 1)",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    width: 68,
+                    height: 26,
+                    boxSizing: "border-box",
+                  }}
+                >
+                  <TableChartOutlinedIcon
+                    fontSize="medium"
+                    sx={{ color: "#7C7989" }}
+                  />
+                  <ExpandMore fontSize="medium" sx={{ color: "#AAA8B4" }} />
+                </div>
               }
+              size="medium"
             />
-          </div>
+            <div
+              className="ReferenceContainerSearchFieldWrap"
+              style={{
+                maxWidth: 400,
+                width: "100%",
+              }}
+            >
+              <OutlinedInput
+                fullWidth
+                label={searchText && "Search"}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  // TODO: calvinhlee - create a MUI convenience function for handling target values
+                  setSearchText(event.target.value);
+                }}
+                placeholder="Search..."
+                size="small"
+                sx={{
+                  borderColor: "#E9EAEF",
+                  background: "rgba(250, 250, 252, 1)",
+                  "&:hover": {
+                    borderColor: "#E9EAEF",
+                  },
+                }}
+                inputProps={{
+                  sx: {
+                    border: "0px !important",
+                    "&:hover": {
+                      border: "0px",
+                    },
+                  },
+                }}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      edge="end"
+                    >
+                      <i
+                        className="fa-regular fa-magnifying-glass"
+                        style={{ fontSize: 16 }}
+                      ></i>
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </div>
+          </Box>
+          <ReferencesTable />
         </Box>
-        <ReferencesTable />
       </Box>
-    </Box>
+    </ReferencesTabContextProvider>
   );
 }
--- pages/reference-manager/components/references/ReferencesTable.tsx
+++ pages/reference-manager/components/references/ReferencesTable.tsx
@@ -1,139 +1,12 @@
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { columns, rows } from "./table_mock_data";
+import { DataGrid } from "@mui/x-data-grid";
 import { useState } from "react";
-
-const columns: GridColDef[] = [
-  { field: "id", headerName: "", width: 0 },
-  { field: "title", headerName: "Title", width: 380 },
-  { field: "authors", headerName: "Authors", width: 280 },
-  { field: "last_author", headerName: "Last Author", width: 240 },
-  {
-    field: "hubs",
-    headerName: "Hubs",
-    description: "This is Hub Column",
-    sortable: false,
-    width: 220,
-    // TODO: calvinhlee - investigate what this is
-    // valueGetter: (params: GridValueGetterParams) =>
-    //   `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-  {
-    field: "published_date",
-    headerName: "Publication Date",
-    type: "date",
-    width: 200,
-  },
-  {
-    field: "published_year",
-    headerName: "Publication Year",
-    type: "string",
-    width: 200,
-  },
-];
-
-const rows = [
-  {
-    id: 0,
-    authors: "Patrick Matalla et al.",
-    hubs: "Optics",
-    last_author: "Christian Koos",
-    published_date: "Oct 13, 2023",
-    published_year: 2023,
-    title: "Visual Prompting via Imagie Inpaining",
-  },
-  {
-    id: 1,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-  {
-    id: 2,
-    authors: "Thomas Clarke et al.",
-    hubs: "Medicine",
-    last_author: "Johnathan Whetstine",
-    published_date: "July 8, 2022",
-    published_year: 2022,
-    title: "Histon Lysine Methylation Dynamics Control",
-  },
-  {
-    id: 3,
-    authors: "Thierry Zami et al.",
-    hubs: "Computer Science",
-    last_author: " Bruno Lavigne",
-    published_date: "May 7, 2013",
-    published_year: 2013,
-    title: "Impact of Crosstalk on 800 Gb/s",
-  },
-  {
-    id: 4,
-    authors: "Patrick Matalla et al.",
-    hubs: "Optics",
-    last_author: "Christian Koos",
-    published_date: "Oct 13, 2023",
-    published_year: 2023,
-    title: "Visual Prompting via Imagie Inpaining",
-  },
-  {
-    id: 5,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-  {
-    id: 9,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-  {
-    id: 10,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-  {
-    id: 6,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-  {
-    id: 7,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-  {
-    id: 8,
-    authors: "Ingrid Bretherton et al.",
-    hubs: "Computer Engineering",
-    last_author: "Ada Cheung",
-    published_date: "Jan 11, 2021",
-    published_year: 2021,
-    title: "Hardware Compairson of Feed-Forward Clock Recovery",
-  },
-];
+import { useReferenceTabContext } from "./context/ReferencesTabContext";
+import { idID } from "@mui/material/locale";
 
 export default function ReferencesTable() {
   const [pageSize, setPageSize] = useState(10);
+  const { setIsTabOpen, setReferenceItemData } = useReferenceTabContext();
 
   return (
     <div style={{ height: 400, width: "100%" }}>
@@ -200,6 +73,16 @@ export default function ReferencesTable() {
         onPageSizeChange={(pageSize) => setPageSize(pageSize)}
         rowsPerPageOptions={[5, 10, 25]}
         autoHeight={true}
+        onCellClick={(params, event, _details) => {
+          event.stopPropagation();
+          setReferenceItemData({
+            ...rows.find((item) => item.id === params?.row?.id),
+          });
+          setIsTabOpen(true);
+        }}
+        // onRowClick={(params, event, details) => {
+        //   event.stopPropagation();
+        // }}
       />
     </div>
   );
