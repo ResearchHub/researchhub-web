8f941be1e043ae22eb23f3b20f7741bc8c202121
Kobe Attias <kobeattias@gmail.com>
2023-03-10 22:09:27 -0500

    [Comments] Sidebar sliding animaiton

--- components/Comment/CommentSidebar.tsx
+++ components/Comment/CommentSidebar.tsx
@@ -6,24 +6,36 @@ type Args = {
   isOpen: boolean;
 }
 
+const WIDTH = 450;
+
 const CommentSidebar = ({ isOpen }: Args) => {
 
   return (
     <div className={css(styles.sidebar, isOpen ? styles.sidebarOpen : styles.sidebarClosed)}>
-      <div style={{ overflowY: "scroll", height: "100vh" }}>
+      <div className={css(styles.feedWrapper)}>
         <CommentFeed unifiedDocumentId={5555} />
       </div>
     </div>
   )
 }
 
-const slideoutKeyframe = {
+const slideOpenKeyframe = {
   '0%': {
-    transform: 'translateX(0%)',
+    marginRight: -WIDTH,
   },
 
   '100%': {
-    transform: 'translateX(-100%)',
+    marginRight: 0,
+  },
+}
+
+const slideCloseKeyframe = {
+  '0%': {
+    marginRight: 0,
+  },
+
+  '100%': {
+    marginRight: -WIDTH,
   },
 }
 
@@ -34,18 +46,28 @@ const styles = StyleSheet.create({
     borderBottom: `1px solid ${moduleColors.border}`,
     position: "sticky",
     top: 0,
-    right: 0,
     paddingRight: 32,
     paddingLeft: 32,
-    width: "400px",
+    width: WIDTH,
+    boxSizing: "border-box",
+  },
+  feedWrapper: {
+    overflowY: "scroll",
+    height: "100vh",
   },
   sidebarOpen: {
-
+    animationName: [slideOpenKeyframe],
+    animationDuration: '0.5s',
+    animationDirection: "normal",
+    animationFillMode: "forwards",
+    animationTiming: "ease-in",
   },
   sidebarClosed: {
-    animationName: [slideoutKeyframe],
-    animationDuration: '1s, 100ms',
-    // animationIterationCount: 'forwards',    
+    animationName: [slideCloseKeyframe],
+    animationDuration: '0.5s',
+    animationDirection: "normal",
+    animationFillMode: "forwards",
+    animationTiming: "ease-out",
   }
 });
 
