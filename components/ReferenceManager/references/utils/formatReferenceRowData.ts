c12554db43a382370321f1b6b05cceb2ea916049
Calvin Lee <calvinhlee23@gmail.com>
2023-03-17 12:00:26 -0700

    manuscript mapping added

--- components/ReferenceManager/references/utils/formatReferenceRowData.ts
+++ components/ReferenceManager/references/utils/formatReferenceRowData.ts
@@ -1,28 +1,50 @@
 import { filterNull } from "~/config/utils/nullchecks";
 import { ReferenceItemDataType } from "../context/ReferencesTabContext";
 
+function formatAuthors(
+  authors: { first_name: string; last_name: string }[]
+): string {
+  return authors
+    .map(
+      (artistEl) => `${artistEl.first_name ?? ""} ${artistEl.last_name ?? ""}`
+    )
+    .join(", ");
+}
+
 function referenceFormatSwitchMap(datum: any): ReferenceItemDataType | null {
   const { citation_type, fields, id } = datum ?? {};
+  const { title, date, access_date } = fields;
+  let lastAuthor;
   switch (citation_type) {
     case "ARTWORK":
       const {
-        access_date,
-        title,
         creators: { artist },
-        date,
       } = fields;
-      const lastAuthor = artist[artist.length - 1];
+      lastAuthor = artist[artist.length - 1];
       return {
         // logical ordering - displayed in ReferenceItemTab
         id,
         citation_type,
         title,
-        authors: artist
-          .map(
-            (artistEl) =>
-              `${artistEl.first_name ?? ""} ${artistEl.last_name ?? ""}`
-          )
-          .join(", "),
+        authors: formatAuthors(artist),
+        last_author: `${lastAuthor?.first_name ?? ""} ${
+          lastAuthor?.last_name ?? ""
+        }`,
+        hubs: "",
+        published_date: date ?? access_date,
+        published_year: date.split("-")[2] ?? access_date.split("-")[2] ?? "",
+      };
+    case "MANUSCRIPT":
+      const {
+        creators: { author },
+      } = fields;
+      lastAuthor = author[author.length - 1];
+
+      return {
+        id,
+        citation_type,
+        title,
+        authors: formatAuthors(author),
         last_author: `${lastAuthor?.first_name ?? ""} ${
           lastAuthor?.last_name ?? ""
         }`,
