6192f98790c55d231067f74b0a21fd3a1c6c4204
Kobe Attias <kobeattias@gmail.com>
2023-03-28 13:03:43 -0400

    [Comments] Changing tree update mechanism to be through react context.

--- components/Comment/lib/contexts.ts
+++ components/Comment/lib/contexts.ts
@@ -1,12 +1,20 @@
 import { createContext } from 'react';
 import { filterOpts, sortOpts } from "./options";
 
-type FeedStateContext = {
+type CommentTreeContext = {
   sort: string|null;
   filter: string|null;
+  onCreate: Function;
+  onUpdate: Function;
+  onFetchMore: Function;
 }
 
-export const FeedStateContext = createContext<FeedStateContext>({
+export const CommentTreeContext = createContext<CommentTreeContext>({
   sort: sortOpts[0].value,
   filter: filterOpts[0].value,
+  // These functions are defined in the component the context is used.
+  // they will receive their value in there since their definition depends on state.
+  onCreate: () => null,
+  onUpdate: () => null,
+  onFetchMore: () => null,
 });
