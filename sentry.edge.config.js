2086a116f6ec4cf3ae85983a9b1a15ed6be32cff
Patrick Lu <lightning.lu7@gmail.com>
2023-03-28 15:37:23 -0700

    updating sentry configs

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -1,5 +1,5 @@
 // This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
+// The config you add here will be used whenever middleware or an Edge route handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === "production") {
     dsn:
       SENTRY_DSN ||
       "https://423f7b6ddcea48b9b50f7ba4baa0e750@o206202.ingest.sentry.io/1817918",
+    // Adjust this value in production, or use tracesSampler for greater control
     environment:
       process.env.REACT_APP_ENV === "staging" ? "staging" : "production",
   });
