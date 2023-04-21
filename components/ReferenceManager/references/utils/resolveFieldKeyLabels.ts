a0dd6290a18e77acbea9510316e8fef6c5c06886
Calvin Lee <calvinhlee23@gmail.com>
2023-04-21 05:47:49 -0700

    more updates to sortSchemaFieldKeys

--- components/ReferenceManager/references/utils/resolveFieldKeyLabels.ts
+++ components/ReferenceManager/references/utils/resolveFieldKeyLabels.ts
@@ -20,12 +20,16 @@ export const resolveFieldKeyLabels = (str: string): string => {
 export const sortSchemaFieldKeys = (fieldKeys: string[]): string[] => {
   const keySet = new Set(fieldKeys);
   const subResult: string[] = [];
+  // logical ordering
   keySet.has("title") && subResult.push("title");
+  keySet.has("DOI") && subResult.push("DOI");
   keySet.has("creators") && subResult.push("creators");
   keySet.has("publication_title") && subResult.push("publication_title");
   keySet.delete("creators");
   keySet.delete("title");
   keySet.delete("publication_title");
+  keySet.delete("DOI");
+  keySet.delete("doi");
 
   return [...subResult, ...Array.from(keySet)];
   // return labels
