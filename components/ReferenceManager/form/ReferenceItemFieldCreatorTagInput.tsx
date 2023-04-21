df35afed5d73cb2c2e3f0ee7cede213e1bdfaf11
Calvin Lee <calvinhlee23@gmail.com>
2023-04-21 05:39:33 -0700

    update default value of ReferenceItemFieldCreatorTagInput

--- components/ReferenceManager/form/ReferenceItemFieldCreatorTagInput.tsx
+++ components/ReferenceManager/form/ReferenceItemFieldCreatorTagInput.tsx
@@ -12,7 +12,7 @@ export default function ReferenceItemFieldCreatorTagInput({
   label,
   onChange,
   required = false,
-  value = "",
+  value = [],
 }: Props): ReactElement {
   return (
     <Box
