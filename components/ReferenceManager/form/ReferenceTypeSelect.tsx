731070babdd8419ac38de45600473b1b0cf2859d
Calvin Lee <calvinhlee23@gmail.com>
2023-04-21 06:06:57 -0700

    refType prefiller

---
+++ components/ReferenceManager/form/ReferenceTypeSelect.tsx
@@ -0,0 +1,98 @@
+import { emptyFncWithMsg, isEmpty } from "~/config/utils/nullchecks";
+import { fetchReferenceCitationSchema } from "../references/api/fetchReferenceCitationSchema";
+import { NullableString } from "~/config/types/root_types";
+import { ReactElement, useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import colors from "~/config/themes/colors";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+
+type MenuItemProps = { label: string; value: string };
+type Props = {
+  formID: string;
+  label: string;
+  menuItemProps: MenuItemProps[];
+  onChange: (value: any) => void;
+  placeholder?: string;
+  required?: boolean;
+  value?: any;
+};
+
+export default function ReferenceTypeSelect({
+  formID,
+  label,
+  required = false,
+  placeholder,
+  value = "",
+  onChange,
+  menuItemProps,
+}: Props): ReactElement {
+  const [referenceTypes, setReferenceTypes] = useState<string[]>([]);
+  const [selectedReferenceType, setSelectedReferenceType] =
+    useState<NullableString>(null);
+
+  // useEffect((): void => {
+  //   fetchReferenceCitationSchema({
+  //     citation_type: selectedReferenceType,
+  //     onError: emptyFncWithMsg,
+  //     onSuccess: onChange,
+  // }, []);
+
+  const referenceTypeMenuEls = menuItemProps.map(
+    ({ label, value }: MenuItemProps): ReactElement<typeof MenuItem> => (
+      <MenuItem key={`ref-item-field-${value}-${label}`} value={value}>
+        {label}
+      </MenuItem>
+    )
+  );
+
+  if (!isEmpty(placeholder)) {
+    referenceTypeMenuEls = [
+      <MenuItem key="ref-item-field-placeholder" value="">
+        <em>{placeholder}</em>
+      </MenuItem>,
+      ...referenceTypeMenuEls,
+    ];
+  }
+
+  return (
+    <Box
+      sx={{
+        background: "transparent",
+        height: "78px",
+        marginBottom: "16px",
+        width: "100%",
+      }}
+    >
+      <Typography
+        color="rgba(36, 31, 58, 1)"
+        fontSize="14px"
+        fontWeight={600}
+        lineHeight="22px"
+        letterSpacing={0}
+        mb="4px"
+        sx={{ background: "transparent" }}
+        width="100%"
+      >
+        {label}
+        {required ? <span style={{ color: colors.BLUE() }}>{"*"}</span> : null}
+      </Typography>
+      <Select
+        displayEmpty
+        fullWidth
+        id={formID}
+        inputProps={{ "aria-label": "Without label" }}
+        onChange={(event: SelectChangeEvent): void =>
+          onChange(event.target.value)
+        }
+        sx={{ background: "#fff" }}
+        value={value}
+        MenuProps={{ MenuListProps: { sx: { height: "320px" } } }}
+        size="small"
+      >
+        {referenceTypeMenuEls}
+      </Select>
+    </Box>
+  );
+}
