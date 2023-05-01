2b0b1e07cdd3e7fda5ef98cfa2223db147a0da38
Patrick Lu <lightning.lu7@gmail.com>
2023-05-01 14:04:28 -0700

    remove onboarding push

--- pages/reference-manager/index.tsx
+++ pages/reference-manager/index.tsx
@@ -30,18 +30,18 @@ function Index(props) {
 export async function getServerSideProps(ctx) {
   const cookies = nookies.get(ctx);
   const authToken = cookies[AUTH_TOKEN];
-  const userURL = generateApiUrl("user");
-  const userResponse = await fetch(userURL, api.GET_CONFIG(authToken));
-  const userJson = await userResponse.json();
+  // const userURL = generateApiUrl("user");
+  // const userResponse = await fetch(userURL, api.GET_CONFIG(authToken));
+  // const userJson = await userResponse.json();
 
-  if (!userJson.results[0].reference_manager_onboarding_complete) {
-    return {
-      redirect: {
-        destination: `/reference-manager/onboarding/welcome`,
-        permanent: false,
-      },
-    };
-  }
+  // if (!userJson.results[0].reference_manager_onboarding_complete) {
+  //   return {
+  //     redirect: {
+  //       destination: `/reference-manager/onboarding/welcome`,
+  //       permanent: false,
+  //     },
+  //   };
+  // }
   const url = generateApiUrl(`organization/0/get_user_organizations`);
   const orgResponse = await fetchUserOrgs({ url }, authToken);
   const org = orgResponse[0];
