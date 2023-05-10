dc6d8d695423948292fe4b8bf7d36cb12ab8eedd
Calvin Lee <calvinhlee23@gmail.com>
2023-05-10 13:45:50 -0700

    testing

--- components/ReferenceManager/form/ReferenceItemRhUserLookupInput.tsx
+++ components/ReferenceManager/form/ReferenceItemRhUserLookupInput.tsx
@@ -1,19 +1,61 @@
-import { ReactElement } from "react";
+import { ReactElement, ReactNode, SyntheticEvent, useState } from "react";
 import { SuggestedUser } from "~/components/SearchSuggestion/lib/types";
 import Autocomplete from "@mui/material/Autocomplete";
 import OutlinedInput from "@mui/material/OutlinedInput";
+import TextField from "@mui/material/TextField";
+import { debounce } from "@mui/material";
+import { emptyFncWithMsg, isEmpty } from "~/config/utils/nullchecks";
+import { fetchUserSuggestions } from "~/components/SearchSuggestion/lib/api";
+import { NullableString } from "~/config/types/root_types";
+import SuggestUsers from "~/components/SearchSuggestion/SuggestUsers";
+import { Cancelable } from "@mui/utils/debounce";
+import { event } from "react-ga";
 
 export type InputProps = {
   disabled?: boolean;
   formID: string;
   label: string;
   onInputChange?: (value: any) => void;
+  onUserSelect?: (user: SuggestedUser) => void;
   placeholder?: string;
   required?: boolean;
-  onUserSelect?: (user: SuggestedUser) => void;
   selectedUser?: SuggestedUser;
+  shouldClearOnSelect?: boolean;
 };
 
+const LOADING_SUGGESTION: SuggestedUser = {
+  firstName: "loading",
+  lastName: "loading",
+  id: -1,
+  reputation: 0,
+  authorProfile: {
+    isClaimed: false,
+    url: "",
+    description: "",
+    headline: "",
+  },
+};
+
+const getDebouncedHandleInputChange = ({
+  debounceTime = 500,
+  onLoad,
+  onInputChange,
+  onFetchSucess,
+}) => {
+  let debounceRef: NodeJS.Timeout | null = null;
+  return async (event: SyntheticEvent, queryString: string) => {
+    event.preventDefault();
+    onInputChange(queryString);
+    if (!isEmpty(onLoad) && !isEmpty(queryString)) {
+      onLoad(queryString);
+    }
+    // debounceRef && debounceRef.clear();
+    debounceRef = setTimeout(async () => {
+      debugger;
+      onFetchSucess(await fetchUserSuggestions(queryString));
+    }, debounceTime);
+  };
+};
 export default function ReferenceItemRhUserLookupInput({
   disabled,
   formID,
@@ -22,27 +64,48 @@ export default function ReferenceItemRhUserLookupInput({
   onUserSelect,
   placeholder,
   required,
+  shouldClearOnSelect,
 }: InputProps): ReactElement {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [suggestedUsers, setSuggestedUsers] = useState<SuggestedUser[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // NOTE: calvinhlee - this way of debouncing looks off. Need to look into this
+  const handleInputChange = getDebouncedHandleInputChange({
+    debounceTime: 500,
+    onInputChange: setInputValue,
+    onFetchSucess: setSuggestedUsers,
+    onLoad: (_queryString: string) => {
+      setIsLoading(true);
+    },
+  });
+  console.warn("handleInputChange: ", handleInputChange);
+
+  const handleSelect = (selectedUser: SuggestedUser): void => {
+    onUserSelect && onUserSelect(selectedUser);
+    shouldClearOnSelect;
+  };
+
   return (
     <Autocomplete
-      disablePortal
-      onSelect={onUserSelect}
-      options={top100Films}
+      // onSelect={handleSelect}
+      disableCloseOnSelect
+      inputValue={inputValue}
+      renderOption={(props, option: SuggestedUser, state): ReactNode => {
+        return <div>HI</div>;
+      }}
+      onInputChange={handleInputChange}
+      options={suggestedUsers}
       fullWidth
       size="small"
-      // handleClick={() => {}}
-      renderInput={(params) => {
-        return (
-          <OutlinedInput
-            {...params}
-            disabled={disabled}
-            label={label}
-            onChange={onInputChange}
-            placeholder={placeholder}
-            required={required}
-          />
-        );
-      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          InputProps={{
+            ...params.InputProps,
+            type: "search",
+          }}
+        />
+      )}
     />
   );
 }
