44055731b836ee9cc65f5f7f1801910d1e8aa586
Calvin Lee <calvinhlee23@gmail.com>
2023-05-11 15:08:34 -0700

    alink style override

--- components/ReferenceManager/references/reference_organizer/ReferenceProjectsNavbarEl.tsx
+++ components/ReferenceManager/references/reference_organizer/ReferenceProjectsNavbarEl.tsx
@@ -1,14 +1,15 @@
 import { Box, Typography } from "@mui/material";
-import { ID } from "~/config/types/root_types";
-import { ReactElement, SyntheticEvent } from "react";
-import { useReferenceUploadDrawerContext } from "../reference_uploader/context/ReferenceUploadDrawerContext";
-import ALink from "~/components/ALink";
-import FolderIcon from "@mui/icons-material/Folder";
-import ReferenceProjectNavbarElOption from "./ReferenceProjectNavbarElOptions";
 import {
   DEFAULT_PROJECT_VALUES,
   useReferenceProjectUpsertContext,
 } from "./context/ReferenceProjectsUpsertContext";
+import { ID } from "~/config/types/root_types";
+import { ReactElement, SyntheticEvent } from "react";
+import { StyleSheet } from "aphrodite";
+import { useReferenceUploadDrawerContext } from "../reference_uploader/context/ReferenceUploadDrawerContext";
+import ALink from "~/components/ALink";
+import FolderIcon from "@mui/icons-material/Folder";
+import ReferenceProjectNavbarElOption from "./ReferenceProjectNavbarElOptions";
 
 type Props = {
   orgSlug: string;
@@ -32,70 +33,83 @@ export default function ReferenceProjectsNavbarEl({
   } = useReferenceProjectUpsertContext();
 
   return (
-    <ALink
-      href={`/reference-manager/${orgSlug}/?project=${projectID}&project_name=${projectName}`}
+    <Box
+      sx={{
+        alignItems: "center",
+        cursor: "pointer",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        maxHeight: 50,
+        px: 2.5,
+        margin: "8px",
+        marginLeft: 0,
+        marginRight: 0,
+        boxSizing: "border-box",
+        width: "100%",
+      }}
     >
-      <Box
-        sx={{
-          alignItems: "center",
-          cursor: "pointer",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          maxHeight: 50,
-          px: 2.5,
-          margin: "8px",
-          marginLeft: 0,
-          marginRight: 0,
-        }}
-        onMouseDown={(event: SyntheticEvent): void => {
-          event.preventDefault();
-          setIsUploadDrawerOpen(false);
-          setProjectIDRefUploader(null);
-        }}
+      <ALink
+        href={`/reference-manager/${orgSlug}/?project=${projectID}&project_name=${projectName}`}
+        overrideStyle={styles.linkOverride}
       >
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <FolderIcon fontSize="small" sx={{ color: "#7C7989" }} />
-          <Typography
-            component="div"
-            fontSize={14}
-            letterSpacing={"1.2px"}
-            noWrap
-            variant="h6"
-            ml={"6px"}
-            color={"#241F3A"}
-          >
-            {projectName}
-          </Typography>
-        </div>
-        <ReferenceProjectNavbarElOption
-          onSelectAddNewReference={(event: SyntheticEvent): void => {
+        <Box
+          sx={{ width: "100%", minWidth: "100%" }}
+          onMouseDown={(event: SyntheticEvent): void => {
             event.preventDefault();
-            setProjectIDRefUploader(projectID);
-            setIsUploadDrawerOpen(true);
-          }}
-          onSelectCreateSubProject={(event: SyntheticEvent): void => {
-            event.preventDefault();
-            setProjectIDRefUploader(null);
             setIsUploadDrawerOpen(false);
-            setProjectUpsertPurpose("create_sub_project");
-            setProjectUpsertValue({ ...DEFAULT_PROJECT_VALUES, projectID });
-            setIsProjectUpsertModalOpen(true);
-          }}
-          onSelectEditProject={(event: SyntheticEvent): void => {
-            event.preventDefault();
             setProjectIDRefUploader(null);
-            setIsUploadDrawerOpen(false);
-            setProjectUpsertPurpose("update");
-            setProjectUpsertValue({
-              ...DEFAULT_PROJECT_VALUES,
-              projectID,
-              projectName,
-            });
-            setIsProjectUpsertModalOpen(true);
           }}
-        />
-      </Box>
-    </ALink>
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <FolderIcon fontSize="small" sx={{ color: "#7C7989" }} />
+            <Typography
+              component="div"
+              fontSize={14}
+              letterSpacing={"1.2px"}
+              noWrap
+              variant="h6"
+              ml={"6px"}
+              color={"#241F3A"}
+            >
+              {projectName}
+            </Typography>
+          </div>
+        </Box>
+      </ALink>
+      <ReferenceProjectNavbarElOption
+        onSelectAddNewReference={(event: SyntheticEvent): void => {
+          event.preventDefault();
+          setProjectIDRefUploader(projectID);
+          setIsUploadDrawerOpen(true);
+        }}
+        onSelectCreateSubProject={(event: SyntheticEvent): void => {
+          event.preventDefault();
+          setProjectIDRefUploader(null);
+          setIsUploadDrawerOpen(false);
+          setProjectUpsertPurpose("create_sub_project");
+          setProjectUpsertValue({ ...DEFAULT_PROJECT_VALUES, projectID });
+          setIsProjectUpsertModalOpen(true);
+        }}
+        onSelectEditProject={(event: SyntheticEvent): void => {
+          event.preventDefault();
+          setProjectIDRefUploader(null);
+          setIsUploadDrawerOpen(false);
+          setProjectUpsertPurpose("update");
+          setProjectUpsertValue({
+            ...DEFAULT_PROJECT_VALUES,
+            projectID,
+            projectName,
+          });
+          setIsProjectUpsertModalOpen(true);
+        }}
+      />
+    </Box>
   );
 }
+
+const styles = StyleSheet.create({
+  linkOverride: {
+    width: "100%",
+  },
+});
