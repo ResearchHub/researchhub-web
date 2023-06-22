ff8e03096eb4057e4552f981d89cdd468b55a942
Calvin Lee <calvinhlee23@gmail.com>
2023-06-21 17:30:26 -0700

    adding project structures

--- components/ReferenceManager/menu/DropdownMenu.tsx
+++ components/ReferenceManager/menu/DropdownMenu.tsx
@@ -56,7 +56,7 @@ export default function DropdownMenu({
     <div>
       <Button
         aria-expanded={open ? "true" : undefined}
-        autoCapitalize="false"
+        autoCapitalize=""
         onClick={(event: MouseEvent<HTMLElement>): void => {
           if (disabled) {
             return;
@@ -66,6 +66,7 @@ export default function DropdownMenu({
         size={size}
         sx={{
           background: "rgba(250, 250, 252, 1)",
+          textTransform: " unset !important",
           padding: 0,
         }}
       >
