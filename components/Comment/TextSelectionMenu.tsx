8ab99a89b83d8ba07411de48e3bdc4515503c3db
Kobe Attias <kobeattias@gmail.com>
2023-07-18 15:03:26 -0400

    Horizontal selection menu for small resolution

--- components/Comment/TextSelectionMenu.tsx
+++ components/Comment/TextSelectionMenu.tsx
@@ -6,18 +6,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { StyleSheet, css } from "aphrodite";
 import colors from "~/config/themes/colors";
 
-const TextSelectionMenu = ({ onCommentClick, onLinkClick }) => {
+type Props = {
+  onCommentClick: (e) => void;
+  onLinkClick: (e) => void;
+  isHorizontal?: boolean;
+};
+
+const TextSelectionMenu = ({
+  onCommentClick,
+  onLinkClick,
+  isHorizontal = false,
+}: Props) => {
   return (
-    <div className={css(styles.container)}>
+    <div className={css(styles.container, isHorizontal && styles.horizontal)}>
       <div className={css(styles.option)} onClick={onCommentClick}>
-        <FontAwesomeIcon icon={faCommentDots} />
+        <FontAwesomeIcon
+          fontWeight={600}
+          fontSize={isHorizontal ? 16 : 22}
+          icon={faCommentDots}
+        />
+        {isHorizontal ? ` Comment` : ``}
       </div>
-      <div className={css(styles.divider)} />
+      <div
+        className={css(
+          styles.divider,
+          isHorizontal && styles.dividerHorizontal
+        )}
+      />
       <div className={css(styles.option)} onClick={onLinkClick}>
         <FontAwesomeIcon
+          fontSize={isHorizontal ? 16 : 22}
+          fontWeight={600}
           icon={faLinkSimple}
           style={{ transform: "rotate(-45deg)" }}
         />
+        {isHorizontal ? ` Link` : ``}
       </div>
     </div>
   );
@@ -25,22 +48,45 @@ const TextSelectionMenu = ({ onCommentClick, onLinkClick }) => {
 
 const styles = StyleSheet.create({
   container: {
-    boxShadow: "0px 4px 20px rgba(36, 31, 58, 0.1)",
-    background: "white",
+    boxShadow:
+      "rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px",
+    background: "#F9FBFD",
     borderRadius: "4px",
+    display: "inline-flex",
+    flexDirection: "column",
+    border: `1px solid #E9EAEF`,
+    // height: 36,
+    boxSizing: "border-box",
+    overflow: "hidden",
+    color: colors.NEW_BLUE(1.0),
+  },
+  horizontal: {
+    flexDirection: "row",
   },
   option: {
-    fontSize: 20,
-    padding: "10px 16px",
+    fontSize: 14,
+    columnGap: "5px",
+    padding: "16px 16px",
+    display: "flex",
+    alignItems: "center",
     ":hover": {
-      color: colors.NEW_BLUE(1.0),
       cursor: "pointer",
+      // color: colors.NEW_BLUE(1.0),
+      background: colors.NEW_BLUE(0.1),
+      transition: "0.2s",
     },
   },
   divider: {
-    width: "75%",
     margin: "0 auto",
+    display: "flex",
+    borderLeft: "unset",
     borderBottom: `1px solid #E9EAEF`,
+    width: "100%",
+  },
+  dividerHorizontal: {
+    height: "100%",
+    border: `1px solid rgb(104 104 104)`,
+    borderLeft: `1px solid #E9EAEF`,
   },
 });
 
