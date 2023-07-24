b352f6d69f48a7a8f9e017b59ed4a60b380e04ff
Calvin Lee <calvinhlee23@gmail.com>
2023-07-24 12:20:59 -0700

    testing on staging

--- config/api.js
+++ config/api.js
@@ -8,6 +8,7 @@ export const apiRoot = {
   production: "backend.researchhub.com",
   staging: "staging-backend.researchhub.com",
   dev: "localhost:8000",
+  // dev: "127.0.0.1:8000" /* localhost */,
 };
 
 const prepFilters = (filters) => {
