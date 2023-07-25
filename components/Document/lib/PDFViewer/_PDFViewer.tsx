2b1cadd2a1bad69b82429785f25f537babd726a8
Kobe Attias <kobeattias@gmail.com>
2023-07-24 20:09:35 -0500

    Resize PDF hell

--- components/Document/lib/PDFViewer/_PDFViewer.tsx
+++ components/Document/lib/PDFViewer/_PDFViewer.tsx
@@ -1,11 +1,16 @@
 /*
- * Important Note: Do not use this component directly.
+ * WARNING 1: Do not use this component directly.
  * Use DocumentViewer component instead since it is capable of properly loading it.
  * Failure to do so, will result in a very large bundle size.
+ *
+ * WARNING 2: Refrain from modifying the HTML structure of this component.
+ * Rendering annotations uses xpath and if this component's structure is modified, it may result
+ * in orphaned annotations which we are unable to render.
+ * Adding a mechanism to find and re-attach orphaned annotations is possible but not trivial.
  */
 
 import { Document, Outline, Page, pdfjs } from "react-pdf";
-import { useCallback, useRef, useState } from "react";
+import { use, useCallback, useEffect, useRef, useState } from "react";
 import { StyleSheet, css } from "aphrodite";
 
 // FIXME: Replace with local worker.
@@ -15,9 +20,10 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 interface Props {
   pdfUrl?: string;
   viewerWidth?: number;
+  contentRef: any;
   showWhenLoading?: any;
   searchText?: string;
-  onLoadSuccess: () => void;
+  onLoadSuccess: ({ numPages, contentRef }) => void;
   onLoadError: () => void;
   onPageRender: (pageNum) => void;
 }
@@ -30,19 +36,28 @@ const PDFViewer = ({
   onLoadError,
   onPageRender,
   viewerWidth = 900,
+  contentRef,
 }: Props) => {
   const [numPages, setNumPages] = useState<null | number>(null);
   const [pagesRendered, setPagesRendered] = useState<number>(1); // Start by rendering one page
   const observer = useRef(null); // Observe the last rendered page to see if it's in view, if not, load more pages
+  const [isDocumentLoaded, setIsDocumentLoaded] = useState(false);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
-    onLoadSuccess();
+    setIsDocumentLoaded(true);
   }
 
   function onDocumentLoadError() {
     onLoadError();
   }
+
+  useEffect(() => {
+    if (isDocumentLoaded && numPages !== null && contentRef?.current) {
+      // All dependencies are ready.
+      onLoadSuccess({ numPages, contentRef });
+    }
+  }, [contentRef, numPages, isDocumentLoaded]);
 
   const lastPageRef = useCallback(
     (node: HTMLDivElement | null) => {
@@ -78,21 +93,24 @@ const PDFViewer = ({
         onLoadSuccess={onDocumentLoadSuccess}
         onLoadError={onDocumentLoadError}
         loading={showWhenLoading || "Loading..."}
+        className
       >
-        {Array.from(new Array(pagesRendered), (el, index) => (
-          <div
-            ref={index + 1 === pagesRendered ? lastPageRef : null}
-            key={`page_${index + 1}`}
-          >
-            <Page
-              pageNumber={index + 1}
-              width={viewerWidth}
-              // customTextRenderer={textRenderer}
-              onRenderSuccess={pageRenderSuccess}
-              loading={showWhenLoading || "Loading..."}
-            />
-          </div>
-        ))}
+        <div className="content" ref={contentRef}>
+          {Array.from(new Array(pagesRendered), (el, index) => (
+            <div
+              ref={index + 1 === pagesRendered ? lastPageRef : null}
+              key={`page_${index + 1}`}
+            >
+              <Page
+                pageNumber={index + 1}
+                width={viewerWidth}
+                // customTextRenderer={textRenderer}
+                onRenderSuccess={pageRenderSuccess}
+                loading={showWhenLoading || "Loading..."}
+              />
+            </div>
+          ))}
+        </div>
       </Document>
     </div>
   );
