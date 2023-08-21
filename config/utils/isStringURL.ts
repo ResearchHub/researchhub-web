e04f61b866ce374163e338907b284a8c64c51fe1
Leo <leo.sun@berkeley.edu>
2023-08-21 16:30:30 -0700

    using non url checker

--- config/utils/isStringURL.ts
+++ config/utils/isStringURL.ts
@@ -1,12 +1,25 @@
 import { isEmpty } from "./nullchecks";
 import { isString } from "./string";
 
+// Deprecated Regex based URL check
+// export const isStringURL = (str: string | null): boolean => {
+//   if (!isString(str) || isEmpty(str)) {
+//     return false;
+//   }
+
+//   return new RegExp(
+//     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+//   ).test(String(str).toLowerCase());
+// };
+
 export const isStringURL = (str: string | null): boolean => {
   if (!isString(str) || isEmpty(str)) {
     return false;
   }
-
-  return new RegExp(
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-  ).test(String(str).toLowerCase());
-};
+  try { 
+    return Boolean( new URL(str)); 
+  }
+  catch(e) { 
+    return false; 
+  }
+}
