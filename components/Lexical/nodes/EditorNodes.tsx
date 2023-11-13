b5f440d92daa4f5f0e97db22a26da9cab7a9d0a0
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-11-13 13:44:34 +0400

    Create EditorNodes.tsx

---
+++ components/Lexical/nodes/EditorNodes.tsx
@@ -0,0 +1,31 @@
+import type { Klass, LexicalNode } from "lexical";
+
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+import { HashtagNode } from "@lexical/hashtag";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { MarkNode } from "@lexical/mark";
+import { OverflowNode } from "@lexical/overflow";
+import { HorizontalRuleNode } from "@lexical/react/LexicalHorizontalRuleNode";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
+
+const EditorNodes: Array<Klass<LexicalNode>> = [
+  HeadingNode,
+  ListNode,
+  ListItemNode,
+  QuoteNode,
+  CodeNode,
+  TableNode,
+  TableCellNode,
+  TableRowNode,
+  CodeHighlightNode,
+  MarkNode,
+  HashtagNode,
+  AutoLinkNode,
+  OverflowNode,
+  HorizontalRuleNode,
+  LinkNode,
+];
+
+export default EditorNodes;
