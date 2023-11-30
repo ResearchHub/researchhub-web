52a65788a0174760ae7ce09781f52f9a3ee1e736
Eimen Hamedat <64800243+eimenhmdt@users.noreply.github.com>
2023-11-30 22:02:14 +0400

    create table plugin

---
+++ components/Lexical/nodes/TableCellNodes.ts
@@ -0,0 +1,25 @@
+import type { Klass, LexicalNode } from "lexical";
+
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+import { HashtagNode } from "@lexical/hashtag";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+
+
+import { ImageNode } from "./ImageNode";
+
+const TableCellNodes: Array<Klass<LexicalNode>> = [
+  HeadingNode,
+  ListNode,
+  ListItemNode,
+  QuoteNode,
+  CodeNode,
+  HashtagNode,
+  CodeHighlightNode,
+  AutoLinkNode,
+  LinkNode,
+  ImageNode,
+];
+
+export default TableCellNodes;
