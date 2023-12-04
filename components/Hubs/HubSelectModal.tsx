70043d00685dd07ca8b7ad593ccb5c49b43f650b
Kobe Attias <kobe@ip-192-168-68-61.us-west-2.compute.internal>
2023-12-03 22:31:08 -0500

    formatting

--- components/Hubs/HubSelectModal.tsx
+++ components/Hubs/HubSelectModal.tsx
@@ -7,80 +7,81 @@ import HubSelect from "../Hubs/HubSelect";
 import { getHubs } from "~/components/Hubs/api/fetchHubs";
 
 const HubSelectModal = ({
-  isModalOpen = true,
-  handleModalClose,
-  handleSelect,
+    isModalOpen = true,
+    handleModalClose,
+    handleSelect,
 }) => {
-  const [hubs, setHubs] = useState<Array<Hub>>([]);
+    const [hubs, setHubs] = useState<Array<Hub>>([]);
+    const [count, setCount] = useState<number>(0);
 
-  useEffect(() => {
-    (async () => {
-      // @ts-ignore
-      const { hubs, count } = await getHubs({});
-      setHubs(hubs);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            // @ts-ignore
+            const { hubs, count } = await getHubs({});
+            setHubs(hubs);
+        })();
+    }, []);
 
-  return (
-    <BaseModal
-      offset={"0px"}
-      isOpen={isModalOpen}
-      hideClose={false}
-      title={"Filter by Hub"}
-      closeModal={handleModalClose}
-      zIndex={12}
-      modalStyle={styles.modalStyle}
-      modalContentStyle={styles.modalContentStyle}
-    >
-      <div className={css(styles.formWrapper)}>
-        <HubSelect
-          count={1}
-          hubs={hubs}
-          withPagination={false}
-          maxCardsPerRow={2}
-          handleClick={(hub) => {
-            handleSelect(hub);
-          }}
-        />
-      </div>
-    </BaseModal>
-  );
+    return (
+        <BaseModal
+            offset={"0px"}
+            isOpen={isModalOpen}
+            hideClose={false}
+            title={"Filter by Hub"}
+            closeModal={handleModalClose}
+            zIndex={12}
+            modalStyle={styles.modalStyle}
+            modalContentStyle={styles.modalContentStyle}
+        >
+            <div className={css(styles.formWrapper)}>
+                <HubSelect
+                    count={count}
+                    hubs={hubs}
+                    withPagination={false}
+                    maxCardsPerRow={2}
+                    handleClick={(hub) => {
+                        handleSelect(hub);
+                    }}
+                />
+            </div>
+        </BaseModal>
+    );
 };
 
 const styles = StyleSheet.create({
-  formWrapper: {
-    width: 540,
-    paddingLeft: 25,
-    paddingRight: 25,
-    marginTop: 25,
-    boxSizing: "border-box",
-    height: "100%",
-    [`@media only screen and (max-width: ${breakpoints.small.str})`]: {
-      width: "100%",
+    formWrapper: {
+        width: 540,
+        paddingLeft: 25,
+        paddingRight: 25,
+        marginTop: 25,
+        boxSizing: "border-box",
+        height: "100%",
+        [`@media only screen and (max-width: ${breakpoints.small.str})`]: {
+            width: "100%",
+        },
+        [`@media only screen and (max-width: ${breakpoints.xxsmall.str})`]: {
+            width: "90%",
+        },
     },
-    [`@media only screen and (max-width: ${breakpoints.xxsmall.str})`]: {
-      width: "90%",
+    modalStyle: {
+        width: "540px",
+        maxHeight: 600,
     },
-  },
-  modalStyle: {
-    width: "540px",
-    maxHeight: 600,
-  },
-  modalTitleStyleOverride: {},
-  modalContentStyle: {
-    position: "relative",
-    minHeight: 560,
-    overflowX: "hidden",
-    padding: "50px 25px ",
-    [`@media only screen and (max-width: ${breakpoints.xxsmall.str})`]: {
-      height: "100%",
+    modalTitleStyleOverride: {},
+    modalContentStyle: {
+        position: "relative",
+        minHeight: 560,
+        overflowX: "hidden",
+        padding: "50px 25px ",
+        [`@media only screen and (max-width: ${breakpoints.xxsmall.str})`]: {
+            height: "100%",
+        },
     },
-  },
-  prevActionWrapper: {
-    position: "absolute",
-    top: 12,
-    left: 10,
-  },
+    prevActionWrapper: {
+        position: "absolute",
+        top: 12,
+        left: 10,
+    },
 });
 
 export default HubSelectModal;
