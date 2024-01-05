5cc780ad57fa52962d03f874339428365734be19
manveer <m13basra@gmail.com>
2024-01-05 08:59:06 -0800

    Get hyperlinks and anchors working with PDFs (#1647)
    * Get hyperlinks and anchors working with PDFs
    
    * Update PDFViewer to preload all pages to simplify anchor-scrolling logic

--- components/Document/lib/PDFViewer/_PDFViewer.tsx
+++ components/Document/lib/PDFViewer/_PDFViewer.tsx
@@ -10,6 +10,7 @@
 //  */
 
 import React, {
+  ReactElement,
   useCallback,
   useEffect,
   useLayoutEffect,
@@ -36,7 +37,7 @@ interface Props {
   pdfUrl?: string;
   viewerWidth?: number;
   contentRef: any;
-  numPagesToPreload?: number;
+  numPagesToPreload?: number | "all";
   showWhenLoading?: any;
   scale: number;
   onReady?: ({ numPages }) => void;
@@ -48,13 +49,13 @@ const PDFViewer = ({
   pdfUrl,
   showWhenLoading,
   onReady,
-  numPagesToPreload = config.numPdfPagesToPreload,
+  numPagesToPreload = config.numPdfPagesToPreload as number | "all",
   onLoadError,
   onPageRender,
   viewerWidth = 860,
   contentRef,
   scale,
-}: Props) => {
+}: Props): ReactElement => {
   const viewerWidthRef = useRef<number>(viewerWidth);
   const scaleRef = useRef<number>(scale);
   const containerRef = useRef<HTMLDivElement>(null);
@@ -70,8 +71,81 @@ const PDFViewer = ({
   const [renderedPages, setRenderedPages] = useState<{
     [pageNumber: number]: Page;
   }>({});
+  // We need to store renderedPages in a ref so that we can access the latest value in `scrollToDestination`.
+  // Otherwise, the `scrollToDestination` used in the onClick handlers of internal links will point to a stale `renderedPages` value.
+  const renderedPagesRef = useRef<{ [pageNumber: number]: Page }>({});
+  useEffect(() => {
+    renderedPagesRef.current = renderedPages;
+  }, [renderedPages]);
   const observer = useRef<HTMLDivElement>(null);
   const eventBus = new EventBus();
+
+  // Store destinations for internal links (e.g. anchors)
+  const internalLinkDestinations = useRef({});
+
+  const scrollToDestination = useCallback(
+    async (destination) => {
+      if (destination === null || destination === undefined) return;
+
+      if (typeof destination === "number") {
+        // Simple case: destination is a page number
+        const targetPageNumber = destination;
+        if (targetPageNumber >= 1 && targetPageNumber <= numPages) {
+          // Logic to scroll to the top of the target page
+          const pageToScroll = (renderedPagesRef.current || {})[
+            targetPageNumber
+          ]?.pageContainer;
+          if (pageToScroll) {
+            pageToScroll.scrollIntoView({ behavior: "smooth" });
+          }
+        }
+      } else if (Array.isArray(destination)) {
+        // desitation is an array of page/coordinate details.
+        // e.g. [ { num: 1, gen: 0 }, { name: 'XYZ' }, 0, 841.89, null ]
+        const objectNumber = destination[0].num;
+        const generationNumber = destination[0].gen;
+        try {
+          const pageNumber =
+            (await pdfDocument.getPageIndex({
+              num: objectNumber,
+              gen: generationNumber,
+            })) + 1; // getPageIndex is zero-based
+          if (pageNumber >= 1 && pageNumber <= numPages) {
+            const pageContainer = (renderedPagesRef.current || {})[pageNumber]
+              ?.pageContainer;
+            if (pageContainer) {
+              pageContainer.scrollIntoView({ behavior: "smooth" });
+            }
+          }
+        } catch (error) {
+          console.log("error navigating to destination", error);
+        }
+      } else {
+        // it's a named destination that we need to look up in the document
+        scrollToNamedDestination(destination);
+      }
+    },
+    [numPages, renderedPagesRef]
+  );
+
+  const scrollToNamedDestination = useCallback(
+    async (namedDestination) => {
+      try {
+        const resolvedDestination = await pdfDocument.getDestination(
+          namedDestination
+        );
+        if (resolvedDestination) {
+          // resolvedDestination is an array of page/coordinate details.
+          // e.g. [ { num: 1, gen: 0 }, { name: 'XYZ' }, 0, 841.89, null ]
+          // So we can handle it in scrollToDestination
+          scrollToDestination(resolvedDestination);
+        }
+      } catch (error) {
+        console.log("error navigating to named destination", error);
+      }
+    },
+    [pdfDocument, scrollToDestination]
+  );
 
   const loadPage = useCallback(
     async (pageNumber) => {
@@ -102,13 +176,49 @@ const PDFViewer = ({
       pdfPageView.setPdfPage(page);
       await pdfPageView.draw();
 
+      // Setup links to internal destinations (e.g. link to a specific citation or figure/table)
+      const annotations = await page.getAnnotations();
+      const internalLinks = annotations.filter(
+        (ann) => ann.subtype === "Link" && ann.url === undefined
+      );
+      internalLinks.forEach((link, index) => {
+        const destination = link.dest; // 'dest' is the PDF.js property for internal destinations
+        internalLinkDestinations.current[
+          `internal-link-${pageNumber}-${index}`
+        ] = destination;
+      });
+
       const pageDiv = pageContainer.querySelector(".page") as HTMLDivElement;
       const annotationsDiv = pageContainer.querySelector(
         ".annotationLayer"
       ) as HTMLDivElement;
 
       if (pageDiv) pageDiv.style.margin = "0 auto";
-      if (annotationsDiv) annotationsDiv.style.display = "none";
+      if (annotationsDiv) {
+        // if we wanted to hide annotations, we'd set display to "none"
+        annotationsDiv.style.display = "block";
+        // we unset left, since the default is 0 but that results in annotations
+        // being rendered further to the left than the page content.
+        annotationsDiv.style.left = "unset";
+
+        // Setup internal and external links
+        const links = annotationsDiv.getElementsByTagName("a");
+        for (let index = 0; index < links.length; index++) {
+          const link = links[index];
+          if (link.href === "#" || link.href.endsWith("#")) {
+            // it's an internal link, so we want to setup custom scrolling to whatever anchor it's pointing to
+            const linkKey = `internal-link-${pageNumber}-${index}`;
+            link.addEventListener("click", (event) => {
+              event.preventDefault();
+              const destination = internalLinkDestinations.current[linkKey];
+              scrollToDestination(destination);
+            });
+          } else {
+            // it's an external link, so we want to open it in a new tab
+            link.target = "_blank";
+          }
+        }
+      }
 
       const textLayerDiv = pageContainer.querySelector(
         ".textLayer"
@@ -210,7 +320,11 @@ const PDFViewer = ({
         };
       });
 
-      if (nextPage <= numPagesToPreload) {
+      if (numPagesToPreload === "all") {
+        if (nextPage < numPages) {
+          setNextPage((prevNextPage) => prevNextPage + 1);
+        }
+      } else if (nextPage <= numPagesToPreload) {
         setNextPage((prevNextPage) => prevNextPage + 1);
       }
 
