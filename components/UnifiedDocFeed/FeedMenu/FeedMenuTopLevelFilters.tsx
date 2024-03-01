5f44a5d860bf55c33883347acc7b468be66e9758
Kobe Attias <kobeattias@gmail.com>
2024-03-01 10:19:21 -0500

    empty commit

--- components/UnifiedDocFeed/FeedMenu/FeedMenuTopLevelFilters.tsx
+++ components/UnifiedDocFeed/FeedMenu/FeedMenuTopLevelFilters.tsx
@@ -100,6 +100,7 @@ const FeedMenuTopLevelFilters = ({
           <span className={css(styles.filterLabel)}>
             {isMyHubs ? <>For You</> : <>{f.label}</>}
           </span>
+          {/*  */}
           {/* {isMyHubs && isSubscribedToHubs && !renderAsDropdown && (
             <span
               className={css(styles.myHubsDown)}
