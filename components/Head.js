2315177789daf67ccdb2c444f6931f8fc24a3394
Kobe Attias <kobeattias@gmail.com>
2024-03-26 10:27:15 -0400

    [Search] Disabling auto-zoom on input fields

--- components/Head.js
+++ components/Head.js
@@ -14,6 +14,10 @@ const HeadComponent = (props) => {
 
   return (
     <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+      />
       <title>{title}</title>
       {props.graph && (
         <script
