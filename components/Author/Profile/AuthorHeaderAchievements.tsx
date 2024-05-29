0266188a084e36e360840a841e859a8b29196373
Kobe Attias <kobeattias@gmail.com>
2024-05-29 11:38:56 -0400

    [Profile] Tooltips for achivement; Linking coauthors

--- components/Author/Profile/AuthorHeaderAchievements.tsx
+++ components/Author/Profile/AuthorHeaderAchievements.tsx
@@ -2,17 +2,51 @@ import colors from "~/config/themes/colors";
 import { FullAuthorProfile } from "../lib/types";
 import { getAchievmentDetails } from "../lib/utils";
 import { css, StyleSheet } from "aphrodite";
+import { Tooltip } from "@mui/material";
+import { faCircleCheck } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const AuthorHeaderAchievements = ({ profile }: { profile: FullAuthorProfile }) => {
+
+  const getTooltipContent = (achievement: string) => {
+    if (achievement === "OPEN_ACCESS") {
+      return (
+        <div className={css(styles.tooltip)}>
+          <div className={css(styles.tooltipLineItem)}>
+            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
+            Published at least 2 papers
+          </div>
+          <div className={css(styles.tooltipLineItem)}>
+            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
+            51% or papers or more are open access
+          </div>          
+        </div>
+      )
+    }
+    else if (achievement === "CITED_AUTHOR") {
+      return (
+        <div className={css(styles.tooltip)}>
+          <div className={css(styles.tooltipLineItem)}>
+            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
+            Author on paper that received at least 1 citation
+          </div>
+        </div>
+      )
+    }    
+  }
+
   return (
     <div className={css(styles.rootWrapper)}>
       {profile.achievements.map((achievement) => {
         const achivementDetails = getAchievmentDetails({ achievement, profile })
         return (
-          <div key={achievement} className={css(styles.achievement)}>
-            <div>{achivementDetails.icon}</div>
-            <div>{achivementDetails.title}</div>
-          </div>
+          <Tooltip title={getTooltipContent(achievement)}>
+            <div key={achievement} className={css(styles.achievement)} data-tip="reputation-tool-tip">
+              <div>{achivementDetails.icon}</div>
+              <div>{achivementDetails.title}</div>
+            </div>
+          </Tooltip>
         )
       })}
       <div className={css(styles.showMore)}>
@@ -28,6 +62,21 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     alignContent: "center",
+  },
+  tooltip: {
+    padding: 6,
+    fontSize: 14,
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "center",    
+    gap: 5,
+  },
+  tooltipLineItem: {
+    display: "flex",
+    alignItems: "center",
+    gap: 8,
+    fontSize: 14,
+    fontWeight: 400,
   },
   achievement: {
     display: "flex",
--- components/Author/Profile/CoAuthors.tsx
+++ components/Author/Profile/CoAuthors.tsx
@@ -2,6 +2,8 @@ import { AuthorProfile } from "~/config/types/root_types";
 import Avatar from "@mui/material/Avatar";
 import { isEmpty } from "~/config/utils/nullchecks";
 import { css, StyleSheet } from "aphrodite";
+import colors from "~/config/themes/colors";
+import Link from "next/link";
 
 const CoAuthors = ({ coauthors }: { coauthors: AuthorProfile[] }) => {
   return (
@@ -9,12 +11,12 @@ const CoAuthors = ({ coauthors }: { coauthors: AuthorProfile[] }) => {
       <div className={css(styles.sectionHeader)}>Coauthors</div>
       <div>
         {coauthors.map((coauthor) => (
-          <div key={coauthor.id} className={css(styles.author)}>
+          <Link href={`/author/${coauthor.id}`} className={css(styles.author)} key={coauthor.id}>
             <Avatar src={coauthor.profileImage} sx={{ width: 25, height: 25, fontSize: 13, }}>
               {isEmpty(coauthor.profileImage) && ((coauthor?.firstName?.[0] ?? "") + (coauthor.lastName?.[0] ?? ""))}
             </Avatar>
             <div>{coauthor.firstName} {coauthor.lastName}</div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
@@ -38,9 +40,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     columnGap: "10px",
     marginBottom: 10,
+    textDecoration: "none",
+    color: colors.BLACK(0.9),
     ":last-child": {
       marginBottom: 0,
-    }    
+    },
+    ":hover": {
+      color: colors.NEW_BLUE(),
+      cursor: "pointer",
+    }
   }
 })
 
