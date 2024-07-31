fe29e73b4a20e3b93712368bec8ac0ee7463136c
Kobe Attias <kobeattias@gmail.com>
2024-07-30 23:05:08 -0400

    [Verify identity] Updating modal to account for two groups of people

--- components/Verification/VerifyIdentityModal.tsx
+++ components/Verification/VerifyIdentityModal.tsx
@@ -16,6 +16,7 @@ import Button from "../Form/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChartLine,
+  faCheck,
   faUser,
   faWarning,
 } from "@fortawesome/pro-solid-svg-icons";
@@ -31,6 +32,8 @@ import {
   faArrowLeft,
   faInfo,
   faInfoCircle,
+  faUserGroup,
+  faUserGraduate,
 } from "@fortawesome/pro-light-svg-icons";
 import { ROUTES as WS_ROUTES } from "~/config/ws";
 import useCurrentUser from "~/config/hooks/useCurrentUser";
@@ -132,11 +135,11 @@ const VerifyIdentityModal = ({ wsResponse, children }: Props) => {
           setIsOpen(false);
         }}
         zIndex={1000000}
-        modalContentStyle={styles.modalStyle}
+        modalContentStyle={step === "INTRO" ? styles.introModalStyle : styles.modalStyle}
         titleStyle={styles.modalTitle}
       >
         <>
-          {step !== "INTRO" && (
+          {!["INTRO", "IDENTITY", "IDENTITY_VERIFIED_SUCCESSFULLY"].includes(step) && (
             <div className={css(styles.breadcrumbsWrapper)}>
               <VerifyIdentityBreadcrumbs
                 selected={
@@ -152,64 +155,65 @@ const VerifyIdentityModal = ({ wsResponse, children }: Props) => {
                 <div className={css(styles.badgeWrapper)}>
                   <VerifiedBadge height={50} width={50} />
                 </div>
-                <div className={css(styles.title)}>
-                  Verify identity to earn RSC rewards on your publications
+                <div className={css(styles.title, styles.introTitle)}>
+                  Verify identity to unlock new features
                 </div>
-                <div className={css(styles.description)}>
-                  First authors of open access papers are eligible for rewards.
-                  Earn rewards whenever your paper gets cited or upvoted.
-                  <p style={{ marginTop: 10 }}>
-                    <ALink theme={"solidPrimary"} href="/">
-                      Learn more about our reward algorithm
-                    </ALink>
+                <div className={css(styles.description, styles.introDescription)}>
+                  <p>
+                    (Takes 1-3 minutes)
+
                   </p>
                 </div>
 
-                <div className={css(styles.steps)}>
-                  <div className={css(styles.stepExplanation)}>
-                    <div className={css(styles.stepIcon)}>
-                      <Image
-                        alt="Scan"
-                        width={36}
-                        height={36}
-                        src={"/static/icons/scan.svg"}
-                      />
+                <div style={{ display: "flex", justifyContent: "space-between", gap: 15, }}>
+                  <div style={{ width: "50%"}}>
+                    <div className={css(styles.sectionTitle)}>
+                      <FontAwesomeIcon icon={faUserGroup} style={{ color: "white", }} fontSize={30} />
+                      All users
                     </div>
-                    Step 1: Verify identity
-                    <span className={css(styles.link)}>
-                      Why is this needed <FontAwesomeIcon icon={faInfoCircle} />
-                    </span>
+                    <ul className={css(styles.list)}>
+                      <li className={css(styles.listItem)}>
+                        <div className={css(styles.icon)}>
+                          <FontAwesomeIcon icon={faCheck} style={{ color: "white"}} fontSize={18} />
+                        </div>
+                        Verified badge
+                      </li>
+                      <li className={css(styles.listItem)}>
+                        <div className={css(styles.icon)}>
+                          <FontAwesomeIcon icon={faCheck} style={{ color: "white"}} fontSize={18} />
+                        </div>
+                        Faster withdrawl limits
+                      </li>
+                    </ul>
                   </div>
-                  <div className={css(styles.stepExplanation)}>
-                    <div className={css(styles.stepIcon)}>
-                      <PaperIcon
-                        width={30}
-                        height={30}
-                        color={"#AAA8B4"}
-                        onClick={undefined}
-                      />
+                  <div style={{ width: "50%"}}>
+                    <div className={css(styles.sectionTitle)}>
+                      <FontAwesomeIcon icon={faUserGraduate} style={{ color: "white", }} fontSize={30} />
+                      Published authors
                     </div>
-                    Step 2: Import publication history
-                  </div>
+                    <ul className={css(styles.list)}>
+                      <li className={css(styles.listItem)}>
+                        <div className={css(styles.icon)}>
+                          <FontAwesomeIcon icon={faCheck} style={{ color: "white"}} fontSize={18} />
+                        </div>                        
+                        Claim RSC rewards on papers
+                      </li>
+                      <li className={css(styles.listItem)}>
+                        <div className={css(styles.icon)}>
+                          <FontAwesomeIcon icon={faCheck} style={{ color: "white"}} fontSize={18} />
+                        </div>                        
+                        Hub-specific reputation score will be calculated for you
+                      </li>
+                    </ul>
+                  </div>                  
 
-                  <div className={css(styles.stepExplanation)}>
-                    <div className={css(styles.stepIcon)}>
-                      <ResearchCoinIcon
-                        version={4}
-                        width={28}
-                        height={28}
-                        color={"#AAA8B4"}
-                      />
-                    </div>
-                    Step 3: View rewards
-                  </div>
                 </div>
+                
                 <div className={css(styles.startButtonWrapper)}>
                   <Button
                     fullWidth
+                    variant="outlined"
                     onClick={() => setStep("IDENTITY")}
-                    theme="solidPrimary"
-                    style={{ width: 200, margin: "20px auto" }}
                   >
                     Start
                   </Button>
@@ -382,12 +386,40 @@ const VerifyIdentityModal = ({ wsResponse, children }: Props) => {
 };
 
 const styles = StyleSheet.create({
+  sectionTitle: {
+    fontWeight: 500,
+    fontSize: 18,
+    marginBottom: 15,
+    color: "white",
+    display: "flex",
+    gap: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingBottom:10,
+  },
+  list: {
+    paddingLeft: 0,
+    fontSize: 16,
+    lineHeight: 1.5,
+    listStyleType: "none",
+  },
+  icon: {
+  },
+  listItem: {
+    color: "white",
+    display: "flex", 
+    alignItems: "flex-start",
+    gap: 8,
+    justifyContent: "flex-start",
+    marginBottom: 5,
+  },
   breadcrumbsWrapper: {
     marginTop: 20,
   },
   startButtonWrapper: {
     width: "100%",
     marginTop: 25,
+    cursor: "pointer",
   },
   link: {
     color: colors.NEW_BLUE(),
@@ -422,12 +454,18 @@ const styles = StyleSheet.create({
     fontWeight: 500,
     textAlign: "center",
   },
+  introTitle: {
+    color: "white",
+  },
   description: {
     color: colors.MEDIUM_GREY2(),
     fontSize: 18,
     width: "100%",
     textAlign: "center",
     margin: "10px auto 40px auto",
+  },
+  introDescription: {
+    color: "white",
   },
   lineItem: {
     display: "flex",
@@ -440,7 +478,8 @@ const styles = StyleSheet.create({
   },
   body: {
     width: "100%",
-    minHeight: 650,
+    // minHeight: 650,
+    maxWidth: 550,
   },
   formWrapper: {
     marginTop: 40,
@@ -456,6 +495,13 @@ const styles = StyleSheet.create({
   modalStyle: {
     padding: "20px 20px",
     width: 550,
+  },
+
+  introModalStyle: {
+    backgroundImage: "url('/static/background/small-banner-background.png')",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",    
+    padding: "10px 20px 20px 20px",
   },
   modalContentStyle: {
     padding: "10px 20px",
