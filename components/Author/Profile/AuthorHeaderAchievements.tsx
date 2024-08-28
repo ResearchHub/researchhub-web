e8a2ddcc8631188a2361db00d87b39f29764b4bc
Kobe Attias <kobeattias@gmail.com>
2024-08-28 12:36:22 -0400

    Achievements with tiers

--- components/Author/Profile/AuthorHeaderAchievements.tsx
+++ components/Author/Profile/AuthorHeaderAchievements.tsx
@@ -1,69 +1,60 @@
 import colors from "~/config/themes/colors";
-import { Achievement, AuthorSummaryStats, FullAuthorProfile } from "../lib/types";
+import { Achievement, AuthorSummaryStats, FullAuthorProfile, TIER_COLORS, TIER_INDICES } from "../lib/types";
 import { getAchievmentDetails } from "../lib/utils";
 import { css, StyleSheet } from "aphrodite";
 import { Tooltip } from "@mui/material";
 import { faCircleCheck, faTrophy, faTrophyStar } from "@fortawesome/pro-light-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactElement } from "react";
 
 
 const AuthorHeaderAchievements = ({ summaryStats, achievements }: { summaryStats: AuthorSummaryStats, achievements: Array<Achievement> }) => {
+  const getTooltipContent = (achievement: Achievement, achivementDetails: { icon: ReactElement, title: string } ) => {
+  const filledColor = TIER_COLORS[achievement.currentMilestoneIndex + 1]
+  const isTopTier = achievement.currentMilestoneIndex === achievement.milestones.length - 1
+  
+  let value = achievement.value;
+  let displayValue = value.toLocaleString();
+  let nextTierValue = achievement.milestones[achievement.currentMilestoneIndex + 1] || 0
+  let nextTierDisplayValue = nextTierValue.toLocaleString();
 
-  const getTooltipContent = (achievement: string) => {
-    if (achievement === "OPEN_ACCESS") {
-      return (
-        <div className={css(styles.tooltip)}>
-          <div className={css(styles.tooltipLineItem)}>
-            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
-            Published at least 2 papers
+  if (achievement.type === "OPEN_ACCESS") {
+    value = Math.round(achievement.value * 100);
+    nextTierValue = nextTierValue * 100;
+    displayValue = value + "%";
+    nextTierDisplayValue = nextTierValue + "%";
+  }
+
+  return (
+    <div className={css(styles.tooltip)}>
+      <div style={{ display: "flex", alignItems: "center", columnGap: 10, }}>
+        {achivementDetails.icon} {achivementDetails.title} ({TIER_INDICES[achievement.currentMilestoneIndex]})
+      </div>
+      <div style={{ fontSize: 13, fontWeight: 400, marginBottom: 10 }}>
+        {achievement.type === "OPEN_SCIENCE_SUPPORTER" ? `Funded open science using ${displayValue} RSC.` : achievement.type === "EXPERT_PEER_REVIEWER" ? `Peer reviewed ${displayValue} publications.`  : achievement.type === "HIGHLY_UPVOTED" ? ` Received ${displayValue} upvotes.` : achievement.type === "CITED_AUTHOR" ? `Author's publications Cited ${displayValue} times.` : achievement.type === "OPEN_ACCESS" ? `Published ${displayValue} of works as open access.` : ""}
+      </div>
+      {!isTopTier && (
+        <>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            Next tier: 
+
+            <div style={{ fontSize: 12 }}>
+              {displayValue} / {nextTierDisplayValue}
+              {achievement.type === "OPEN_SCIENCE_SUPPORTER" ? " RSC" : achievement.type === "EXPERT_PEER_REVIEWER" ? " reviews" : achievement.type === "HIGHLY_UPVOTED" ? " upvotes" : achievement.type === "CITED_AUTHOR" ? " citations" : ""}
+            </div>
+
           </div>
-          <div className={css(styles.tooltipLineItem)}>
-            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
-            51% or papers or more are open access
-          </div>          
-        </div>
-      )
-    }
-    else if (achievement === "CITED_AUTHOR") {
-      return (
-        <div className={css(styles.tooltip)}>
-          <div className={css(styles.tooltipLineItem)}>
-            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
-            Author on paper that received at least 1 citation
+          <div className={css(styles.progressBar)}>
+            <div style={{ position: "absolute", top: 2, paddingLeft: 5  }}>
+              {TIER_INDICES[achievement.currentMilestoneIndex + 1]}
+            </div>
+            <div style={{ borderRadius: "4px 0 0 4px", background: filledColor, height: "100%", width: (achievement.pctProgress * 100) + "%" }}>
+            </div>
           </div>
-        </div>
-      )
-    }
-    else if (achievement === "OPEN_SCIENCE_SUPPORTER") {
-      return (
-        <div className={css(styles.tooltip)}>
-          <div className={css(styles.tooltipLineItem)}>
-            <FontAwesomeIcon icon={faCircleCheck} color={colors.GREEN()} />
-            Funded open science using RSC
-          </div>
-        </div>
-      )
-    }    
-    else if (achievement.includes("HIGHLY_UPVOTED")) {
-      return (
-        <div className={css(styles.tooltip)}>
-          <div className={css(styles.tooltipLineItem)}>
-            <FontAwesomeIcon icon={faCircleCheck} color={"#B9F2FF"} />
-            Received at least 10 upvotes
-          </div>
-        </div>
-      )
-    }
-    else if (achievement.includes("EXPERT_PEER_REVIEWER")) {
-      return (
-        <div className={css(styles.tooltip)}>
-          <div className={css(styles.tooltipLineItem)}>
-            <FontAwesomeIcon icon={faCircleCheck} color={"#B9F2FF"} />
-            Peer reviewed at least 1 publication
-          </div>
-        </div>
-      )
-    }
+        </>
+      )}
+    </div>
+  )
   }
 
   return (
@@ -80,8 +71,17 @@ const AuthorHeaderAchievements = ({ summaryStats, achievements }: { summaryStats
         const achivementDetails = getAchievmentDetails({ achievement, summaryStats, })
 
         return (
-          <Tooltip title={getTooltipContent(achievement)}>
-            <div key={achievement} className={css(styles.achievement)}>
+          <Tooltip
+            title={getTooltipContent(achievement, achivementDetails)}
+            componentsProps={{
+              tooltip: {
+                sx: {
+                  bgcolor: "rgba(36, 31, 58, 1)",
+                },
+              },
+            }}            
+          >
+            <div key={achievement.type} className={css(styles.achievement)}>
               <div>{achivementDetails.icon}</div>
               <div>{achivementDetails.title}</div>
             </div>
@@ -98,6 +98,13 @@ const AuthorHeaderAchievements = ({ summaryStats, achievements }: { summaryStats
 }
 
 const styles = StyleSheet.create({
+  progressBar: {
+    borderRadius: 4,
+    width: "100%",
+    height: 20,
+    background: "rgba(124, 121, 137, 1)",
+    position: "relative"
+  },
   rootWrapper: {
     rowGap: 5,
     display: "flex",
@@ -107,11 +114,12 @@ const styles = StyleSheet.create({
   },
   tooltip: {
     padding: 6,
-    fontSize: 14,
+    fontSize: 13,
     display: "flex",
     flexDirection: "column",
     alignContent: "center",    
     gap: 5,
+    width: 250,
   },
   tooltipLineItem: {
     display: "flex",
