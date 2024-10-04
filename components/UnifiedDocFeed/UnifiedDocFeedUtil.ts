58bc3ad0693d97dbbf5c3621c1d1bd709592c5e6
Gregor Zurowski <gregor@zurowski.net>
2024-10-04 12:18:45 +0200

    fix: Stop capitalizing header labels
    This fix will prevent hub names like `bioRxiv` to be rendered
    as `BioRxiv`.

--- components/UnifiedDocFeed/UnifiedDocFeedUtil.ts
+++ components/UnifiedDocFeed/UnifiedDocFeedUtil.ts
@@ -1,5 +1,3 @@
-import { capitalize } from "../../config/utils/string";
-
 type FormatMainHeaderArgs = {
   label: string;
   isHomePage: boolean;
@@ -11,9 +9,5 @@ export function formatMainHeader({
   isHomePage,
   isLiveFeed,
 }: FormatMainHeaderArgs): string | null {
-  return isHomePage || isLiveFeed
-    ? `Explore ResearchHub`
-    : label
-    ? `${capitalize(label)}`
-    : null;
+  return isHomePage || isLiveFeed ? `Explore ResearchHub` : label;
 }
