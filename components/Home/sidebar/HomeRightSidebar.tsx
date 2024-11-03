2f1b68a03f2061ab1fd5e1deab2b9edbc0625ed7
Tyler Diorio <109099227+TylerDiorio@users.noreply.github.com>
2024-11-03 08:39:14 -0700

    Also fix sidebar render bug
    Don't wait until the server tells us whether to show the banner to the user or not before rendering the right sidebar.

--- components/Home/sidebar/HomeRightSidebar.tsx
+++ components/Home/sidebar/HomeRightSidebar.tsx
@@ -1,26 +1,15 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/pro-light-svg-icons";
-import { getEducationalCarouselElements } from "~/components/shared/carousel/presets/RhEducationalCarouselElements";
-import { INFO_TAB_EXIT_KEY } from "~/components/Banner/constants/exitable_banner_keys";
 import { ReactElement, useState, useEffect } from "react";
 import { styles } from "./styles/HomeRightSidebarStyles";
-import colors from "~/config/themes/colors";
 import ColumnContainer from "../../Paper/SideColumn/ColumnContainer";
-import ExitableBanner from "~/components/Banner/ExitableBanner";
 import HomeSidebarBountiesSection from "./HomeSidebarBountiesSection";
 import { css, StyleSheet } from "aphrodite";
-import RhCarousel from "~/components/shared/carousel/RhCarousel";
 import { useSelector } from "react-redux";
 import { RootState } from "~/redux";
-import { parseUser } from "~/config/types/root_types";
-import { isEmpty } from "~/config/utils/nullchecks";
 import { useDismissableFeature } from "~/config/hooks/useDismissableFeature";
-import useCurrentUser from "~/config/hooks/useCurrentUser";
 import UnifiedCarousel from "./UnifiedCarousel";
 
-export default function HomeRightSidebar(): ReactElement | null {
+export default function HomeRightSidebar(): ReactElement {
   const auth = useSelector((state: RootState) => state.auth);
-  const [dismissStatus, setDismissStatus] = useState<"unchecked" | "checked">("unchecked");
   
   const {
     isDismissed: isCarouselDismissed,
@@ -28,17 +17,12 @@ export default function HomeRightSidebar(): ReactElement | null {
     dismissStatus: carouselDismissStatus,
   } = useDismissableFeature({ auth, featureName: "educational-carousel" });
 
-  // Don't render anything until we've confirmed the dismissal state
-  if (carouselDismissStatus === "unchecked") {
-    return null;
-  }
-
   return (
     <div className={css(styles.HomeRightSidebar)}>
       <ColumnContainer overrideStyles={styles.HomeRightSidebarContainer}>
-        {!isCarouselDismissed && carouselDismissStatus === "checked" && (
+        {carouselDismissStatus === "checked" && !isCarouselDismissed && (
           <UnifiedCarousel
-            onDismissCarousel={dismissCarousel}
+            onDismissCarousel={() => dismissCarousel()}
           />
         )}
         <HomeSidebarBountiesSection shouldLimitNumCards={!isCarouselDismissed} />
