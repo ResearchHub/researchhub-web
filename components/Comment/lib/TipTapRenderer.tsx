7d3ea7162778dffa3072b1f82f04b4ae1c89d7c8
nicktytarenko <nicktytarenko@gmail.com>
2025-04-27 14:19:44 +0300

    make it bolder a bit

--- components/Comment/lib/TipTapRenderer.tsx
+++ components/Comment/lib/TipTapRenderer.tsx
@@ -755,6 +755,7 @@ const styles = StyleSheet.create({
     color: colors.BLUE(1),
     cursor: "pointer",
     textDecoration: "none",
+    fontWeight: 500,
     ":hover": {
       textDecoration: "underline",
     },
